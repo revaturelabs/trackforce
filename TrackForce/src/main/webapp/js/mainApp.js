9b51a2a28e9a053e1f8b8041a7aba0bbe22c695c
DESKTOP-POTLDLH\rguaj <rguajardo95@gmail.com>
2017-11-01 15:35:48 -0400

    Fixed the SPA

--- TrackForce/src/main/webapp/js/mainApp.js
+++ TrackForce/src/main/webapp/js/mainApp.js
@@ -1,27 +1,36 @@
-var mainApp = angular.module("mainApp", [ 'ngRoute', 'chart.js' ]);
+var mainApp = angular.module('mainApp', [ 'ngRoute', 'chart.js' ]);
+
 mainApp.config(function($routeProvider) {
-	$routeProvider.when("/batchListing", {
+	$routeProvider
+
+	// Home Page route
+	.when("/", {
+		templateUrl : "home.html",
+		controller : "mainCtrl"
+	})
+
+	// Mapped View Page route
+	.when("/batchListing", {
 		templateUrl : "batchListing.html",
-		controller : "batchListingCtrl"
-	}).when("/batchDetails", {
+		controller : "batchCtrl"
+	})
+
+	// Unmapped View Page route
+	.when("/batchDetails", {
 		templateUrl : "batchDetails.html",
-		controller : "batchDetailsCtrl"
-	}).otherwise({
-		redirectTo : '/'
-	});
+		controller : "batchCtrl"
+	})
 
 });
-mainApp.controller("batchListingCtrl", function mainController($scope) {
-	$scope.labels = [ "SDET", "Full Stack Java", ".NET", "Salesforce" ];
-	$scope.data = [ 15, 20, 25, 30 ];
-	$scope.type = 'polarArea';
-	$scope.toggle = function() {
-		$scope.type = $scope.type === 'polarArea' ? 'pie' : 'polarArea';
-	};
-});
 
-mainApp.controller("batchDetailsCtrl", function($scope) {
-	$scope.labels = [ 'Mapped', 'Unmapped' ];
-	//$scope.series = ['Mapped', 'Unmapped'];
-	$scope.data = [ [ 25, 35 ] ];
+mainApp.controller("mainCtrl", function mainCtrl($scope, $http) {
+
+})
+
+mainApp.controller("batchCtrl", function($scope) {
+	$scope.labels = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ];
+	$scope.series = [ 'Series A', 'Series B' ];
+
+	$scope.data = [ [ 65, 59, 80, 81, 56, 55, 40 ],
+			[ 28, 48, 40, 19, 86, 27, 90 ] ];
 });

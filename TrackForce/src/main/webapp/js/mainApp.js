185a4fe470b341df9cbef8ba6c383e8e605c3d62
DESKTOP-POTLDLH\rguaj <rguajardo95@gmail.com>
2017-11-01 20:14:38 -0400

    Added http methods in mainApp.js for batches.

--- TrackForce/src/main/webapp/js/mainApp.js
+++ TrackForce/src/main/webapp/js/mainApp.js
@@ -27,7 +27,90 @@ mainApp.controller("mainCtrl", function mainCtrl($scope, $http) {
 
 })
 
-mainApp.controller("batchCtrl", function($scope) {
+mainApp.controller("batchCtrl", function($scope, $http) {
+	
+	$scope.getBatches = function($http) {
+		// Simple GET request example:
+		$http({
+		  method: 'GET',
+		  url: 'http://localhost:8080/TrackForce/batches'
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+			$scope.batches = response.data;
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		})
+		
+	};
+	
+	$scope.getCountPerBatchType = function($http) {
+		// Simple GET request example:
+		$http({
+		  method: 'GET',
+		  url: 'http://localhost:8080/TrackForce/batches/type'
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+			$scope.amountType = response.data;
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		})
+		
+	};
+	
+	$scope.getBatchAssociates = function($http) {
+		// Simple GET request example:
+		$http({
+		  method: 'GET',
+		  url: 'http://localhost:8080/TrackForce/batches/' + $scope.batchname + '/associates'
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+			$scope.associatesBatch = response.data;
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		})
+		
+	};
+	
+	$scope.getBatchInfo = function($http) {
+		// Simple GET request example:
+		$http({
+		  method: 'GET',
+		  url: 'http://localhost:8080/TrackForce/batches' + $scope.batchname + '/info'
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+			$scope.batchInfo = response.data;
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		})
+		
+	};
+	
+	$scope.getMapStatusBatch = function($http) {
+		// Simple GET request example:
+		$http({
+		  method: 'GET',
+		  url: 'http://localhost:8080/TrackForce/batches' + $scope.batchname + '/batchChart'
+		}).then(function successCallback(response) {
+		    // this callback will be called asynchronously
+		    // when the response is available
+			$scope.batchMapStatus = response.data;
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		})
+		
+	};
+	
+	
+	
 	$scope.labels = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ];
 	$scope.series = [ 'Series A', 'Series B' ];
 

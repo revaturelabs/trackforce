4d0cf83d8054fb3741610107a4322c9f72e5e240
Jacob Hackel <jacob.hackel@gmail.com>
2017-11-02 10:02:54 -0400

    changed Client to ClientInfo, added get method for pulling list of client names(note: not the final version), changed path names to reflect changes

--- TrackForce/src/main/java/com/revature/services/ClientResource.java
+++ TrackForce/src/main/java/com/revature/services/ClientResource.java
@@ -1,28 +1,50 @@
 package com.revature.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import com.revature.model.Client;
+import com.revature.model.ClientInfo;
 
 @Path("/clients")
 public class ClientResource {
 
+	// should be list of client object/entity from DB
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Client getAllClients() {
-		Client clients = new Client("All Clients", 100, 95, 75, 107, 145, 23, 65, 72, 15, 34);
+	public Response getAllClients() {
+		List<String> clients = new ArrayList<>();
+		clients.add("Client1");
+		clients.add("Client2");
+		clients.add("Client3");
+		
+		Map<String, List<String>> entity = new HashMap<>();
+		entity.put("name", clients);
+
+		return Response.ok(entity).build();
+	}
+
+	@GET
+	@Path("info")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public ClientInfo getAllClientInfo() {
+		ClientInfo clients = new ClientInfo("All Clients", 100, 95, 75, 107, 145, 23, 65, 72, 15, 34);
 		return clients;
 	}
 
 	@GET
 	@Path("{clientid}")
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Client getAClient(@PathParam("clientid") int clientid) {
-		Client aClient = new Client("A Single Client", 5, 17, 34, 12, 6, 44, 23, 25, 16, 12);
+	public ClientInfo getClientInfo(@PathParam("clientid") int clientid) {
+		ClientInfo aClient = new ClientInfo("A Single Client", 5, 17, 34, 12, 6, 44, 23, 25, 16, 12);
 		return aClient;
 	}
 

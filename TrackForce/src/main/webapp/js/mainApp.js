d8d7bd077941222bfe1f115c0e539095e6ee5409
ryantimms <ryantimms92@gmail.com>
2017-11-02 09:04:44 -0400

    updated js controller

--- TrackForce/src/main/webapp/js/mainApp.js
+++ TrackForce/src/main/webapp/js/mainApp.js
@@ -35,30 +35,49 @@ mainApp.controller("batchCtrl", function($scope) {
 			[ 28, 48, 40, 19, 86, 27, 90 ] ];
 });
 
-
-mainApp.controller("clientDetailsCtrl", function($scope, $http){
-$scope.getAllClients=function(){
-	$http(method: "GET", 
-		url:"http://localhost:8080/TrackForce/track/clients")
-			.then(function(response){
-		$scope.clients=response.data;
-		$scope.labels=['Training', 'Reserved', 'Selected', 'Confirmed', 'Deployed'];
-		$scope.series=['Mapped','Unmapped'];
-		$scope.data=[$scope.clients.training,$scope.clients.reserved,$scope.clients.selected, 
-			$scope.clients.confirmed, $scope.clients.deployed];
-		});
+mainApp.controller("clientDetailsCtrl", function($scope, $http) {
+	$scope.getAllClients = function() {
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/TrackForce/track/clients"
+		})
+				.then(
+						function(response) {
+							$scope.clients = response.data;
+							$scope.labels = [ 'Training', 'Reserved - Mapped',
+								'Reserved - Unmapped', 'Selected - Mapped',
+								'Selected - Unmapped', 'Confirmed - Mapped',
+								'Confirmed - Unmapped', 'Deployed - Mapped',
+								'Deployed - Unmapped' ];
+							$scope.series = [ 'Mapped', 'Unmapped' ];
+							$scope.data = [ $scope.clients.trainingMapped,
+									$scope.clients.reservedMapped,$scope.clients.reservedUnmapped,
+									$scope.clients.selectedMapped,$scope.clients.selectedUnmapped,
+									$scope.clients.confirmedMapped,$scope.clients.confirmedUnmapped,
+									$scope.clients.deployedMapped,$scope.clients.deployedUnmapped  ];
+						});
 	}
 
-$scope.getOneClient=function(){
-	$http(method: "GET", 
-		url:"http://localhost:8080/TrackForce/track/clients/"+$scope.searchValue)
-			.then(function(response){
-		$scope.client=response.data;
-		$scope.labels=['Training', 'Reserved', 'Selected', 'Confirmed', 'Deployed'];
-		$scope.series=['Mapped','Unmapped'];
-		$scope.data=[$scope.client.training,$scope.client.reserved,$scope.client.selected, 
-			$scope.client.confirmed, $scope.client.deployed];
-		});
+	$scope.getOneClient = function() {
+		$http(
+				{
+					method : "GET",
+					url : "http://localhost:8080/TrackForce/track/clients/"
+							+ $scope.searchValue
+				}).then(
+				function(response) {
+					$scope.client = response.data;
+					$scope.labels = [ 'Training', 'Reserved - Mapped',
+							'Reserved - Unmapped', 'Selected - Mapped',
+							'Selected - Unmapped', 'Confirmed - Mapped',
+							'Confirmed - Unmapped', 'Deployed - Mapped',
+							'Deployed - Unmapped' ];
+					$scope.data = [ $scope.clients.trainingMapped,
+						$scope.clients.reservedMapped,$scope.clients.reservedUnmapped,
+						$scope.clients.selectedMapped,$scope.clients.selectedUnmapped,
+						$scope.clients.confirmedMapped,$scope.clients.confirmedUnmapped,
+						$scope.clients.deployedMapped,$scope.clients.deployedUnmapped  ];
+				});
 	}
+
 });
-

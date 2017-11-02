5d70486a0ea5395273073bf47f833304790ee817
Jacob Hackel <jacob.hackel@gmail.com>
2017-11-02 13:04:13 -0400

    hooked up rest service to dao implementation

--- TrackForce/src/main/java/com/revature/dao/ClientDao.java
+++ TrackForce/src/main/java/com/revature/dao/ClientDao.java
@@ -1,5 +1,14 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import com.revature.entity.TfClient;
+import com.revature.model.ClientInfo;
+
 public interface ClientDao {
 
+	List<TfClient> getAllTfClients();
+	ClientInfo getAllClientInfo();
+	ClientInfo getClientInfo(int id);
+	
 }
--- TrackForce/src/main/java/com/revature/services/ClientResource.java
+++ TrackForce/src/main/java/com/revature/services/ClientResource.java
@@ -11,20 +11,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.revature.dao.ClientDao;
+import com.revature.dao.ClientDaoImpl;
 import com.revature.entity.TfClient;
 import com.revature.model.ClientInfo;
 
 @Path("/clients")
 public class ClientResource {
+	
+	private ClientDao clientDaoImpl = new ClientDaoImpl();
 
-	// should be list of client object/entity from DB
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON })
 	public Response getAllClients() {
-		List<TfClient> clients = new ArrayList<>();
-		clients.add(new TfClient(new BigDecimal(1), "Client 1"));
-		clients.add(new TfClient(new BigDecimal(2), "Client 2"));
-		clients.add(new TfClient(new BigDecimal(3), "Client 3"));
+		List<TfClient> clients = clientDaoImpl.getAllTfClients();
 
 		return Response.ok(clients).build();
 	}
@@ -33,7 +33,7 @@ public class ClientResource {
 	@Path("info")
 	@Produces({ MediaType.APPLICATION_JSON })
 	public ClientInfo getAllClientInfo() {
-		ClientInfo clients = new ClientInfo("All Clients", 100, 95, 75, 107, 145, 23, 65, 72, 15, 34);
+		ClientInfo clients = clientDaoImpl.getAllClientInfo();
 		return clients;
 	}
 
@@ -41,7 +41,7 @@ public class ClientResource {
 	@Path("{clientid}")
 	@Produces({ MediaType.APPLICATION_JSON })
 	public ClientInfo getClientInfo(@PathParam("clientid") int clientid) {
-		ClientInfo aClient = new ClientInfo("A Single Client", 5, 17, 34, 12, 6, 44, 23, 25, 16, 12);
+		ClientInfo aClient = clientDaoImpl.getClientInfo(clientid);
 		return aClient;
 	}
 

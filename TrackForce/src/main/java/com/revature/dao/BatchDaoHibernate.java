2247aa8c1aba61e97e809db4f6862ed3c70828a6
MPersil <Persil.Matthew@gmail.com>
2017-11-03 16:13:08 -0400

    Edited getBatch to use a stored procedure.

--- TrackForce/src/main/java/com/revature/dao/BatchDaoHibernate.java
+++ TrackForce/src/main/java/com/revature/dao/BatchDaoHibernate.java
@@ -3,54 +3,60 @@ package com.revature.dao;
 import java.sql.Timestamp;
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.ParameterMode;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.engine.jdbc.batch.spi.Batch;
+import org.hibernate.procedure.ProcedureCall;
 import org.hibernate.query.Query;
+import org.hibernate.result.Output;
+import org.hibernate.result.ResultSetOutput;
 
 import com.revature.entity.TfBatch;
 import com.revature.utils.HibernateUtil;
 
 public class BatchDaoHibernate implements BatchDao {
 
-	// fromdate is a variable we created, batchstartdate is from the
-	public List<TfBatch> getBatchDetails(Timestamp fromdate, Timestamp todate) {
-		String batchdetails = "from com.revature.entity.TfBatch where (tfBatchStartDate between :fromdate and :todate) or (tfBatchEndDate between  :fromdate and :todate)";
-		SessionFactory conn = HibernateUtil.getSession();
-		Session obj=conn.getCurrentSession();
-		Query<TfBatch> q = obj.createQuery(batchdetails);
-		q.setParameter("fromdate", fromdate);
-		q.setParameter("from_date", fromdate);
-		q.setParameter("todate", todate);
-		q.setParameter("to_date", todate);
-		List<TfBatch> batch_details = q.list();
-		return batch_details;
+    // fromdate is a variable we created, batchstartdate is from the
+    public List<TfBatch> getBatchDetails(Timestamp fromdate, Timestamp todate) {
+        String batchdetails = "from com.revature.entity.TfBatch where (tfBatchStartDate between :fromdate and :todate) or (tfBatchEndDate between  :fromdate and :todate)";
+        SessionFactory conn = HibernateUtil.getSession();
+        Session obj = conn.getCurrentSession();
+        Query<TfBatch> q = obj.createQuery(batchdetails);
+        q.setParameter("fromdate", fromdate);
+        q.setParameter("from_date", fromdate);
+        q.setParameter("todate", todate);
+        q.setParameter("to_date", todate);
+        List<TfBatch> batch_details = q.list();
+        return batch_details;
 
-	}
-	
-    
+    }
+
     /**
      * Get a batch from the database given its name.
      */
     @Override
-    public TfBatch getBatch(String batchName){
+    public TfBatch getBatch(String batchName) {
         SessionFactory sessionFactory = HibernateUtil.getSession();
         Session session = sessionFactory.openSession();
-        
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<TfBatch> criteriaQuery = builder.createQuery(TfBatch.class);
-        Root<TfBatch> root = criteriaQuery.from(TfBatch.class);
-        criteriaQuery.select(root).where(builder.equal(root.get("tfBatchName"), batchName));
-        Query<TfBatch> query = session.createQuery(criteriaQuery);
-        TfBatch batch = query.getSingleResult();
-        
+
+        // CriteriaBuilder builder = session.getCriteriaBuilder();
+        // CriteriaQuery<TfBatch> criteriaQuery = builder.createQuery(TfBatch.class);
+        // Root<TfBatch> root = criteriaQuery.from(TfBatch.class);
+        // criteriaQuery.select(root).where(builder.equal(root.get("tfBatchName"),
+        // batchName));
+        // Query<TfBatch> query = session.createQuery(criteriaQuery);
+        // TfBatch batch = query.getSingleResult();
+
+        ProcedureCall call = session.createStoredProcedureCall("Test");
+        call.registerParameter(1, Integer.class, ParameterMode.IN).bindValue(1);
+        call.registerParameter(2, Class.class, ParameterMode.REF_CURSOR);
+
+        Output output = call.getOutputs().getCurrent();
+        TfBatch batch = (TfBatch) ((ResultSetOutput) output).getSingleResult();
+
         session.close();
-        
+
         return batch;
     }
 }

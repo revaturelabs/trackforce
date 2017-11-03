aa158e34212ed5ea9c8831841d2f890c6f12bfd4
Jacob Hackel <jacob.hackel@gmail.com>
2017-11-03 16:37:15 -0400

    client resource test beginning

---
+++ TrackForce/src/test/java/com/revature/services/ClientResourceTest.java
@@ -0,0 +1,17 @@
+package com.revature.services;
+
+import org.mockito.Mock;
+import org.testng.annotations.Test;
+
+import com.revature.dao.ClientDao;
+import com.revature.dao.ClientDaoImpl;
+
+public class ClientResourceTest {
+
+	@Mock private ClientDao clientDaoImpl = new ClientDaoImpl();
+	
+	
+	@Test
+	public void f() {
+	}
+}

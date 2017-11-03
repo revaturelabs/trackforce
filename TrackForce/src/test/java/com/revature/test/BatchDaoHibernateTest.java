58c78b4d6d9b878dc5405ac8db64ea78e7f6c8f9
MPersil <Persil.Matthew@gmail.com>
2017-11-03 13:07:49 -0400

    Working to make batch work with serialization.

--- TrackForce/src/test/java/com/revature/test/BatchDaoHibernateTest.java
+++ TrackForce/src/test/java/com/revature/test/BatchDaoHibernateTest.java
@@ -10,22 +10,12 @@ import com.revature.entity.TfBatch;
 
 public class BatchDaoHibernateTest {
     
-  @Test (enabled=false)
-  public void getCurriculumNameTest() {
-      BatchDao batchDao = new BatchDaoHibernate();
-      
-      String curriculumName = batchDao.getBatchCirriculumName(0);
-      assertNotNull(curriculumName);
-      
-      System.out.println(curriculumName);
-  }
-  
   @Test
   public void getBatchByIDTest() {
       BatchDao batchDao = new BatchDaoHibernate();
       TfBatch batch = batchDao.getBatch(1);
       
       assertNotNull(batch);
-      System.out.println(batch);
+      System.out.println(batch.getTfClient());
   }
 }

dd284f221edba25cf532e2129eef2e3608dae679
Jarrod Bieber <jarrodb94@gmail.com>
2017-11-06 09:23:35 -0500

    pushing javadocs changes so far

--- TrackForce/src/main/java/com/revature/model/ClientInfo.java
+++ TrackForce/src/main/java/com/revature/model/ClientInfo.java
@@ -1,5 +1,10 @@
 package com.revature.model;
 
+/**
+ * This class models all information that pertains to clients
+ * @author Jacob Hackel, Jarrod Bieber
+ * @version 1.0
+ */
 public class ClientInfo {
 
 	private String name; // Name of client
@@ -13,10 +18,27 @@ public class ClientInfo {
 	private int confirmedUnmapped;
 	private int deployedMapped;
 	private int deployedUnmapped;
-
+	
+	/**
+	 * Default no-arg constructor
+	 */
 	public ClientInfo() {
 	}
-
+	
+	/**
+	 * Constructs a ClientInfo object
+	 * @param name The name of the client
+	 * @param trainingMapped The number of associates who are currently training and mapped to this client.
+	 * @param trainingUnmapped The number of associates who are currently training and not mapped to this client.
+	 * @param reservedMapped The number of associates who are finished training and mapped to this client.
+	 * @param openUnmapped The number of associates who are finished training and are not mapped to this client.
+	 * @param selectedMapped The number of associates who have been selected by, and were mapped to this client.
+	 * @param selectedUnmapped The number of associates who have been selected by, but were not mapped to this client.
+	 * @param confirmedMapped The number of associates who have been confirmed by and were mapped to this client.
+	 * @param confirmedUnmapped The number of associates who have been confirmed, and were not mapped to this client.
+	 * @param deployedMapped The number of associates who are deployed to a client location and were mapped to this client.
+	 * @param deployedUnmapped The number of associates who are deployed to a client location and were not mapped to this client.
+	 */
 	public ClientInfo(String name, int trainingMapped, int trainingUnmapped, int reservedMapped, int openUnmapped,
 			int selectedMapped, int selectedUnmapped, int confirmedMapped, int confirmedUnmapped, int deployedMapped,
 			int deployedUnmapped) {
@@ -34,71 +56,134 @@ public class ClientInfo {
 		this.deployedUnmapped = deployedUnmapped;
 	}
 	
-
+	/**
+	 * This returns the name of this client
+	 * @return this client's name
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * This allows the client name to be set
+	 * @param name the name to set for this client
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * This returns the number associates who are mapped and training
+	 * @return the number of mapped associates who are training
+	 */
 	public int getTrainingMapped() {
 		return trainingMapped;
 	}
-
+	
+	/**
+	 * This allows the number of mapped associates who are training to be set
+	 * @param trainingMapped the number of mapped associates who are training
+	 */
 	public void setTrainingMapped(int trainingMapped) {
 		this.trainingMapped = trainingMapped;
 	}
-
+	
+	/**
+	 * This returns the number of associates who are unmapped and training
+	 * @return the number of unmapped associates who are training
+	 */
 	public int getTrainingUnmapped() {
 		return trainingUnmapped;
 	}
-	
+
+	/**
+	 * This allows the number of unmapped associates who are training to be set.
+	 * @param trainingUnmapped the number of unmapped associates who are training
+	 */
 	public void setTrainingUnmapped(int trainingUnmapped) {
 		this.trainingUnmapped = trainingUnmapped;
 	}
-
+	
+	
 	public void confirmed(int trainingUnmapped) {
 		this.trainingUnmapped = trainingUnmapped;
 	}
-
+	
+	/**
+	 * This returns the number of associates who are mapped and finished with training.
+	 * @return the number of mapped associates who are done training
+	 */
 	public int getReservedMapped() {
 		return reservedMapped;
 	}
-
+	
+	/**
+	 * This allows the number of mapped associates who are finished training to be set.
+	 * @param reservedMapped the number of unmapped associates who are done training.
+	 */
 	public void setReservedMapped(int reservedMapped) {
 		this.reservedMapped = reservedMapped;
 	}
-
+	
 	public int getOpenUnmapped() {
 		return openUnmapped;
 	}
-
+	
 	public void setOpenUnmapped(int openUnmapped) {
 		this.openUnmapped = openUnmapped;
 	}
 
+	/**
+	 * This returns the number of mapped associates who have been selected
+	 * by the client.
+	 * @return The number of mapped associates who were selected by this client.
+	 */
 	public int getSelectedMapped() {
 		return selectedMapped;
 	}
-
+	
+	/**
+	 * This allows the number of mapped associates who have been selected
+	 * by the client to be set.
+	 * @param selectedMapped The number of mapped associates who are selected by this client.
+	 */
 	public void setSelectedMapped(int selectedMapped) {
 		this.selectedMapped = selectedMapped;
 	}
-
+	
+	/**
+	 * This returns the number of associates who were selected by the client, but weren't mapped.
+	 * @return The number of unmapped associates who were selected by this client.
+	 */
 	public int getSelectedUnmapped() {
 		return selectedUnmapped;
 	}
-
+	
+	
+	/**
+	 * This allows the number of associates who are selected by the client,
+	 * but aren't mapped, to be set.
+	 * @param selectedUnmapped The number of unmapped associates who are selected.
+	 */
 	public void setSelectedUnmapped(int selectedUnmapped) {
 		this.selectedUnmapped = selectedUnmapped;
 	}
-
+	
+	/**
+	 * This returns the number of associates who were mapped and selected
+	 * by the client.
+	 * @return The number of mapped associates who were confirmed by this client.
+	 */
 	public int getConfirmedMapped() {
 		return confirmedMapped;
 	}
-
+	
+	/**
+	 * This allows the number of associates who were mapped and confirmed
+	 * by this client to be set.
+	 * @param confirmedMapped The number of mapped associates who were confirmed by
+	 * this client.
+	 */
 	public void setConfirmedMapped(int confirmedMapped) {
 		this.confirmedMapped = confirmedMapped;
 	}

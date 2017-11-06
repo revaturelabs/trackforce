24a7b95e6df0f931d9ffe8eef01ebab85ec2fd48
Jacob Hackel <jacob.hackel@gmail.com>
2017-11-06 17:05:21 -0500

    addition to homeresource

--- TrackForce/src/main/java/com/revature/services/HomeResource.java
+++ TrackForce/src/main/java/com/revature/services/HomeResource.java
@@ -1,20 +1,56 @@
 package com.revature.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.revature.entity.TfAssociate;
 import com.revature.model.ClientInfo;
 
 @Path("/")
 public class HomeResource {
-	
+
 	@GET
 	@Path("info")
 	@Produces({ MediaType.APPLICATION_JSON })
 	public ClientInfo getMappedAndUnmappedInfo() {
-		return new ClientInfo("My test",2,4,24,6,5,15,61,14,13,4);
+		List<TfAssociate> associates = new ArrayList<>();
+		return countAssociatesBasedOnStatus(associates);
+	}
+
+	private ClientInfo countAssociatesBasedOnStatus(List<TfAssociate> associates) {
+		ClientInfo clientInfo = new ClientInfo();
+		for (TfAssociate associate : associates) {
+			switch (associate.getTfMarketingStatus().getTfMarketingStatusId().intValue()) {
+			case 1:
+				clientInfo.setTrainingMapped(clientInfo.getTrainingMapped() + 1);
+			case 2:
+				clientInfo.setReservedMapped(clientInfo.getReservedMapped() + 1);
+			case 3:
+				clientInfo.setSelectedMapped(clientInfo.getSelectedMapped() + 1);
+			case 4:
+				clientInfo.setConfirmedMapped(clientInfo.getConfirmedMapped() + 1);
+			case 5:
+				clientInfo.setDeployedMapped(clientInfo.getDeployedMapped() + 1);
+			case 6:
+				clientInfo.setTrainingUnmapped(clientInfo.getTrainingUnmapped() + 1);
+			case 7:
+				clientInfo.setOpenUnmapped(clientInfo.getOpenUnmapped() + 1);
+			case 8:
+				clientInfo.setSelectedUnmapped(clientInfo.getSelectedUnmapped() + 1);
+			case 9:
+				clientInfo.setConfirmedUnmapped(clientInfo.getConfirmedUnmapped() + 1);
+			case 10:
+				clientInfo.setDeployedUnmapped(clientInfo.getDeployedUnmapped() + 1);
+			}
+		}
+
+		clientInfo = new ClientInfo("My test", 2, 4, 24, 6, 5, 15, 61, 14, 13, 4);
+		return clientInfo;
 	}
 
 }

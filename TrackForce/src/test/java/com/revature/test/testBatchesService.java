af2c0cc6c4d749974f3d502cb09e47c73832744d
MPersil <Persil.Matthew@gmail.com>
2017-11-06 15:23:13 -0500

    Updated testBatchesService to reflect BatchInfo change.

--- TrackForce/src/test/java/com/revature/test/testBatchesService.java
+++ TrackForce/src/test/java/com/revature/test/testBatchesService.java
@@ -1,32 +1,29 @@
 package com.revature.test;
 
 import java.sql.Timestamp;
-
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
-import com.revature.dao.BatchDaoHibernate;
-import com.revature.entity.TfBatch;
+import com.revature.model.BatchInfo;
 import com.revature.services.BatchesService;
 
 public class testBatchesService {
+   
   @Test
   public void testGetBatchInfo() {
 	  BatchesService batchesService = new BatchesService();
-	  TfBatch batch = batchesService.getBatchInfo("1709 Sept11 JTA");
+	  BatchInfo batch = batchesService.getBatchInfo("1709 Sept11 JTA");
 	  System.out.println(batch);
   }
-  
   
   @Test
   public void testGetBatches() {
 	  long firstDate = Timestamp.valueOf("2017-09-10 10:10:10.0").getTime();
 	  long secondDate = Timestamp.valueOf("2017-12-30 10:10:10.0").getTime();
 	  BatchesService batchesService = new BatchesService();
-	  List<TfBatch> batches = batchesService.getBatches(firstDate, secondDate);
+	  List<BatchInfo> batches = batchesService.getBatches(firstDate, secondDate);
 	  Assert.assertNotNull(batches);
   }
 	

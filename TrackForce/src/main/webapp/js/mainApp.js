9e42c8d1057b91b56f8ad182c2b11b4e8176ddb7
adrian.chavez <adrianschavez17@gmail.com>
2017-11-07 08:58:58 -0500

    edits to batchDetails.html, batchListing.html mainApp.js testBatchesService.java

--- TrackForce/src/main/webapp/js/mainApp.js
+++ TrackForce/src/main/webapp/js/mainApp.js
@@ -15,7 +15,7 @@ mainApp.config(function($routeProvider) {
 	// Unmapped View Page route
 	.when("/batchDetails", {
 		templateUrl : "batchDetails.html",
-		controller : "batchCtrl"
+		controller : "batchDetailsCtrl"
 	}).when("/clientDetails", {
 		templateUrl : "clientDetails.html",
 		controller : "clientCtrl"
@@ -88,27 +88,7 @@ mainApp.controller("batchCtrl", function($scope, $http) {
 			}
 		})
 	};
-	$scope.getBatchAssociates = function($http) {
-		// Simple GET request example:
-		$http(
-				{
-					method : 'GET',
-					url : 'http://localhost:8080/TrackForce/track/batches/'
-							+ $scope.batchname + '/associates'
-				}).then(function successCallback(response) {
-			// this callback will be called asynchronously
-			// when the response is available
-			$scope.associatesBatch = response.data;
-		}, function errorCallback(response) {
-			// called asynchronously if an error occurs
-			// or server returns response with an error status.
-			$scope.assosicatesBatch = {
-				"firstname" : "Raul",
-				"lastname" : "Dummy-Data",
-				"associateId" : "000"
-			};
-		})
-	};
+	
 	$scope.getBatchInfo = function($http) {
 		// Simple GET request example:
 		$http(
@@ -129,40 +109,61 @@ mainApp.controller("batchCtrl", function($scope, $http) {
 			};
 		})
 	};
+});
+
+mainApp.controller("batchDetailsCtrl", function($scope,$http){
 	$scope.getMapStatusBatch = function() {
 		// Simple GET request example:
 		$http(
 				{
 					method : 'GET',
-					url : 'http://localhost:8080/TrackForce/track/batches/'
-							+ '1707 Jul10 PEGA' + '/batchChart'
-				}).then(function successCallback(response) {
+					url : 'http://localhost:8080/TrackForce/track/batches/' 
+						+ '1707 Jul10 PEGA' + '/batchChart'
+				}).then(function(response) {
 			// this callback will be called asynchronously
 			// when the response is available
 			var batchMapStatus = response.data;
-			$scope.mappedOrUnmapped = ['Mapped', 'Unmapped'];
-			$scope.data4 = [batchMapStatus.mappedCount, batchMapStatus.unmappedCount];
+			console.log(response.data);
+			$scope.labels = ['Mapped', 'Unmapped'];
+			$scope.data = [batchMapStatus.Mapped, batchMapStatus.Unmapped];
+			$scope.options = {
+					scales : {
+						yAxes : [ {
+							ticks : {
+								beginAtZero : true
+							}
+						} ]
+					}
+				};
 		}, function errorCallback(response) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
-			$scope.batchMapSatus = {
-				"Mapped" : "0",
-				"Unmapped" : "0"
-			}
 		})
 	};
-	$scope.labels = ["Mapped","Unmapped"]
-	$scope.data = ["70","61"]; 
-	$scope.options = {
-		scales : {
-			yAxes : [ {
-				ticks : {
-					beginAtZero : true
-				}
-			} ]
-		}
+	$scope.getBatchAssociates = function() {
+		// Simple GET request example:
+		$http(
+				{
+					method : 'GET',
+					url : 'http://localhost:8080/TrackForce/track/batches/'
+							+ '1707 Jul10 PEGA' + '/associates'
+				}).then(function successCallback(response) {
+			// this callback will be called asynchronously
+			// when the response is available
+			$scope.associatesBatch = response.data;
+			console.log(response.data); 
+		}, function errorCallback(response) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+			$scope.assosicatesBatch = {
+				"firstname" : "Raul",
+				"lastname" : "Dummy-Data",
+				"associateId" : "000"
+			};
+		})
 	};
 });
+
 // Controller used for the search bar function
 mainApp.controller("clientSearchAndListCtrl", function($scope, $http) {
 	/*

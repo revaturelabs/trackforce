c69604181b6194e0f3b7d2b0efcc475767adfbae
Jacob Hackel <jacob.hackel@gmail.com>
2017-11-10 14:26:18 -0500

    started test with reflections

--- TrackForce/src/test/java/com/revature/utils/StatusInfoUtilTest.java
+++ TrackForce/src/test/java/com/revature/utils/StatusInfoUtilTest.java
@@ -1,9 +1,30 @@
 package com.revature.utils;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class StatusInfoUtilTest {
-  @Test
-  public void f() {
-  }
+
+	Method getStageBasedOnStatusInfosAndStatusIDMethod;
+
+	@BeforeClass
+	public void beforeClass() {
+		StatusInfoUtil.class.getDeclaredMethods();
+		try {
+			getStageBasedOnStatusInfosAndStatusIDMethod = StatusInfoUtil.class
+					.getDeclaredMethod("getStageBasedOnStatusInfosAndStatusID", List.class, int.class);
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
+		getStageBasedOnStatusInfosAndStatusIDMethod.setAccessible(true);
+	}
+
+	@Test
+	public void getStageBasedOnStatusInfosAndStatusIDTest() {
+		System.out.println("The method: ");
+		System.out.println(getStageBasedOnStatusInfosAndStatusIDMethod.getName());
+	}
 }

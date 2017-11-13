84d60be0a6543e17d6c31e4662fc1aa7e3412630
aldmnatividad@gmail.com <aldmnatividad@gmail.com>
2017-11-13 12:54:19 -0500

    fix HomeDaoImplTest

--- TrackForce/src/test/java/com/revature/dao/HomeDaoImplTest.java
+++ TrackForce/src/test/java/com/revature/dao/HomeDaoImplTest.java
@@ -23,40 +23,11 @@ import org.testng.annotations.BeforeSuite;
 
 public class HomeDaoImplTest {
 
-  @Test
-  public void getAllTfAssociatesTest() {
-	  Session session = HibernateUtil.getSession().openSession();
-	  assertNotNull(session);
-		CriteriaQuery<TfAssociate> cq = session.getCriteriaBuilder().createQuery(TfAssociate.class);
-		assertNotNull(cq);
-		cq.from(TfAssociate.class);
-		assertNotNull(cq);
-		assertEquals(cq.getRoots().size(), 1);
-		List<TfAssociate> associates = session.createQuery(cq).getResultList();
-		assertFalse(associates.isEmpty());
-		for (TfAssociate associate : associates) {
-			if (associate.getTfBatch() != null) {
-				Hibernate.initialize(associate.getTfBatch());
-				System.out.println(associate.getTfBatch().getTfBatchName());
-				//assertNotNull(associate.getTfBatch());
-			}
-			Hibernate.initialize(associate.getTfMarketingStatus());
-			//assertNotNull(associate.getTfMarketingStatus());
-			Hibernate.initialize(associate.getTfClient());
-			//assertNotNull(associate.getTfClient());
-			
-			if (associate.getTfBatch() != null) {
-				if(associate.getTfBatch().getTfCurriculum()  != null) {
-					Hibernate.initialize(associate.getTfBatch().getTfCurriculum());
-					//System.out.println(associate.getTfBatch().getTfCurriculum().getTfCurriculumName());
-				}
-			}
-			//assertNotNull(associate.getTfBatch().getTfCurriculum());
-		}
-		
-		
-
-		session.close();
-		assertFalse(session.isConnected());
-  }
+	HomeDaoImpl hdao = new HomeDaoImpl();
+	
+	@Test
+	public void getAllTfAssociatesTest() {
+		List<TfAssociate> result = hdao.getAllTfAssociates();
+		assertNotNull(result);
+	}
 }

89866e8e3098fc1f4edd9d9f618e733f2de980d2
DESKTOP-POTLDLH\rguaj <rguajardo95@gmail.com>
2017-11-13 15:52:22 -0500

    hello

--- TrackForce/src/main/webapp/js/associateCtrl.js
+++ TrackForce/src/main/webapp/js/associateCtrl.js
@@ -6,9 +6,15 @@ angular.module('mainApp').controller('associateCtrl', function($http, $scope, $r
 		method : "GET",
 		url : "http://localhost:8080/TrackForce/track/associates/all"
 	}).then(function(response) {
-		$scope.curriculum = $rootScope.curriculum; 
 		$scope.associateInfo = response.data;
 	});
+	
+	$http({
+  		method : "GET",
+ 		url : "http://localhost:8080/TrackForce/track/associates/" + $routeParams.associateId
+  	}).then(function(response) {
+  		$scope.associate = response.data;
+  	})
 	
 	$scope.updateAssociate = function() {
 		$http({

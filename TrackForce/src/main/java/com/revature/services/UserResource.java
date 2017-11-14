0e12bcba101018ef76739605bae237562e511998
Jacob Hackel <jacob.hackel@gmail.com>
2017-11-14 10:05:50 -0500

    added status codes to send back for login

--- TrackForce/src/main/java/com/revature/services/UserResource.java
+++ TrackForce/src/main/java/com/revature/services/UserResource.java
@@ -20,10 +20,10 @@ import com.revature.utils.PasswordStorage.InvalidHashException;
 @Path("/user")
 public class UserResource {
 
-	 UserDaoImpl userDaoImpl = new UserDaoImpl();
+	UserDaoImpl userDaoImpl = new UserDaoImpl();
 
 	/**
-	 * Returns a Response object with a redirect 
+	 * Returns a Response object with a redirect
 	 * 
 	 * @param username
 	 *            Entered user name from a form.
@@ -38,21 +38,25 @@ public class UserResource {
 	@Produces({ MediaType.TEXT_HTML })
 	public Response submitCredentials(@FormParam("username") String username, @FormParam("password") String password) {
 		TfUser tfUser = userDaoImpl.getUser(username);
-		String hashedPassword = tfUser.getTfUserHashpassword();
-		try {
-			if (PasswordStorage.verifyPassword(password, hashedPassword)) {
-				System.out.println("Password verified");
-				//URI homeLocation = new URI("html/index.html");
-				URI homeLocation = new URI("../../../../TrackForce/html/index.html");
-				System.out.println("URI: " + homeLocation);
-				System.out.println("username: " + username + ", password: " + password);
-				System.out.println("User role ID: " + tfUser.getTfRole().getTfRoleName());
-				return Response.temporaryRedirect(homeLocation).build();
+		if (tfUser != null) {
+			String hashedPassword = tfUser.getTfUserHashpassword();
+			try {
+				if (tfUser.equals(new TfUser()))
+					return Response.status(Response.Status.UNAUTHORIZED).build();
+				else if (PasswordStorage.verifyPassword(password, hashedPassword)) {
+					System.out.println("Password verified");
+					URI homeLocation = new URI("../../../../TrackForce/html/index.html");
+					System.out.println("URI: " + homeLocation);
+					System.out.println("username: " + username + ", password: " + password);
+					System.out.println("User role name: " + tfUser.getTfRole().getTfRoleName());
+					return Response.temporaryRedirect(homeLocation).build();
+				} else
+					return Response.status(Response.Status.UNAUTHORIZED).build();
+			} catch (URISyntaxException | CannotPerformOperationException | InvalidHashException e) {
+				e.printStackTrace();
 			}
-		} catch (URISyntaxException | CannotPerformOperationException | InvalidHashException e) {
-			e.printStackTrace();
 		}
-		return Response.noContent().build();
+		return Response.status(Response.Status.BAD_REQUEST).build();
 	}
 
 }

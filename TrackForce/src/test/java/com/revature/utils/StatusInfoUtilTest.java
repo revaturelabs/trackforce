6fcb0c93508f4c249d840447d9e34afd037e764a
Jarrod Bieber <jarrodb94@gmail.com>
2017-11-15 08:50:31 -0500

    Jacob's addition to StatusInfoUtilTest

--- TrackForce/src/test/java/com/revature/utils/StatusInfoUtilTest.java
+++ TrackForce/src/test/java/com/revature/utils/StatusInfoUtilTest.java
@@ -4,9 +4,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.revature.services.ClientResource;
 
 public class StatusInfoUtilTest {
 
@@ -32,5 +35,47 @@ public class StatusInfoUtilTest {
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void tempTest() {
+
+		ClientResource clientResource = new ClientResource();
+
+		List<Map<String, Object>> maps = (List<Map<String, Object>>) clientResource.getAllClientsWithAssociates().getEntity();
+
+		for(Map<String, Object> map : maps) {
+
+			System.out.print(map.get("id") + " ");
+
+			System.out.println(map.get("name"));
+
+		}
+
+	}
+
+	
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void tempTest2() {
+
+		System.out.println();
+
+		
+
+		ClientResource clientResource = new ClientResource();
+
+		List<Map<String, Object>> maps = (List<Map<String, Object>>) clientResource.getAllClients().getEntity();
+
+		for(Map<String, Object> map : maps) {
+
+			System.out.print(map.get("id") + " ");
+
+			System.out.println(map.get("name"));
+
+		}
+
 	}
 }

fc1756402c70d9d8fb2254ee20d3a58619b0f62b
Adam Lif <adamlnk@neb.rr.com>
2017-11-16 09:48:30 -0500

    Formatting

--- TrackForce/src/main/java/com/revature/services/ClientResource.java
+++ TrackForce/src/main/java/com/revature/services/ClientResource.java
@@ -23,107 +23,107 @@ import com.revature.utils.StatusInfoUtil;
 @Path("/clients")
 public class ClientResource {
 
-	private ClientDaoImpl clientDaoImpl = new ClientDaoImpl();
-	private HomeDaoImpl homeDaoImpl = new HomeDaoImpl();
+    private ClientDaoImpl clientDaoImpl = new ClientDaoImpl();
+    private HomeDaoImpl homeDaoImpl = new HomeDaoImpl();
 
-	/**
-	 * Returns a map of all of the clients with associates from the TfClient table
-	 * as a response object.
-	 * 
-	 * @return A map of TfClients with associates as a Response object
-	 */
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response getAllClientsWithAssociates() {
-		List<TfClient> clients = clientDaoImpl.getAllTfClients();
-		List<Map<String, Object>> entity = new ArrayList<>();
-		for (TfClient client : clients) {
-			if (!client.getTfAssociates().isEmpty()) {
-				Map<String, Object> map = new HashMap<>();
-				map.put("id", client.getTfClientId());
-				map.put("name", client.getTfClientName());
-				entity.add(map);
-			}
-		}
-		return Response.ok(entity).build();
-	}
-	
-	/**
-	 * Returns a map of all of the clients from the TfClient table as a response
-	 * object.
-	 * 
-	 * @return A map of TfClients as a Response object
-	 */
-	@GET
-	@Path("all")
-	@Produces({ MediaType.APPLICATION_JSON })
-	public Response getAllClients() {
-		List<TfClient> clients = clientDaoImpl.getAllTfClients();
-		List<Map<String, Object>> entity = new ArrayList<>();
-		for (TfClient client : clients) {
-			Map<String, Object> map = new HashMap<>();
-			map.put("id", client.getTfClientId());
-			map.put("name", client.getTfClientName());
-			entity.add(map);
-		}
-		return Response.ok(entity).build();
-	}
+    /**
+     * Returns a map of all of the clients with associates from the TfClient table
+     * as a response object.
+     * 
+     * @return A map of TfClients with associates as a Response object
+     */
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response getAllClientsWithAssociates() {
+        List<TfClient> clients = clientDaoImpl.getAllTfClients();
+        List<Map<String, Object>> entity = new ArrayList<>();
+        for (TfClient client : clients) {
+            if (!client.getTfAssociates().isEmpty()) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", client.getTfClientId());
+                map.put("name", client.getTfClientName());
+                entity.add(map);
+            }
+        }
+        return Response.ok(entity).build();
+    }
 
-	/**
-	 * Returns a StatusInfo object representing all clients' associates and their
-	 * statuses.
-	 * 
-	 * @return A StatusInfo object for all clients
-	 */
-	@GET
-	@Path("info")
-	@Produces({ MediaType.APPLICATION_JSON })
-	public StatusInfo getAllClientInfo() {
-		init();
-		return StatusInfoUtil.getAllAssociatesStatusInfo();
-	}
+    /**
+     * Returns a map of all of the clients from the TfClient table as a response
+     * object.
+     * 
+     * @return A map of TfClients as a Response object
+     */
+    @GET
+    @Path("all")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response getAllClients() {
+        List<TfClient> clients = clientDaoImpl.getAllTfClients();
+        List<Map<String, Object>> entity = new ArrayList<>();
+        for (TfClient client : clients) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("id", client.getTfClientId());
+            map.put("name", client.getTfClientName());
+            entity.add(map);
+        }
+        return Response.ok(entity).build();
+    }
 
-	/**
-	 * Returns a StatusInfo object representing a client's associates and their
-	 * statuses.
-	 * 
-	 * @param clientid
-	 *            The id of the client in the TfClient table
-	 * @return A StatusInfo object for a specified client
-	 */
-	@GET
-	@Path("{clientid}")
-	@Produces({ MediaType.APPLICATION_JSON })
-	public StatusInfo getClientInfo(@PathParam("clientid") int clientid) {
-		init();
-		if (clientid < 1)
-			return new StatusInfo();
-		else {
-			return StatusInfoUtil.getClientStatusInfo(clientid);
-		}
-	}
+    /**
+     * Returns a StatusInfo object representing all clients' associates and their
+     * statuses.
+     * 
+     * @return A StatusInfo object for all clients
+     */
+    @GET
+    @Path("info")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public StatusInfo getAllClientInfo() {
+        init();
+        return StatusInfoUtil.getAllAssociatesStatusInfo();
+    }
 
-	/**
-	 * Initializes objects needed for functionality from the StatusInfoUtil when
-	 * maps in StatusInfoUtil are empty.
-	 */
-	private void init() {
-		if (StatusInfoUtil.mapsAreEmpty()) {
-			initForce();
-		}
-	}
+    /**
+     * Returns a StatusInfo object representing a client's associates and their
+     * statuses.
+     * 
+     * @param clientid
+     *            The id of the client in the TfClient table
+     * @return A StatusInfo object for a specified client
+     */
+    @GET
+    @Path("{clientid}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public StatusInfo getClientInfo(@PathParam("clientid") int clientid) {
+        init();
+        if (clientid < 1)
+            return new StatusInfo();
+        else {
+            return StatusInfoUtil.getClientStatusInfo(clientid);
+        }
+    }
 
-	/**
-	 * Forces initialization of objects needed for functionality from the
-	 * StatusInfoUtil.
-	 */
-	@PUT
-	@Path("init")
-	public void initForce() {
-		HomeDaoImpl.clearAssociates();
-		clientDaoImpl.clearClients();
-		StatusInfoUtil.clearMaps();
-		List<TfAssociate> tfAssociates = homeDaoImpl.getAllTfAssociates();
-		StatusInfoUtil.updateStatusInfoFromAssociates(tfAssociates);
-	}
+    /**
+     * Initializes objects needed for functionality from the StatusInfoUtil when
+     * maps in StatusInfoUtil are empty.
+     */
+    private void init() {
+        if (StatusInfoUtil.mapsAreEmpty()) {
+            initForce();
+        }
+    }
+
+    /**
+     * Forces initialization of objects needed for functionality from the
+     * StatusInfoUtil.
+     */
+    @PUT
+    @Path("init")
+    public void initForce() {
+        HomeDaoImpl.clearAssociates();
+        clientDaoImpl.clearClients();
+        StatusInfoUtil.clearMaps();
+        List<TfAssociate> tfAssociates = homeDaoImpl.getAllTfAssociates();
+        StatusInfoUtil.updateStatusInfoFromAssociates(tfAssociates);
+    }
 }

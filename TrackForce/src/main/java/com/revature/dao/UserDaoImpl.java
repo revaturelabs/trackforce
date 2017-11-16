94716ea9cee57109f49a564ad2ce4058d55b7d96
Adam Lif <adamlnk@neb.rr.com>
2017-11-16 09:15:28 -0500

    Formatting

--- TrackForce/src/main/java/com/revature/dao/UserDaoImpl.java
+++ TrackForce/src/main/java/com/revature/dao/UserDaoImpl.java
@@ -21,72 +21,72 @@ import com.revature.utils.jdbcUtil;
 
 public class UserDaoImpl implements UserDAO {
 
-	@Override
-	public TfUser getUser(String username) {
-		TfUser user;
-		SessionFactory sessionFactory = HibernateUtil.getSession();
-		try (Session session = sessionFactory.openSession()) {
-			CriteriaBuilder builder = session.getCriteriaBuilder();
-			CriteriaQuery<TfUser> criteriaQuery = builder.createQuery(TfUser.class);
+    @Override
+    public TfUser getUser(String username) {
+        TfUser user;
+        SessionFactory sessionFactory = HibernateUtil.getSession();
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<TfUser> criteriaQuery = builder.createQuery(TfUser.class);
 
-			Root<TfUser> root = criteriaQuery.from(TfUser.class);
+            Root<TfUser> root = criteriaQuery.from(TfUser.class);
 
-			criteriaQuery.select(root).where(builder.equal(root.get("tfUserUsername"), username));
+            criteriaQuery.select(root).where(builder.equal(root.get("tfUserUsername"), username));
 
-			Query<TfUser> query = session.createQuery(criteriaQuery);
+            Query<TfUser> query = session.createQuery(criteriaQuery);
 
-			try {
-				user = query.getSingleResult();
-				if(user.getTfRole() != null) {
-					if (user.getTfRole().getTfRoleName() != null){
-						Hibernate.initialize(user.getTfRole().getTfRoleName());
-					}
-				}
-				
-			} catch (NoResultException nre) {
-				user = new TfUser();
-			}
-		}
-		return user;
-	}
+            try {
+                user = query.getSingleResult();
+                if (user.getTfRole() != null) {
+                    if (user.getTfRole().getTfRoleName() != null) {
+                        Hibernate.initialize(user.getTfRole().getTfRoleName());
+                    }
+                }
 
-	@Override
-	public String getUserHash(TfUser user) {
-		
-		String hashpass = new String();
-		
-		Connection connection = null;
-		Statement stmt = null;
-		
-		try {
-			connection = jdbcUtil.getConnection();
+            } catch (NoResultException nre) {
+                user = new TfUser();
+            }
+        }
+        return user;
+    }
 
-			stmt = connection.createStatement();
+    @Override
+    public String getUserHash(TfUser user) {
 
-			String sql = "SELECT * FROM TF_USER WHERE TF_USER_ID=" + user.getTfUserId();
+        String hashpass = new String();
 
-			ResultSet rs = stmt.executeQuery(sql);
+        Connection connection = null;
+        Statement stmt = null;
 
-			if (rs.next()) {
+        try {
+            connection = jdbcUtil.getConnection();
 
-				hashpass = rs.getString("TF_HASHPASSWORD");
-			}
+            stmt = connection.createStatement();
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null) {
-					stmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return hashpass;
-	}
+            String sql = "SELECT * FROM TF_USER WHERE TF_USER_ID=" + user.getTfUserId();
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            if (rs.next()) {
+
+                hashpass = rs.getString("TF_HASHPASSWORD");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return hashpass;
+    }
 }

5e0fd123c2541dad5765eaf27ca69d7bfb03b81f
ryantimms <ryantimms92@gmail.com>
2017-11-17 11:27:06 -0500

    code smell fixes

--- TrackForce/src/main/webapp/test/spec/batchFeatureTest.js
+++ TrackForce/src/main/webapp/test/spec/batchFeatureTest.js
@@ -2,18 +2,17 @@
 
 // Test Suite for Batch Feature change
 describe('Test Batch Feature Suite', function() {
-
+	var mockScope={};
+	var controller;
 	// Before each test it will mock the main module we are using
 	beforeEach(angular.mock.module('mainApp'));
 	
 	// Here we setup the $httpBackend service from angular-mocks that mocks http requests
 	// Allows us to not depend on Rest Services to be setup to test http requests
-	beforeEach(angular.mock.inject(function($httpBackend, $rootScope) {
-		var backend = $httpBackend;
+	beforeEach(angular.mock.inject(function($httpBackend) {
 		var fromDate = new Date(123456789);
 		var toDate = new Date(123456789);
-		
-		$rootScope.backend.expect("GET", 'http://localhost:8080/TrackForce/track/batches/' + fromDate.getTime() + '/' + toDate.getTime()).respond(
+		$httpBackend.expect("GET", 'http://localhost:8080/TrackForce/track/batches/' + fromDate.getTime() + '/' + toDate.getTime()).respond(
 				[
 					{ "name": "SDET", "startdate": "hello", "enddate": "bye"},
 					{ "name": ".NET", "startdate": "hello", "enddate": "bye"},
@@ -23,8 +22,8 @@ describe('Test Batch Feature Suite', function() {
 
 	// Inject dependencies needed for controller
 	beforeEach(angular.mock.inject(function($rootScope, $controller, $http) {
-		var mockScope = $rootScope.$new();
-		var controller = $controller('batchCtrl', {
+		mockScope = $rootScope.$new();
+		controller = $controller('batchCtrl', {
 			$scope: mockScope,
 			$http: $http
 		});
@@ -34,8 +33,6 @@ describe('Test Batch Feature Suite', function() {
 	describe('Test functions for $http methods', function() {
 		it('Test getBatches()', function() {
 			expect(mockScope.getBatches).not.toBeNull();
-			console.log('hello there');
-			console.log(mockScope.batches);
 		});
 
 	});

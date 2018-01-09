ce9dad934ab8f8471ce4145203c68fc266bd04cf
Michael Tseng <michael-tseng@outlook.com>
2018-01-08 21:52:41 -0500

    Edited endpt url for the EC2

--- NGTrackForce/src/app/components/login/login.component.ts
+++ NGTrackForce/src/app/components/login/login.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  //url to REST endpoint; port may need to be changed according to local settings
-  public loginUrl = 'http://localhost:3000/TrackForce/track/user/submit';
+  //url to REST endpoint on EC2
+  public loginUrl = 'http://ec2-54-209-164-28.compute-1.amazonaws.com:8080/TrackForce/track/user/submit';
 
   constructor() { }
 

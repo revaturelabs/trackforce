e8c9ed8d0eae478236b4cef2247330ef648860ad
Alex Peterson <amp1993@gmail.com>
2018-01-09 13:00:01 -0500

    fixed imports

--- NGTrackForce/src/app/components/associate-list/associate-list.component.ts
+++ NGTrackForce/src/app/components/associate-list/associate-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AssociateService } from '../../../services/associates-service';
+import { AssociateService } from '../../services/associates-service';
 import { Associate } from '../../../models/Associate';
-import { ClientService } from '../../../services/clients-service';
+import { ClientService } from '../../services/clients-service';
 import { Client } from '../../../models/Client';
 
 /**

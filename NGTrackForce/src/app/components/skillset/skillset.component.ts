e621a46eb356f7d93051daabe4fbf13f79835838
Michael Warren <mwarren04011990@gmail.com>
2018-01-10 16:21:16 -0500

    added version-control flag,template to skillset component

--- NGTrackForce/src/app/components/skillset/skillset.component.ts
+++ NGTrackForce/src/app/components/skillset/skillset.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SelectedStatuses } from '../../services/selectedStatuses';
+import { AutoUnsubscribe } from '../../autoUnsubscribe';
 
 @Component({
   selector: 'app-skillset',
@@ -11,6 +12,7 @@ import { SelectedStatuses } from '../../services/selectedStatuses';
  * The skillset component
  * @author : Michael Warren
  */
+@AutoUnsubscribe
 export class SkillsetComponent implements OnInit {
   /**
    * The selected status 
@@ -25,6 +27,10 @@ export class SkillsetComponent implements OnInit {
    */
   private skillID : number;
   /**
+   * The flag that tells Angular, and the developer, whether or not ng2_chart dependency is actually being used
+   */
+  USE_NG2_CHART : boolean = true;
+  /**
    * The types of charts
    */
   public static readonly chartTypes = {
@@ -37,7 +43,7 @@ export class SkillsetComponent implements OnInit {
    */
   chartType = SkillsetComponent.chartTypes.BAR;
   /**
-   * The chart options
+   * The chart options, as a JavaScript-style object, and pre-initialized so as to DRY up our code...
    */
   chartOptions : {[k: string]: any} = {
     type : this.chartType,
@@ -78,19 +84,24 @@ export class SkillsetComponent implements OnInit {
     this.chartOptions.type = type;
     switch (type)
     {
+      // if type is either PIE or POLAR_AREA...
       case SkillsetComponent.chartTypes.PIE:
       case SkillsetComponent.chartTypes.POLAR_AREA:
+        // ... we're displaying the chart legend and on the right of the container
         this.chartOptions.legend = {
           display : true,
           position: 'right'
         };
+        // ... and getting rid of the scales ...
         if (this.chartOptions.scales) delete this.chartOptions.scales;
-        return;
+        break;
+      // otherwise, for BAR charts...
       case SkillsetComponent.chartTypes.BAR:
-
+        // ...we give no legend...
         this.chartOptions.legend = {
           display:false
         };
+        // ...but give scales...
         this.chartOptions.scales = 
         {
           yAxes: [
@@ -101,12 +112,15 @@ export class SkillsetComponent implements OnInit {
             }
           ]
         };
-        return;
+        break;
     }
     // it's a mock, for right now
     return type;
   }
   
+  /**
+   * Exposing SKILL_INFO in a safe way
+   */
   public static getSkillInfo()
   {
     return SkillsetComponent.SKILL_INFO;

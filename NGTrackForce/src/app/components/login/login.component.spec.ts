3acfeb6139a7dbfc1ae5d388d1f030ef1e091a18
Michael Tseng <michael-tseng@outlook.com>
2018-01-10 15:48:54 -0500

    Finished Karma tests

--- NGTrackForce/src/app/components/login/login.component.spec.ts
+++ NGTrackForce/src/app/components/login/login.component.spec.ts
@@ -3,8 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { environment } from '../../../environments/environment';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
-import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -14,7 +15,7 @@ describe('LoginComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       providers: [AuthenticationService],
-      imports: [FormsModule]
+      imports: [FormsModule, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   }));
--- NGTrackForce/src/app/services/authentication/authentication.service.spec.ts
+++ NGTrackForce/src/app/services/authentication/authentication.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import 'rxjs/add/operator/map';
 
 import { AuthenticationService } from './authentication.service';
@@ -10,12 +10,12 @@ import { AuthenticationService } from './authentication.service';
 describe('AuthenticationService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthenticationService],
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule],
+      providers: [AuthenticationService]
     });
   });
 
-  it('should be created', inject([AuthenticationService], (service: AuthenticationService) => {
+  it('should be created', inject([HttpTestingController, AuthenticationService], (httpMock: HttpTestingController, service: AuthenticationService) => {
     expect(service).toBeTruthy();
   }));
 });

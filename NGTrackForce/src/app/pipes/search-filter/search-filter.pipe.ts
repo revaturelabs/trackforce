b33b1cef7b80e91d35388618f242505a230be4f6
Han Jung <hsjungcr@gmail.com>
2018-01-10 14:03:14 -0500

    pipe added

---
+++ NGTrackForce/src/app/pipes/search-filter/search-filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+/**
+ * @author Han Jung
+ * @description Filters out array of things based on search criteria
+ */
+@Pipe({
+  name: 'searchFilter'
+})
+export class SearchFilterPipe implements PipeTransform {
+
+  transform(items: any[], searchText: string): any[] {
+    if(!items) return [];
+    if(!searchText) return items;
+
+    searchText=searchText.toLowerCase();
+    return items.filter( it => {
+      return it.toLowerCase().includes(searchText);
+    });
+  }
+
+}

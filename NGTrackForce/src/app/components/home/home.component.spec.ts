1cffc6cf93755e393c6793f48afd4b393f81cd45
Sean Vaeth <seanaveth@gmail.com>
2018-01-11 10:53:18 -0500

    updated test

--- NGTrackForce/src/app/components/home/home.component.spec.ts
+++ NGTrackForce/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { RequestService } from '../../services/request.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [RequestService]
     })
     .compileComponents();
   }));

74e5a5c5a9c9a05c7cede1bc6a074c210773a015
pibe2 <pibe2@illinois.edu>
2018-01-11 14:59:03 -0500

    fixing tests

--- NGTrackForce/src/app/components/batch-list/batch-list.component.spec.ts
+++ NGTrackForce/src/app/components/batch-list/batch-list.component.spec.ts
@@ -1,30 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BatchListComponent } from './batch-list.component';
+import {BatchListComponent} from './batch-list.component';
 import {ChartsModule} from 'ng2-charts';
 import {AuthenticationService} from '../../services/authentication/authentication.service';
 import {BatchService} from '../../services/batch/batch.service';
+import {RouterModule} from '@angular/router';
+import {appRoutes} from '../../routing/routes';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {HomeComponent} from '../home/home.component';
+import {ClientMappedComponent} from '../client-mapped/client-mapped.component';
+import {AssociateListComponent} from '../associate-list/associate-list.component';
+import {LoginComponent} from '../login/login.component';
 
-describe('BatchListComponent', () => {
-  let component: BatchListComponent;
-  let fixture: ComponentFixture<BatchListComponent>;
+TestBed.configureTestingModule({
+  declarations: [
+    BatchListComponent,
+    HomeComponent,
+    ClientMappedComponent,
+    AssociateListComponent,
+    LoginComponent
+  ],
+  providers: [
+    BatchService,
+    AuthenticationService,
+    /* todo add interceptor */
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    ChartsModule
+  ]
+}).compileComponents()
+  .then((data) => {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BatchListComponent ],
-      providers: [ BatchService, AuthenticationService /* todo add interceptor */ ],
-      imports: [ChartsModule]
-    })
-    .compileComponents();
-  }));
+    describe('BatchListComponent', () => {
+      let component: BatchListComponent;
+      let fixture: ComponentFixture<BatchListComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BatchListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      beforeEach(() => {
+        fixture = TestBed.createComponent(BatchListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+      it('should create', () => {
+        expect(component).toBeTruthy();
+      });
+    });
+
+  }, console.log);

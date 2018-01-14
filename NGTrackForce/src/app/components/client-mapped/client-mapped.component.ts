d6141df746b59d6de4a54b9bc6223f85cf43b85b
Evan West <evan-west@hotmail.com>
2018-01-14 08:05:13 -0500

    Comments for component and service.

--- NGTrackForce/src/app/components/client-mapped/client-mapped.component.ts
+++ NGTrackForce/src/app/components/client-mapped/client-mapped.component.ts
@@ -7,20 +7,60 @@ import { ClientMappedService } from '../../services/client-mapped-service/client
   styleUrls: ['./client-mapped.component.css']
 })
 export class ClientMappedComponent implements OnInit {
-  public selectedStatus='Selected'; //Pass this in from the service
+
+  /* ===========================
+  Member variables
+  ============================ */
+  /**
+   * @description selectedStatus is printed above the displayed chart to indicate 
+   * the marketing status for the data being displayed. 
+   */
+  public selectedStatus; //Pass this in from the service
   
+  /**
+   * @description statusID is initialized with the value stored in the URL.
+   * It is used to grab mapped associates from the database based on current 
+   * marketing status. 
+   */
   public statusID;
+
+  /**
+   * @description chartType determines the type of chart displayed to the user: bar, pie, or polarArea charts
+   */
   public chartType: String;
+
+  /**
+   * ??? Check this ???
+   * @description clientMappedLabels holds the client names fetched from the database
+   */
   public clientMappedLabels: string[];
+
+  /**
+   * 
+   * @description clientMappedData holds the number of associate under current status ID 
+   * fetched from the database
+   */
   public clientMappedData: number[];
-  public clientMappedDataSet: any;
+
+  /**
+   * @description chartLegend is a boolean determining if a legend is displayed. 
+   * Legends should be displayed for pie and polarArea charts, but not for bar charts
+   * (i.e. bar chart legends act differently than the pie and polarArea chart legends)
+   */
   public chartLegend:boolean;
+
+  /**
+   * @description chartOption contians configuration options for whatever type of chart
+   * is being displayed. 
+   */
   public chartOptions:any;
 
-  constructor(private clientMappedService: ClientMappedService) {
-    var id=window.location.href.split("form-comp/")[1];
-  }
+  /* ===========================
+  Methods
+  ============================ */
+  constructor(private clientMappedService: ClientMappedService) {}
 
+  //Run on initialization
   ngOnInit() {
     console.log("Inisde ngOnInit");
 
@@ -29,21 +69,46 @@ export class ClientMappedComponent implements OnInit {
     
     console.log("about to make a http");
 
-    //Fetch the statusId to be displayed from the URL
+    //Fetch from the URL the statusId for the data to be displayed 
     //For now, parse out the desired number
     //To-Do: Use "Activated Routes" to fetch the value 
     this.statusID = window.location.href.split('client-mapped/')[1];
+
+    //Initialize 'selectedStatus' to correct string. 
+    if(this.statusID == 0) {
+      this.selectedStatus = "Training";
+    } else if(this.statusID == 1) {
+      this.selectedStatus = "Some Status Type";
+    } else if(this.statusID == 2) {
+      this.selectedStatus = "Some Status Type";
+    } else if(this.statusID == 3) {
+      this.selectedStatus == "Some Status Type";
+    }
+
     console.log("this.statusID = " + this.statusID);
+
+    //HTTP request to fetch data. See client-mapped-service 
     this.clientMappedService.getAssociatesByStatus(this.statusID).subscribe( data => {
       console.log("made http request");
+      
+      /*
+      Store the data from the http request in temporary objects.
+      In order for the property binding refresh on clientMappedData 
+      and clientMappedLabels to take affect, they need to be set 
+      equal to an object. (i.e. clientMappedData.push(...)and 
+      clientMappedLabels.push(...) does not trigger property binding
+      and does not display data).
+      */
       let temp_clientMappedLabels: string[] = [];
       let temp_clientMappedData: number[] = [];
       console.log(data);
 
+      //Loop over 'data' and extract fetched information
       for(let d in data) {
         const temp_name = data[d].name;
         const temp_count = data[d].count;
         if(temp_count > 0){
+          //Check if the fetched name is empty
           if(data[d].name == ""){
             console.log('Name is empty');
             temp_clientMappedLabels.push("Empty Name");
@@ -53,6 +118,8 @@ export class ClientMappedComponent implements OnInit {
           temp_clientMappedData.push(data[d].count);
         }
       }
+
+      //Trigger property binding
       this.clientMappedData = temp_clientMappedData;
       this.clientMappedLabels = temp_clientMappedLabels;
     })
@@ -83,7 +150,7 @@ export class ClientMappedComponent implements OnInit {
 	}
 
 
-   // events
+  //Placeholder for events. Current application specifications does not dictate any actions
   public chartClicked(e:any):void {
     console.log(e);
   }
--- NGTrackForce/src/app/services/client-mapped-service/client-mapped-service.ts
+++ NGTrackForce/src/app/services/client-mapped-service/client-mapped-service.ts
@@ -9,12 +9,16 @@ export class ClientMappedService {
   constructor(private http: HttpClient) { }
 
   /**
-   * 
-   * @param statusId 
+   * @function getAssociatesByStatus
+   * @description Make an http request to the /track/client webservice, fetching mapped associates
+   * with the given marketing status. 
+   * @param statusId Contains the marketing status id used to fetch data
    */
   getAssociatesByStatus(statusId) {
     console.log("Inside ClientMappedService");
     console.log("statudId: " + statusId);
+    
+    //environment.url contains the url to the webservice. See the environment import, above.
     return this.http.get(environment.url + 'TrackForce/track/client/'+ statusId);
   }
 }

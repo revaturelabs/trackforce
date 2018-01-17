a2352e81b90f5992901f0ce5432e5d3d1e2191d0
Sean Vaeth <seanaveth@gmail.com>
2018-01-16 20:39:16 -0500

    more fixes

--- NGTrackForce/src/app/constants/theme.constants.ts
+++ NGTrackForce/src/app/constants/theme.constants.ts
@@ -9,4 +9,5 @@ export class ThemeConstants {
   static SKILL_COLORS: Array<Color> = [{ backgroundColor: ['#004d47', '#00cffa', '#52958b', '#008dcb', '#b2dbd5', '#6eb5c0', '#006c84', '#113743'] }];
   static BATCH_COLORS: Array<Color> = [{ backgroundColor: ['#004d47', '#00cffa', '#52958b', '#008dcb', '#b2dbd5', '#6eb5c0', '#006c84', '#113743'] }];
   static BAR_COLORS: Array<Color> = [{ backgroundColor: '#e85410'}, { backgroundColor: '#59504c' }];
+  static BATCH_DETAILS_COLORS: Array<Color> = [{ backgroundColor: ['#ff8d3f', '#514f4f', '#68a225', '#506d2f', '#324851', '#b3de81', '#7da3a1', '#a2c523', '#6e6702', '#2e4600', '#004d47', '#00cffa', '#52958b', '#008dcb', '#b2dbd5', '#6eb5c0', '#006c84', '#113743'] }];
 }

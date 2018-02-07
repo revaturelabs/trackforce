ec0343c4f40bc5dee3b1f78755a47fcbe952338b
soduor <stepoduor@gmail.com>
2018-02-07 06:41:17 -0800

    Associate List POM complete

--- TrackForce/src/test/java/com/revature/test/admin/pom/AssociateListTab.java
+++ TrackForce/src/test/java/com/revature/test/admin/pom/AssociateListTab.java
@@ -5,72 +5,206 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class AssociateListTab {
-	
+
 	static WebElement element = null;
-	
-	//Associate List tab on navbar
-	public static WebElement tab(WebDriver d) {
-		element = d.findElement(By.xpath("/html/body/app/div/app-associate-list/app-navbar/nav/div/ul[1]/li[4]/a "));
-		return element;
+
+	// Associate List tab on navbar
+	public static boolean tab(WebDriver d) {
+		
+		try {
+			element = d
+					.findElement(By.xpath("/html/body/app/div/app-associate-list/app-navbar/nav/div/ul[1]/li[4]/a "));
+			element.click();
+			return true;
+		} catch (Throwable e) {
+			return false;
+		}
 	}
-	
-	
-	//********************TEXT FIELDS ************************
-	
-	//Search By Text input field
-	public static WebElement searchByTextInputField(WebDriver d) {
-		element = d.findElement(By.xpath("//*[@id=\"name\"]"));
-		return element;
+
+	// ********************TEXT FIELDS ************************
+
+	// Search By Text input field
+	public static boolean searchByTextInputField(WebDriver d) {
+
+		try {
+			element = d.findElement(By.xpath("//*[@id=\"name\"]"));
+			element.sendKeys("Test Name");
+			return true;
+		} catch (Throwable e) {
+			return false;
+		}
 	}
-	
-	
-	//*************DROP DOWNS ****************************
-	
-	//Marketing Status drop down
-	public static WebElement marketingStatusDropDown(WebDriver d) {
-		element = d.findElement(By.xpath("//*[@id=\"mStatus\"]"));
-		return element;
+
+	// *************DROP DOWNS ****************************
+
+	// Marketing Status drop down
+	public static boolean marketingStatusDropDown(WebDriver d) {
+
+		try {
+			element = d.findElement(By.xpath("//*[@id=\"mStatus\"]"));
+			element.sendKeys("MAPPED: TRAINING");
+			return true;
+		} catch (Throwable e) {
+			return false;
+		}
 	}
-	
-	//Curriculum drop down
-	public static WebElement curriculumDropDown(WebDriver d) {
+
+	// Curriculum drop down
+	public static boolean curriculumDropDown(WebDriver d) {
+		
+		try {
 		element = d.findElement(By.xpath("//*[@id=\"curriculum\"]"));
-		return element;
+		element.sendKeys("Java");
+		return true;
+		}catch (Throwable e) {
+			return false;
+		}
 	}
-	
-	//Client drop down
-	public static WebElement clientDropDown(WebDriver d) {
+
+	// Client drop down
+	public static boolean clientDropDown(WebDriver d) {
+		
+		try {
 		element = d.findElement(By.xpath("//*[@id=\"client\"]"));
-		return element;
+		element.sendKeys("Accenture");
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
 	}
-	
-	//Update by Marketing Status drop down
-	public static WebElement updateByMarketingStatusDropDown(WebDriver d) {
+
+	// Update by Marketing Status drop down
+	public static boolean updateByMarketingStatusDropDown(WebDriver d) {
+		
+		try {
 		element = d.findElement(By.xpath("//*[@id=\"uStatus\"]"));
-		return element;
+		element.sendKeys("MAPPED: TRAINING");
+		return true;
+		}catch (Throwable e) {
+			return false;
+		}
 	}
-	
-	//Client drop down
-	public static WebElement clientDropDrown(WebDriver d) {
+
+	// Client drop down
+	public static boolean clientDropDrown(WebDriver d) {
+		
+		try {
 		element = d.findElement(By.xpath("//*[@id=\"uclient\"]"));
-		return element;
+		element.sendKeys("Accenture");
+		return true;
+		}catch (Throwable e) {
+			return false;
+		}
 	}
-	
-	//******************** BUTTONS **************************
-	
-	//Update button
-	public static WebElement updateButton(WebDriver d) {
+
+	// ******************** BUTTONS **************************
+
+	// Update button
+	public static boolean updateButton(WebDriver d) {
+		
+		try {
 		element = d.findElement(By.xpath("//*[@id=\"submit\"] "));
-		return element;
+		element.click();
+		return true;
+		}catch(Throwable e) {
+		return false;
+		}
 	}
-	
-	//****************** CHECKBOXES **************************
-	
-	//Edit check box for first row in the field
-	public static WebElement editCheckBox(WebDriver d) {
+
+	// ****************** CHECKBOXES **************************
+
+	// Edit check box for first row in the field
+	public static boolean editCheckBox(WebDriver d) {
+		
+		try {
 		element = d.findElement(By.xpath("//*[@id=\"70\"]"));
-		return element;
+		element.click();
+		return true;
+		}catch(Throwable e) {
+		return false;
+		}
 	}
-	
+
+	// ****************FILTER BUTTONS ******************************
+
+	public static boolean editFilter(WebDriver d) {
+		
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[1] "));
+		element.click();
+		return true;
+		} catch(Throwable e) {
+			return false;
+		}
+	}
+
+	public static boolean associateIdFilter(WebDriver d) {
+		
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[2] "));
+		element.click();
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
+	}
+
+	public static boolean firstNameFilter(WebDriver d) {
+		
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[3] "));
+		element.click();
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
+	}
+
+	public static boolean lastNameFilter(WebDriver d) {
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[4] "));
+		element.click();
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
+	}
+
+	public static boolean marketingStatusFilter(WebDriver d) {
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[5] "));
+		element.click();
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
+	}
+
+	public static boolean curriculumFilter(WebDriver d) {
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[6] "));
+		element.click();
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
+	}
+
+	public static boolean clientFilter(WebDriver d) {
+		try {
+		element = d
+				.findElement(By.xpath("/html/body/app/div/app-associate-list/div/div[2]/div[1]/table/thead/tr/th[7] "));
+		element.click();
+		return true;
+		}catch(Throwable e) {
+			return false;
+		}
+	}
 
 }

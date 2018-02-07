54c951cab88b32d13366d2eec7d87f4bac64d3d4
soduor <stepoduor@gmail.com>
2018-02-07 12:44:00 -0800

    Added method stubs to associate list cukes

--- TrackForce/src/test/java/com/revature/test/admin/cukes/AssociatesListCukes.java
+++ TrackForce/src/test/java/com/revature/test/admin/cukes/AssociatesListCukes.java
@@ -1,5 +1,91 @@
 package com.revature.test.admin.cukes;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class AssociatesListCukes {
+	
+	@Given("^I want to search by text$")
+	public static void i_want_to_search_by_text() throws Throwable {
+
+	}
+
+	@When("^I input text in the search by text input field$")
+	public static void i_input_text_in_the_search_by_text_input_field() throws Throwable {
+
+	}
+
+	@Then("^The item is retrieved$")
+	public static void the_item_is_retrieved() throws Throwable {
+
+	}
+
+	@When("^I select a marketing status value from the marketing status drop drown$")
+	public static void i_select_a_marketing_status_value_from_the_marketing_status_drop_drown() throws Throwable {
+
+	}
+
+	@Then("^the table is filtered by that marketing status$")
+	public static void the_table_is_filtered_by_that_marketing_status() throws Throwable {
+
+	}
+
+	@Given("^I want to filter by curriculum$")
+	public static void i_want_to_filter_by_curriculum() throws Throwable {
+
+	}
+
+	@When("^I select a curriculum value from the curriculum drop down$")
+	public static void i_select_a_curriculum_value_from_the_curriculum_drop_down() throws Throwable {
+
+	}
+
+	@Then("^the table is filtered by that curriculum$")
+	public static void the_table_is_filtered_by_that_curriculum() throws Throwable {
+
+	}
+
+	@Given("^I want to filter by client$")
+	public static void i_want_to_filter_by_client() throws Throwable {
+
+	}
+
+	@When("^I select a client value from the client drop down$")
+	public static void i_select_a_client_value_from_the_client_drop_down() throws Throwable {
+
+	}
+
+	@Then("^the table is filtered by that client$")
+	public static void the_table_is_filtered_by_that_client() throws Throwable {
+
+	}
+
+	@Given("^I want to edit an associates status$")
+	public static void i_want_to_edit_an_associates_status() throws Throwable {
+
+	}
+
+	@When("^I click an associate's checkbox$")
+	public static void i_click_an_associate_s_checkbox() throws Throwable {
+
+	}
+
+	@When("^I select a update by marketing status value from the update by marketing status drop down$")
+	public static void i_select_a_update_by_marketing_status_value_from_the_update_by_marketing_status_drop_down()
+			throws Throwable {
+
+	}
+
+	@When("^I click the update button$")
+	public static void i_click_the_update_button() throws Throwable {
+
+	}
+
+	@Then("^the associate's information is updated$")
+	public static void the_associate_s_information_is_updated() throws Throwable {
+
+	}
 
 }

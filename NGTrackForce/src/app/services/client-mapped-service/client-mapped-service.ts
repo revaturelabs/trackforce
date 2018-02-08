53d2179adbf45ee3aef9bf2274d3c21291455908
DFM <dfmai@ucdavis.edu>
2018-02-07 22:40:00 -0500

    fixed import in client-mapped services for httpclient

--- NGTrackForce/src/app/services/client-mapped-service/client-mapped-service.ts
+++ NGTrackForce/src/app/services/client-mapped-service/client-mapped-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http/';
+import { HttpClient } from '@angular/common/http';
 import { environment}  from '../../../environments/environment'
 /**
  * This service is used to handle data for the ClientMappedComponent
@@ -11,13 +11,13 @@ export class ClientMappedService {
   /**
    * @function getAssociatesByStatus
    * @description Make an http request to the /track/client webservice, fetching mapped associates
-   * with the given marketing status. 
+   * with the given marketing status.
    * @param statusId Contains the marketing status id used to fetch data
    */
   getAssociatesByStatus(statusId) {
     console.log("Inside ClientMappedService");
     console.log("statudId: " + statusId);
-    
+
     //environment.url contains the url to the webservice. See the environment import, above.
     return this.http.get(environment.url + 'TrackForce/track/associates/client/'+ statusId);
   }

a9a4dcc197d2ea11e523f4993220d162fa905eb2
Ofer Rosen <oferrosenn@gmail.com>
2018-02-09 15:01:24 -0500

    modified DatabaseServices, removed DBLoaderUtil,DBPopulatorUtil,DBLoaderUtilTest,TestDBLoaderUtil

--- TrackForce/src/test/java/com/revature/test/TestSuite.java
+++ TrackForce/src/test/java/com/revature/test/TestSuite.java
@@ -1,7 +1,6 @@
 package com.revature.test;
 
 import com.revature.utils.HibernateUtil;
-import com.revature.utils.TestDBLoaderUtil;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -11,7 +10,7 @@ public class TestSuite extends BaseTest {
     @BeforeSuite
     public void setupHibernate() throws SQLException {
         // session factory already init in BaseTest CCtor
-        new TestDBLoaderUtil().populate();
+        //new TestDBLoaderUtil().populate();
     }
 
     @AfterSuite

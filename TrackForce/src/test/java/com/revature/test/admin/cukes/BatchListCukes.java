9dfd560f5107b1fa85d395e0809664664aa4a9b0
Tuan Pham <tuandpham75@gmail.com>
2018-02-12 09:40:03 -0500

    Minor changes before push

--- TrackForce/src/test/java/com/revature/test/admin/cukes/BatchListCukes.java
+++ TrackForce/src/test/java/com/revature/test/admin/cukes/BatchListCukes.java
@@ -1,12 +1,17 @@
 package com.revature.test.admin.cukes;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import com.revature.test.admin.pom.BatchListTab;
 import com.revature.test.utils.WaitToLoad;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -41,7 +46,6 @@ public class BatchListCukes {
 		}
 		try {
 			BatchListTab.getFirstBatchName(wd).click();
-			//BatchListTab.executeJSClick(wd, WaitToLoad.findDynamicElement(wd, By.xpath("/html/body/app/div/app-batch-list/div/div[2]/div[1]/table/tbody/tr[1]/td[1]/a"), 30));
 		} catch (Throwable e) {
 			System.out.println("Failed to click first batch name");
 		}
@@ -80,21 +84,39 @@ public class BatchListCukes {
 		}
 
 		try {
-			// Verify that the IDs grabbed from first batch are all in the correct batch by looking at the associate list
-			List<WebElement> associateIDs = BatchListTab.grabAssociatesIDs(wd);
+//			// Verify that the IDs grabbed from first batch are all in the correct batch by looking at the associate list
+//			List<WebElement> associateIDs = BatchListTab.grabAssociatesIDs(wd);
+//			List<WebElement> associateRows = BatchListTab.grabAssociatesBatchInfo(wd);
+//			// Search through the List of Associates taken from Associate List Tab
+//			for (int i = 0; i < associateIDs.size(); i++) {
+//				// If the IDs match
+//				if (IDs.containsKey(associateIDs.get(i).getText())) {
+//					// and if the batch name is in the same row
+//					if (associateRows.get(i).getText().contains(batchName)) {
+//						IDs.remove(associateIDs.get(i).getText());
+//					}
+//				}
+//			}
+//			// Return true when the map is empty; All of the IDs in the map were found in the Associate List with same batch name
+//			return IDs.isEmpty();
+			
+	        // remove potential dups
+	        // increase lookup speed to O(1)
 			List<WebElement> associateRows = BatchListTab.grabAssociatesBatchInfo(wd);
-			// Search through the List of Associates taken from Associate List Tab
-			for (int i = 0; i < associateIDs.size(); i++) {
-				// If the IDs match
-				if (IDs.containsKey(associateIDs.get(i).getText())) {
-					// and if the batch name is in the same row
-					if (associateRows.get(i).getText().contains(batchName)) {
-						IDs.remove(associateIDs.get(i).getText());
-					}
-				}
-			}
-			// Return true when the map is empty; All of the IDs in the map were found in the Associate List with same batch name
-			return IDs.isEmpty();
+	        // remove potential dups
+	        // increase lookup speed to O(1)
+	        Set<String> listHash = new HashSet<String>();
+	        for (WebElement val : associateRows)
+	        {
+	            listHash.add(val.getText());
+	        }
+
+	        for (HashMap.Entry<String, String> entry : IDs.entrySet())
+	        {
+	            if ( !((listHash.contains(entry.getValue())) && (listHash.contains(entry.getKey()))) )
+	                return false;
+	        }
+	        return true;
 		} catch (Throwable e) {
 			System.out.println("Can't grab associate list ID's");
 			return false;

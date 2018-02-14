44d30afcd68a9ab89e6b175d5644b11daa7053f7
Antonio Dotson <antoniomdotson@gmail.com>
2018-02-14 15:36:36 -0500

    Update the Junction DAO information

--- TrackForce/src/main/java/com/revature/dao/JunctionDao.java
+++ TrackForce/src/main/java/com/revature/dao/JunctionDao.java
@@ -14,16 +14,19 @@ import com.revature.model.ClientInfo;
 
 public interface JunctionDao {
 
-	//@return an array of batch names and technologies for each batch
+	//@return a list of batch names and technologies for each batch
   List  GET_ALL_TECH_PER_BATCH() throws IOException;
 	
-	//@return an array of batch_name and a count of associates for a given technology 
+	//@return a list of batch_name and a count of associates for a given technology 
 	List GET_COUNT_OF_ALL_BATCH_PER_TECH(int tech_id);
 	List GET_COUNT_OF_ALL_BATCH_PER_TECH(String techname);
 	
 	
-	//@return an array of batch names, and an account of associates. The associate account would
-	//need to added to get the correct sum of all associates within a given timeframe.
-	List GET_COUNT_OF_ALL_BATCH_PER_DATE(Date date,String techname);
+	/*@return a list of batch names, and an account of associates. The associate account would
+	 need to be added to get the correct sum of all associates within a given time frame that has a
+	specific skill.
+	This method takes two dates and the technology name  */
+	
+	List GET_COUNT_OF_ALL_BATCH_PER_DATE(Date date1, Date date2, String techname);
 	
 }
--- TrackForce/src/main/java/com/revature/dao/JunctionDaoImpl.java
+++ TrackForce/src/main/java/com/revature/dao/JunctionDaoImpl.java
@@ -61,16 +61,17 @@ public class JunctionDaoImpl implements JunctionDao {
 	
 
 	@Override
-	public List GET_COUNT_OF_ALL_BATCH_PER_DATE(Date date, String techname) {
+	public List GET_COUNT_OF_ALL_BATCH_PER_DATE(Date date1,Date date2, String techname) {
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
         	String sql = "Select a.tf_batch_name, count(p.TF_ASSOCIATE_ID) as Associate FROM ADMIN.tf_batch a left\r\n" + 
         			"join ADMIN.tf_batch_junction tf on tf.tf_batch_id = a.tf_batch_id left join ADMIN.tf_tech s on s.TF_TECH_ID = tf.TF_TECH_ID left join ADMIN.tf_associate p\r\n" + 
-        			"on p.TF_BATCH_ID = tf.TF_BATCH_ID where TO_CHAR(a.tf_batch_end_date, 'YYYY-MM-DD') <= TO_Char(:date, 'YYYY-MM-DD')AND s.tf_tech_name= :techname group by a.TF_BATCH_NAME;\r\n" + 
-        			"\r\n"; 
+        			"on p.TF_BATCH_ID = tf.TF_BATCH_ID where TO_CHAR(a.tf_batch_end_date, 'YYYY-MM-DD') BETWEEN TO_Char(:date1, 'YYYY-MM-DD')AND TO_Char(:date2, 'YYYY-MM-DD') AND \r\n" + 
+        			"s.tf_tech_name =:techname group by a.TF_BATCH_NAME;\r\n" ; 
         			
 		    Query query = session.createNativeQuery(sql);
-		    query.setParameter(0, date);
-		    query.setParameter(1, techname);
+		    query.setParameter(0, date1);
+		    query.setParameter(1, date2);
+		    query.setParameter(2, techname);
         	List query_results = query.list();
             return query_results;
         } catch (NoResultException nre) {

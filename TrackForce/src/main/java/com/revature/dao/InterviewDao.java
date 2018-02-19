1ac4163cc8daf7754d8f2dbe96768e7ab706bf02
Andrew Crenwelge <andrewcrenwelge@gmail.com>
2018-02-18 22:09:04 -0500

    More work on interviews

--- TrackForce/src/main/java/com/revature/dao/InterviewDao.java
+++ TrackForce/src/main/java/com/revature/dao/InterviewDao.java
@@ -1,18 +1,16 @@
 package com.revature.dao;
 
 import java.io.IOException;
-
 import java.util.Map;
 import java.util.Set;
 
-import com.revature.entity.TfInterview;
-import com.revature.entity.TfTech;
 import com.revature.model.InterviewInfo;
+import com.revature.request.model.InterviewFromClient;
 
 public interface InterviewDao {
 	
 	public Map<Integer, InterviewInfo> getInterviewsByAssociate(int associateId) throws IOException;
     public Map<Integer, InterviewInfo> getAllInterviews();
     public Set<InterviewInfo> getInterviewFromCache();
-    public void addInterviewForAssociate(int associateid, InterviewInfo ii);
+    public void addInterviewForAssociate(int associateid, InterviewFromClient ifc);
 }

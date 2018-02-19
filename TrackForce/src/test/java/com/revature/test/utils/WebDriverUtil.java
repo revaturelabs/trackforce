5e27fa66628e870d188fbc25a368901b752bbfae
wtg1890 <william.gentry02@gmail.com>
2018-02-19 10:45:35 -0500

    added browser support for both windows and linux

--- TrackForce/src/test/java/com/revature/test/utils/WebDriverUtil.java
+++ TrackForce/src/test/java/com/revature/test/utils/WebDriverUtil.java
@@ -1,9 +1,11 @@
 package com.revature.test.utils;
 
 import java.io.File;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class WebDriverUtil {
 
@@ -13,14 +15,33 @@ public class WebDriverUtil {
 		
 	}
 	
+//	public static WebDriver getChromeDriver() {
+//		if (chromeDriver == null) {
+//			File f1 = new File("src/main/resources/chromedriver.exe");
+//			System.setProperty("webdriver.chrome.driver", f1.getAbsolutePath());
+//			return new ChromeDriver();
+//		}
+//		else 
+//			return chromeDriver;
+//	}
+	
+	
 	public static WebDriver getChromeDriver() {
 		if (chromeDriver == null) {
-			File f1 = new File("src/main/resources/chromedriver.exe");
-			System.setProperty("webdriver.chrome.driver", f1.getAbsolutePath());
-			return new ChromeDriver();
-		}
-		else 
+			File f = null;
+			if (System.getenv("PATH").contains("/home/ec2-user/")) {
+				f = new File("src/main/resources/chromedriver");
+				System.out.println("Chrome driver path: " + f.getAbsolutePath());
+				System.setProperty("webdriver.chrome.driver", f.getAbsolutePath());
+				return new ChromeDriver();
+			} else {
+				f = new File("src/main/resources/chromedriver.exe");
+				System.out.println("Chrome driver path:"+f.getAbsolutePath());
+				System.setProperty("webdriver.chrome.driver", f.getAbsolutePath());
+				return new ChromeDriver();
+			}
+		} else 
 			return chromeDriver;
-	}
+		}
 	
 }

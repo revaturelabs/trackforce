8b18a6334aa60c1cfe01aa8e9e13f421e32c0930
DFM <dfmai@ucdavis.edu>
2018-02-20 11:18:51 -0500

    changed path in chromedriver

--- TrackForce/src/test/java/com/revature/test/utils/WebDriverUtil.java
+++ TrackForce/src/test/java/com/revature/test/utils/WebDriverUtil.java
@@ -30,7 +30,7 @@ public class WebDriverUtil {
 		if (chromeDriver == null) {
 			File f = null;
 			if (System.getenv("PATH").contains("/home/ec2-user/")) {
-				f = new File("src/main/resources/chromedriver");
+				f = new File("/home/ec2-user/.jenkins/workspace/TrackForce Server/TrackForce/src/main/resources/chromedriver");
 				System.out.println("Chrome driver path: " + f.getAbsolutePath());
 				System.setProperty("webdriver.chrome.driver", f.getAbsolutePath());
 				return new ChromeDriver();

c9529573613578a0780c4f4fd7fbeec2bf369b01
RobinPierre <robin.pierre16@outlook.com>
2018-02-20 14:36:50 -0500

    Fixed SonarQube smells

--- TrackForce/src/main/java/com/revature/runnable/PSDCacheRunner.java
+++ TrackForce/src/main/java/com/revature/runnable/PSDCacheRunner.java
@@ -62,7 +62,6 @@ public class PSDCacheRunner implements Runnable {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException ex) {
-				ex.printStackTrace();
 				LogUtil.logger.error(ex);
 			}
 		}

dc52e95c3a1971bd270079ad2ce5918abae0d8f9
Ian <kieldro@gmail.com>
2018-05-23 14:13:53 -0400

    removes package-lock and adds to ignore file. Adds new method to batch resource.

--- TrackForce/src/main/java/com/revature/services/BatchesService.java
+++ TrackForce/src/main/java/com/revature/services/BatchesService.java
@@ -10,67 +10,81 @@ import com.revature.dao.BatchDaoHibernate;
 import com.revature.model.AssociateInfo;
 import com.revature.model.BatchInfo;
 
-public class BatchesService{
+public class BatchesService {
 
-    private BatchDao batchDao;
+	private BatchDao batchDao;
 
-    public BatchesService() {
-        this.batchDao = new BatchDaoHibernate();
-    }
+	public BatchesService() {
+		this.batchDao = new BatchDaoHibernate();
+	}
 
-    public BatchesService(BatchDao batchDao) {
-    	this.batchDao = batchDao;
-    }
+	public BatchesService(BatchDao batchDao) {
+		this.batchDao = batchDao;
+	}
 
-    /**
-     * Get all batches
-     *
-     * @return - A list of the batch info. Batch info contains batch name, client
-     * name, batch start date, and batch end date.
-     * @throws IOException
-     */
-    public Set<BatchInfo> getAllBatches() {
-    	return batchDao.getAllBatches();
-    }
+	/**
+	 * Get all batches
+	 *
+	 * @return - A list of the batch info. Batch info contains batch name, client
+	 *         name, batch start date, and batch end date.
+	 * @throws IOException
+	 */
+	public Set<BatchInfo> getAllBatches() {
+		return batchDao.getAllBatches();
+	}
 
-    public List<BatchInfo> getAllBatchesSortedByDate() {
-        return batchDao.getBatchesSortedByDate();
-    }
+	public List<BatchInfo> getAllBatchesSortedByDate() {
+		return batchDao.getBatchesSortedByDate();
+	}
 
-    public BatchInfo getBatchById(Integer id) {
-    	return batchDao.getBatchById(id);
-    }
+	public Set<BatchInfo> getBatchesByCurri(String c) {
+		Set<BatchInfo> batches = batchDao.getAllBatches();
+		// remove all batches not equal to curriculum
+		for (BatchInfo b : batches) {
+			if(! b.getCurriculumName().equals(c))
+				batches.remove(b);
+		}
+		return batches;
+	}
 
-    /**
-     * Gets all batches that are running within a given date range
-     *
-     * @param fromdate - the starting date of the date range
-     * @param todate   - the ending date of the date range
-     * @return - A list of the batch info. Batch info contains batch name, client
-     * name, batch start date, and batch end date.
-     * @throws IOException
-     */
-    public List<BatchInfo> getBatches(Long fromdate, Long todate) {
-        List<BatchInfo> batches = getAllBatchesSortedByDate();
+	public BatchInfo getBatchById(Integer id) {
+		return batchDao.getBatchById(id);
+	}
+
+	/**
+	 * Gets all batches that are running within a given date range
+	 *
+	 * @param fromdate
+	 *            - the starting date of the date range
+	 * @param todate
+	 *            - the ending date of the date range
+	 * @return - A list of the batch info. Batch info contains batch name, client
+	 *         name, batch start date, and batch end date.
+	 * @throws IOException
+	 */
+	public List<BatchInfo> getBatches(Long fromdate, Long todate) {
+		List<BatchInfo> batches = getAllBatchesSortedByDate();
 		List<BatchInfo> sublist = new LinkedList<BatchInfo>();
-        for (BatchInfo bi : batches) {
-            if (bi.getStartLong() != null && bi.getEndLong() != null && bi.getEndLong() >= fromdate && bi.getEndLong() <= todate) sublist.add(bi);
-        }
-        return sublist;
-    }
+		for (BatchInfo bi : batches) {
+			if (bi.getStartLong() != null && bi.getEndLong() != null && bi.getEndLong() >= fromdate
+					&& bi.getEndLong() <= todate)
+				sublist.add(bi);
+		}
+		return sublist;
+	}
 
+	/**
+	 * Gets the information of the associates in a particular batch
+	 *
+	 * @param batchIdStr
+	 *            - the name of a batch that is in the database
+	 * @return - A list of the lists of associate info. Associate info contains id,
+	 *         first name, last name, and marketing status.
+	 * @throws IOException
+	 */
+	public Set<AssociateInfo> getAssociatesForBranch(Integer id) {
+		return batchDao.getBatchAssociates(id);
 
-    /**
-     * Gets the information of the associates in a particular batch
-     *
-     * @param batchIdStr - the name of a batch that is in the database
-     * @return - A list of the lists of associate info. Associate info contains id,
-     * first name, last name, and marketing status.
-     * @throws IOException
-     */
-    public Set<AssociateInfo> getAssociatesForBranch(Integer id) {
-    	return batchDao.getBatchAssociates(id);
-
-    }
+	}
 
 }
--- TrackForce/src/test/java/com/revature/test/services/BatchesServiceTest.java
+++ TrackForce/src/test/java/com/revature/test/services/BatchesServiceTest.java
@@ -17,132 +17,128 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.*;
 
-public class BatchesServiceTest{
+public class BatchesServiceTest {
+	@Mock
+	private BatchDao mockBatchDao;
 
-    @Mock
-    private BatchDao mockBatchDao;
+	private BatchesService batchService;
+	private AssociateInfo assoc1, assoc2, assoc3;
+	private BatchInfo batch1, batch2, batch3;
+	private Set<BatchInfo> batchSet;
+	private List<BatchInfo> batchList;
+	private Set<AssociateInfo> associateSet;
 
-    private BatchesService batchService;
-    private AssociateInfo assoc1, assoc2, assoc3;
-    private BatchInfo batch1, batch2, batch3;
-    private Set<BatchInfo> batchSet;
-    private List<BatchInfo> batchList;
-    private Set<AssociateInfo> associateSet;
+	@BeforeTest
+	public void beforeAll() throws IOException {
+		MockitoAnnotations.initMocks(this);
 
-    @BeforeTest
-    public void beforeAll() throws IOException {
-        MockitoAnnotations.initMocks(this);
+		// creates 3 associates
+		assoc1 = createAssocInfo(1, 1, "b1", 1, "c1", 1, "s1", 1, "c1");
+		assoc2 = createAssocInfo(2, 2, "b2", 2, "c2", 2, "s2", 2, "c2");
+		assoc3 = createAssocInfo(3, 3, "b3", 3, "c3", 3, "s3", 3, "c3");
 
-//        Map<Integer, BatchInfo> mockBatchMap = new HashMap();
-        assoc1 = createAssocInfo(1, 1, "b1", 1, "c1", 1, "s1", 1, "c1");
-        assoc2 = createAssocInfo(2, 2, "b2", 2, "c2", 2, "s2", 2, "c2");
-        assoc3 = createAssocInfo(3, 3, "b3", 3, "c3", 3, "s3", 3, "c3");
+		batch1 = createBatchInfo(1, "b1", "c1", 0, 100);
+		batch1.setAssociates(new HashSet<>(Arrays.asList(assoc1)));
 
-        batch1 = createBatchInfo(1, "b1", "c1", 0, 100);
-        batch1.setAssociates(new HashSet<>(Arrays.asList(assoc1)));
+		batch2 = createBatchInfo(2, "b2", "c2", 100, 200);
+		batch2.setAssociates(new HashSet<>(Arrays.asList(assoc2)));
 
-        batch2 = createBatchInfo(2, "b2", "c2", 100, 200);
-        batch2.setAssociates(new HashSet<>(Arrays.asList(assoc2)));
+		batch3 = createBatchInfo(3, "b3", "c3", 200, 300);
+		batch3.setAssociates(new HashSet<>(Arrays.asList(assoc3)));
 
-        batch3 = createBatchInfo(3, "b3", "c3", 200, 300);
-        batch3.setAssociates(new HashSet<>(Arrays.asList(assoc3)));
+		// mockBatchMap.put(batch1.getId(), batch1);
+		// mockBatchMap.put(batch2.getId(), batch2);
+		// mockBatchMap.put(batch3.getId(), batch3);
 
-//        mockBatchMap.put(batch1.getId(), batch1);
-//        mockBatchMap.put(batch2.getId(), batch2);
-//        mockBatchMap.put(batch3.getId(), batch3);
-        
-        batchSet = new TreeSet<>();
-        batchSet.add(batch1);
-        
-        batchList = new ArrayList<>();
-        batchList.add(batch1);
-        batchList.add(batch2);
-        batchList.add(batch3);
-        
-        associateSet = new TreeSet<>();
-        associateSet.add(assoc1);
+		batchSet = new TreeSet<>();
+		batchSet.add(batch1);
 
-        Mockito.when(mockBatchDao.getAllBatches()).thenReturn(batchSet);
-        Mockito.when(mockBatchDao.getBatchesSortedByDate()).thenReturn(batchList);
-        Mockito.when(mockBatchDao.getBatchById(Matchers.anyInt())).thenReturn(batch1);
-        Mockito.when(mockBatchDao.getBatchAssociates(Matchers.anyInt())).thenReturn(associateSet);
-        
+		batchList = new ArrayList<>();
+		batchList.add(batch1);
+		batchList.add(batch2);
+		batchList.add(batch3);
 
-    }
+		associateSet = new TreeSet<>();
+		associateSet.add(assoc1);
 
-    @BeforeMethod
-    public void beforeEach() {
-        batchService = new BatchesService(mockBatchDao);
-    }
+		Mockito.when(mockBatchDao.getAllBatches()).thenReturn(batchSet);
+		Mockito.when(mockBatchDao.getBatchesSortedByDate()).thenReturn(batchList);
+		Mockito.when(mockBatchDao.getBatchById(Matchers.anyInt())).thenReturn(batch1);
+		Mockito.when(mockBatchDao.getBatchAssociates(Matchers.anyInt())).thenReturn(associateSet);
+	}
 
-    //old test for a method that isn't used
-//    @Test(enabled = false)
-//    public void testGetBatchChartInfo() throws Exception {
-//        List<BatchInfo> actualChartInfo = batchService.getBatchChartInfo(0L, 1L);
-//        Assert.assertEquals(actualChartInfo.size(), 1);
-//        Assert.assertTrue(actualChartInfo.contains(batch1));
-//    }
+	@BeforeMethod
+	public void beforeEach() {
+		batchService = new BatchesService(mockBatchDao);
+	}
 
-    @Test(enabled = true)
-    public void testGetBatches() throws Exception {
-        List<BatchInfo> results = batchService.getBatches(new Long(50), new Long(250));
-        List<BatchInfo> comparator = new ArrayList<>();
-        comparator.add(batch1);
-        comparator.add(batch2);
-        Assert.assertEquals(comparator, results);
-    }
+	// old test for a method that isn't used
+	// @Test(enabled = false)
+	// public void testGetBatchChartInfo() throws Exception {
+	// List<BatchInfo> actualChartInfo = batchService.getBatchChartInfo(0L, 1L);
+	// Assert.assertEquals(actualChartInfo.size(), 1);
+	// Assert.assertTrue(actualChartInfo.contains(batch1));
+	// }
 
-    
-//These method have no logic. They just call a method and return the result
-//Are they worth implementing tests?
-    
-//    public void testGetAllBatches() throws Exception {
-//    	
-//    }
-//    
-//    public void getAllBatchesSortedByDate() throws Exception {
-//    	
-//    }
-//    
-//    public void getBatchById() throws Exception {
-//    	
-//    }
-//    
-//    @Test(enabled = false)
-//    public void testGetAssociatesForBranch() throws Exception {
-//        Set<AssociateInfo> associates = batchService.getAssociatesForBranch(1);
-//        Assert.assertTrue(associateSet, associates);
-//    }
+	@Test(enabled = true)
+	public void testGetBatches() throws Exception {
+		List<BatchInfo> results = batchService.getBatches(new Long(50), new Long(250));
+		List<BatchInfo> comparator = new ArrayList<>();
+		comparator.add(batch1);
+		comparator.add(batch2);
+		Assert.assertEquals(comparator, results);
+	}
 
-    AssociateInfo createAssocInfo(int id, int batchId, String batchName, int clientId, String clientName,
-                                  int msid, String msName, int currId, String currName) {
-        AssociateInfo assoc = new AssociateInfo();
-        assoc.setId(new Integer(id));
-        assoc.setBatchId(new Integer(batchId));
-        assoc.setBatchName(batchName);
-        assoc.setBid(new Integer(batchId));
-        assoc.setClid(new Integer(clientId));
-        assoc.setClientId(new Integer(clientId));
-        assoc.setClient(clientName);
-        assoc.setMarketingStatusId(new Integer(msid));
-        assoc.setMsid(new Integer(msid));
-        assoc.setMarketingStatus(msName);
-        assoc.setCurriculumId(new Integer(currId));
-        assoc.setCurriculumName(currName);
+	// These method have no logic. They just call a method and return the result
+	// Are they worth implementing tests?
 
-        return assoc;
-    }
+	// public void testGetAllBatches() throws Exception {
+	//
+	// }
+	//
+	// public void getAllBatchesSortedByDate() throws Exception {
+	//
+	// }
+	//
+	// public void getBatchById() throws Exception {
+	//
+	// }
+	//
+	// @Test(enabled = false)
+	// public void testGetAssociatesForBranch() throws Exception {
+	// Set<AssociateInfo> associates = batchService.getAssociatesForBranch(1);
+	// Assert.assertTrue(associateSet, associates);
+	// }
 
-    BatchInfo createBatchInfo(int id, String name, String curriculumName, long startLong, long endLong) {
-        BatchInfo info = new BatchInfo();
+	AssociateInfo createAssocInfo(int id, int batchId, String batchName, int clientId, String clientName, int msid,
+			String msName, int currId, String currName) {
+		AssociateInfo assoc = new AssociateInfo();
+		assoc.setId(new Integer(id));
+		assoc.setBatchId(new Integer(batchId));
+		assoc.setBatchName(batchName);
+		assoc.setBid(new Integer(batchId));
+		assoc.setClid(new Integer(clientId));
+		assoc.setClientId(new Integer(clientId));
+		assoc.setClient(clientName);
+		assoc.setMarketingStatusId(new Integer(msid));
+		assoc.setMsid(new Integer(msid));
+		assoc.setMarketingStatus(msName);
+		assoc.setCurriculumId(new Integer(currId));
+		assoc.setCurriculumName(currName);
 
-        info.setId(new Integer(id));
-        info.setBatchName(name);
-        info.setCurriculumName(curriculumName);
-        info.setStartLong(startLong);
-        info.setStartTs(new Timestamp(startLong));
-        info.setEndLong(endLong);
+		return assoc;
+	}
 
-        return info;
-    }
+	BatchInfo createBatchInfo(int id, String name, String curriculumName, long startLong, long endLong) {
+		BatchInfo info = new BatchInfo();
+
+		info.setId(new Integer(id));
+		info.setBatchName(name);
+		info.setCurriculumName(curriculumName);
+		info.setStartLong(startLong);
+		info.setStartTs(new Timestamp(startLong));
+		info.setEndLong(endLong);
+
+		return info;
+	}
 }

55efb359502e63c564f5ce871ed9b671b76cd384
unknown <mitch.ryan.harrison@gmail.com>
2018-05-25 13:09:50 -0400

    trying to add post to interview resource

--- TrackForce/src/main/java/com/revature/resources/InterviewResource.java
+++ TrackForce/src/main/java/com/revature/resources/InterviewResource.java
@@ -1,15 +1,32 @@
 package com.revature.resources;
 
 
+import static com.revature.utils.LogUtil.logger;
+
+import java.io.IOException;
+import java.util.Set;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.hibernate.HibernateException;
+
+import com.revature.model.BatchInfo;
+import com.revature.model.InterviewInfo;
 import com.revature.services.AssociateService;
 import com.revature.services.InterviewService;
 import com.revature.services.JWTService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 
 @Path("/")
@@ -19,6 +36,46 @@ public class InterviewResource {
 	private JWTService jService = new JWTService();
 	private static InterviewService is = new InterviewService();
 	
+	
+	@GET
+	public Response getAllInterviews() throws HibernateException, IOException {
+		//TODO handle exception
+		Set<InterviewInfo> interviews = is.getAllInterviews();
+		Status status = interviews == null || interviews.isEmpty() ? Status.NO_CONTENT : Status.OK;
+		System.out.println("inside get all interviews");
+		return Response.status(status).entity(interviews).build();
+	}
+	
+	@GET
+	@Path("/{interviewid}")
+	public Response getAssociateInterviews(@PathParam("associateid") Integer associateid,@HeaderParam("Authorization") String token) {
+		System.out.println(token);
+		Claims claims = null;
+		System.out.println("Before the try block");
+		try {
+			System.out.println("In the try block");
+			if (token == null) {
+				throw new UnsupportedJwtException("token null");
+			}
+			claims = jService.getClaimsFromToken(token);
+			System.out.println("Print claims " + claims);
+		
+		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException
+				| IllegalArgumentException | NullPointerException e) {
+			System.out.println("in the catch block");
+			e.printStackTrace();
+			return Response.status(403).build();
+		}
+		
+		if (claims.getId().equals("1")) {
+			Set<InterviewInfo> associateinfo = service.getInterviewsByAssociate(associateid);
+			return Response.ok(associateinfo).build();
+		} else {
+			return Response.status(403).build();
+		}
+	}
+
+	
 	@POST
 	public Response createInterview(@PathParam("associateid") int associateid) {
 	
@@ -26,8 +83,10 @@ public class InterviewResource {
 	return Response.status(201).build();
 	}
 	
+	
+	@GET 
 	@Path("/{interviewid}")
-	@GET Response getClientInterview(@PathParam("interviewid") int interviewid) {
+	public Response getClientInterview(@PathParam("interviewid") int interviewid) {
 		return Response.status(200).build();
 	
 	}

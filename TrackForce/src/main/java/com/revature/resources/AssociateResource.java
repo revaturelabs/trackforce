aeff5ba0ce8da5cd860c8f32f1f42f5daeaab7d0
Ian <kieldro@gmail.com>
2018-05-29 17:49:32 -0400

    merging with dev

--- TrackForce/src/main/java/com/revature/resources/AssociateResource.java
+++ TrackForce/src/main/java/com/revature/resources/AssociateResource.java
@@ -1,9 +1,8 @@
 package com.revature.resources;
 
-
+import static com.revature.utils.LogUtil.logger;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,8 +12,6 @@ import javax.persistence.StoredProcedureQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -24,7 +21,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import static com.revature.utils.LogUtil.logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -32,21 +28,11 @@ import org.hibernate.Transaction;
 import com.revature.dao.AssociateDaoHibernate;
 import com.revature.model.AssociateInfo;
 import com.revature.model.ClientMappedJSON;
-import com.revature.model.InterviewInfo;
 import com.revature.request.model.AssociateFromClient;
-import com.revature.request.model.InterviewFromClient;
 import com.revature.services.AssociateService;
-import com.revature.services.InterviewService;
 import com.revature.services.JWTService;
 import com.revature.utils.HibernateUtil;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureException;
-import io.jsonwebtoken.UnsupportedJwtException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -56,7 +42,7 @@ import io.swagger.annotations.ApiParam;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AssociateResource {
-	
+
 	private AssociateService service = new AssociateService();
 	private JWTService jService = new JWTService();
 
@@ -65,12 +51,8 @@ public class AssociateResource {
 	 * associate has no marketing status or curriculum, replaces them with blanks.
 	 * If associate has no client, replaces it with "None".
 	 *
-	 * The different user types and their ID's are 
-	 * Admin: 1
-	 * Trainer: 2
-	 * Sales/Delivery 3
-	 * Staging Manager 4
-	 * Associate 5
+	 * The different user types and their ID's are Admin: 1 Trainer: 2
+	 * Sales/Delivery 3 Staging Manager 4 Associate 5
 	 * 
 	 * @return A Response object with a list of TfAssociate objects.
 	 * @throws IOException
@@ -137,26 +119,29 @@ public class AssociateResource {
 		return Response.ok(mappedStats).build();
 	}
 
-//	@GET
-//	@Path("unmapped/{statusId}")
-//	public Response getUnmappedInfo(@PathParam("statusId") int statusId) {
-//		return Response.ok(service.getUnmappedInfo(statusId)).build();
-//	}
-//
-//	@GET
-//	@Path("{associateid}/interviews")
-//	public Response getAssociateInterviews(@PathParam("associateid") Integer associateid) {
-//		Set<InterviewInfo> associateinfo = service.getInterviewsByAssociate(associateid);
-//		return Response.ok(associateinfo).build();
-//	}
-//
-//	@POST
-//	@Path("{associateid}/interviews")
-//	public Response addAssociateInterview(@PathParam("associateid") Integer associateid, InterviewFromClient ifc) {
-//		InterviewService is = new InterviewService();
-//		is.addInterviewByAssociate(associateid, ifc);
-//		return Response.ok().build();
-//	}
+	// @GET
+	// @Path("unmapped/{statusId}")
+	// public Response getUnmappedInfo(@PathParam("statusId") int statusId) {
+	// return Response.ok(service.getUnmappedInfo(statusId)).build();
+	// }
+	//
+	// @GET
+	// @Path("{associateid}/interviews")
+	// public Response getAssociateInterviews(@PathParam("associateid") Integer
+	// associateid) {
+	// Set<InterviewInfo> associateinfo =
+	// service.getInterviewsByAssociate(associateid);
+	// return Response.ok(associateinfo).build();
+	// }
+	//
+	// @POST
+	// @Path("{associateid}/interviews")
+	// public Response addAssociateInterview(@PathParam("associateid") Integer
+	// associateid, InterviewFromClient ifc) {
+	// InterviewService is = new InterviewService();
+	// is.addInterviewByAssociate(associateid, ifc);
+	// return Response.ok().build();
+	// }
 
 	/**
 	 * Update the marketing status or client of an associate
@@ -235,13 +220,12 @@ public class AssociateResource {
 	 * return Response.ok().build(); }
 	 */
 
-	
-//	@Path("/interviews") 
-//	public InterviewResource getAllInterview() {
-//		return new InterviewResource();
-//	}
+	// @Path("/interviews")
+	// public InterviewResource getAllInterview() {
+	// return new InterviewResource();
+	// }
 
-	@ApiOperation(value = "returns all interviews for associate", notes= "Gets a list of all interviews for a specific associate.")
+	@ApiOperation(value = "returns all interviews for associate", notes = "Gets a list of all interviews for a specific associate.")
 	@Path("/{associateid}/interviews")
 	public InterviewResource addAssociateInterview() {
 		return new InterviewResource();

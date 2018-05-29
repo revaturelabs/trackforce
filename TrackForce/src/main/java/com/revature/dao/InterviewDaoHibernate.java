7223755c2e16809e29ea8095d2728e7e9d88349a
Ed-Mar <35546470+Ed-Mar@users.noreply.github.com>
2018-05-29 09:44:40 -0400

    Hold this

--- TrackForce/src/main/java/com/revature/dao/InterviewDaoHibernate.java
+++ TrackForce/src/main/java/com/revature/dao/InterviewDaoHibernate.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.NoResultException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -32,56 +33,56 @@ import com.revature.utils.PersistentStorage;
 public class InterviewDaoHibernate implements InterviewDao {
 
 	public Map<Integer, InterviewInfo> getAllInterviews() {
-        Map<Integer, InterviewInfo> techs = new HashMap<>();
+		Map<Integer, InterviewInfo> techs = new HashMap<>();
 		Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<TfInterview> cq = cb.createQuery(TfInterview.class);
-            Root<TfInterview> from = cq.from(TfInterview.class);
-            CriteriaQuery<TfInterview> all = cq.select(from);
-            Query<TfInterview> tq = session.createQuery(all);
-            return createInterviewMap(tq.getResultList());
-        } catch(Exception e) {
+		try {
+			CriteriaBuilder cb = session.getCriteriaBuilder();
+			CriteriaQuery<TfInterview> cq = cb.createQuery(TfInterview.class);
+			Root<TfInterview> from = cq.from(TfInterview.class);
+			CriteriaQuery<TfInterview> all = cq.select(from);
+			Query<TfInterview> tq = session.createQuery(all);
+			return createInterviewMap(tq.getResultList());
+		} catch (Exception e) {
 
-            LogUtil.logger.error(e);
-        }
-        finally {
-        	session.close();
+			LogUtil.logger.error(e);
+		} finally {
+			session.close();
 		}
 		return techs;
-    }
+	}
 
 	@Override
 	public Map<Integer, InterviewInfo> getInterviewsByAssociate(int associateId) throws IOException {
-		 Map<Integer, InterviewInfo> interviews = null;
-		 Session session = HibernateUtil.getSessionFactory().openSession();
-	        try {
-	            CriteriaBuilder builder = session.getCriteriaBuilder();
-	            CriteriaQuery<TfInterview> criteriaQuery = builder.createQuery(TfInterview.class);
-	            Root<TfInterview> root = criteriaQuery.from(TfInterview.class);
-	            criteriaQuery.select(root).where(builder.equal(root.get("tfAssociate"), associateId));
-	            Query<TfInterview> query = session.createQuery(criteriaQuery);
-	            return createInterviewMap(query.getResultList());
-	        } catch (NoResultException nre) {
-	            LogUtil.logger.error(nre);
-	        }
-	        finally {
-	        	session.close();
-			}
-			return interviews;
+		Map<Integer, InterviewInfo> interviews = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<TfInterview> criteriaQuery = builder.createQuery(TfInterview.class);
+			Root<TfInterview> root = criteriaQuery.from(TfInterview.class);
+			criteriaQuery.select(root).where(builder.equal(root.get("tfAssociate"), associateId));
+			Query<TfInterview> query = session.createQuery(criteriaQuery);
+			return createInterviewMap(query.getResultList());
+		} catch (NoResultException nre) {
+			LogUtil.logger.error(nre);
+		} finally {
+			session.close();
+		}
+		return interviews;
 	}
 
-	public Map<Integer, InterviewInfo> createInterviewMap(List<TfInterview> interviews){   // works in tandem with 'getInterviewByAssociate()' method
-        Map<Integer, InterviewInfo> map = new HashMap<>();
-        if (interviews != null) {
-            for (TfInterview tfi : interviews) {
-                map.put(tfi.getTfInterviewId(), Dao2DoMapper.map(tfi));
-            }
-        }
-        return map;
-    }
+	public Map<Integer, InterviewInfo> createInterviewMap(List<TfInterview> interviews) { // works in tandem with
+																							// 'getInterviewByAssociate()'
+																							// method
+		Map<Integer, InterviewInfo> map = new HashMap<>();
+		if (interviews != null) {
+			for (TfInterview tfi : interviews) {
+				map.put(tfi.getTfInterviewId(), Dao2DoMapper.map(tfi));
+			}
+		}
+		return map;
+	}
 
-	public Set<InterviewInfo> getInterviewFromCache(){
+	public Set<InterviewInfo> getInterviewFromCache() {
 		return PersistentStorage.getStorage().getInterviews();
 	}
 
@@ -89,9 +90,7 @@ public class InterviewDaoHibernate implements InterviewDao {
 		return PersistentStorage.getStorage().getInterviewsAsMap().get(id);
 	}
 
-
-
-	public void cacheAllInterviews(){
+	public void cacheAllInterviews() {
 		PersistentStorage.getStorage().setInterviews(new InterviewDaoHibernate().getAllInterviews());
 	}
 
@@ -109,25 +108,64 @@ public class InterviewDaoHibernate implements InterviewDao {
 			Integer id = Integer.parseInt(max.toBigInteger().toString()) + 1;
 			tfi.setTfInterviewId(id);
 			tfi.setTfAssociate(session.get(TfAssociate.class, associateid));
-			tfi.setTfInterviewDate(Timestamp.from(new Date(ifc.getInterviewDate()).toInstant()));
-			//tfi.setTfInterviewFeedback(ifc.getInterviewFeedback());
+			
+			// tfi.setTfInterviewFeedback(ifc.getInterviewFeedback());
 			tfi.setTfClient(session.get(TfClient.class, ifc.getClientId()));
 			tfi.setTfInterviewType(session.load(TfInterviewType.class, ifc.getTypeId()));
+			tfi.setTfInterviewDate(Timestamp.from(new Date(ifc.getInterviewDate()).toInstant()));
+			
+			//tfi.setTfAssociateFeedback(tfAssociateFeedback);
+			
+			
+			
+			/*
+			 * @Column(name = "TF_ASSOCIATE_FEEDBACK", length = 2000) private String
+			 * tfAssociateFeedback;
+			 * 
+			 * @Column(name = "TF_CLIENT_FEEDBACK", length = 2500) private String
+			 * tfClientFeedback;
+			 * 
+			 * @Column(name = "TF_JOB_DESCRIPTION", length = 2000) private String
+			 * tfJobDescription;
+			 * 
+			 * @Column(name = "TF_DATE_SALES_ISSUED") private Timestamp tfDateSalesIssued;
+			 * 
+			 * @Column(name = "TF_DATE_ASSOCIATE_ISSUED") private Timestamp
+			 * tfDateAssociateIssued;
+			 * 
+			 * @Column(name = "TF_IS_INTERVIEW_FLAGGED") private Integer
+			 * tfIsInterviewFlagged = 0;
+			 * 
+			 * @Column(name = "TF_FLAG_REASON", length = 300) private String tfFlagReason;
+			 * 
+			 * @Column(name = "TF_IS_CLIENT_FEEDBACK_VISIABLE") private Integer
+			 * tfIsClientFeedbackVisiable = 0;
+			 */
+
 			session.saveOrUpdate(tfi);
 			t1.commit();
-        } catch (NullPointerException e) {
-        	LogUtil.logger.error(e);
-        	if (t1 != null) {
-        		t1.rollback();
-        	}
-        } catch (Exception e) {
-        	LogUtil.logger.error(e);
-        	if (t1 != null) {
-        		t1.rollback();
-        	}
-        }
-        finally {
+		} catch (NullPointerException e) {
+			LogUtil.logger.error(e);
+			if (t1 != null) {
+				t1.rollback();
+			}
+		} catch (Exception e) {
+			LogUtil.logger.error(e);
+			if (t1 != null) {
+				t1.rollback();
+			}
+		} finally {
 			session.close();
 		}
+	}
+	/**
+	 * Send a Interview object and it gets put in the TF_Interview table
+	 * @Edboi
+	 */
+
+	@Override
+	public boolean createInterview(TfInterview parmInterview) {
+		
+		return false;
 	}
 }

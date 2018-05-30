c0765f2e23d3b04f736f9311929899bb17aa7827
Ian <kieldro@gmail.com>
2018-05-30 12:57:53 -0400

    BatchResourceTests status code

--- TrackForce/src/test/java/com/revature/test/resources/BatchTests.java
+++ TrackForce/src/test/java/com/revature/test/resources/BatchTests.java
@@ -2,21 +2,42 @@ package com.revature.test.resources;
 
 import static com.revature.utils.LogUtil.logger;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.model.AssociateInfo;
+
 /**
  * testNG tests for the BatchResource
+ * 
  * @author Ian Buitrago
  *
  */
 public class BatchTests {
 	@Test
-	public void testGetAllBatches() {
+	public void testGetAllBatches() throws IOException {
 		logger.info("Testing getAllBatches()...");
+		String URL = "http://localhost:8085/TrackForce/api/associates/1";
+		HttpUriRequest request = new HttpGet(URL);
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		int status = response.getStatusLine().getStatusCode();
 
-	     HttpUriRequest request = new HttpGet( "http://localhost:8085/TrackForce/api/batches/adam" );
+		Assert.assertEquals(status, HttpStatus.SC_OK);
 
-	     HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );  
+		String jsonMimeType = "application/json";
+		String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+		AssociateInfo events = new ObjectMapper().readValue(response.getEntity().getContent(), AssociateInfo.class);
+		
+//		Assert.assertEquals(jsonMimeType, mimeType);
+
 	}
 }

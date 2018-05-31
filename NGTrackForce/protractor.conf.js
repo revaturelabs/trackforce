a3841092db56acd97b7ad0ddb95eccd948a81a36
Nicholas McComb <nicholas.e.mccomb@gmail.com>
2018-05-31 10:48:27 -0400

    fixed merge conflict resolution error in protractor.conf

--- NGTrackForce/protractor.conf.js
+++ NGTrackForce/protractor.conf.js
@@ -23,9 +23,14 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  // baseUrl: 'http://52.207.66.231:4200/',
+  // directConnect: true,
+    //baseUrl: 'http://localhost:4200/',
+   baseUrl: 'http://34.227.178.103:8090/NGTrackForce/',
+   //for pipeline delpoyment
+     seleniumAddress: "http://127.0.0.1:4444/wd/hub",
+    // SELENIUM_PROMISE_MANAGER: false,
+    //
+    //
 
   framework: 'jasmine',
   jasmineNodeOpts: {

6197ab37d4143f56704a61c007906d03eb316ead
Ian <kieldro@gmail.com>
2018-05-31 19:07:54 -0400

    Resource tests now uses environmental variable TOMCAT_PORT so unit tests can run on any machine locally on a tomcat server with variable port numbers.

--- TrackForce/src/test/java/com/revature/test/resources/BatchTests.java
+++ TrackForce/src/test/java/com/revature/test/resources/BatchTests.java
@@ -21,8 +21,9 @@ import com.revature.services.JWTService;
 import com.revature.services.UserService;
 
 /**
- * testNG tests for the BatchResource
- * NOTE: server must be running
+ * testNG tests for the BatchResource against the a live server. NOTE: server
+ * must be running Set system wide variable TOMCAT_PORT to eg 8085 for linux/mac
+ * set in your IDE(STS) in testng run configuration
  * 
  * @author Ian Buitrago
  *
@@ -31,24 +32,32 @@ public class BatchTests {
 	JWTService jService;
 	UserService uService;
 	String token;
+	String domain;
 
 	@BeforeClass
-	public void testinit() {
-		try {
-			logger.info("testInit()...");
-			uService = new UserService(); // throws SQLException???
-			jService = new JWTService(); // throws SQLException???
-			token = jService.createToken("Ian", 1);
-			logger.info("token generated: " + token);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void init() {
+		logger.info("BatchTests.init()...");
+		
+		uService = new UserService();
+		jService = new JWTService(); 	// throws SQLException???
+		
+		String port = System.getenv().get("TOMCAT_PORT");
+		domain = "http://localhost:" + port + "/";
+		logger.info("	domain = " + domain);
+		
+		
+		token = jService.createToken("Ian", 1);
+		logger.info("token generated: " + token);
 	}
 
+	/**
+	 * Tests dummy resource. If it fails, the server may be off.
+	 */
 	@Test
 	public void testAdam() throws IOException {
 		logger.info("Testing adam()...");
-		String URL = "http://localhost:8085/TrackForce/api/batches/adam";
+		String URL = domain + "TrackForce/api/batches/adam";
+		logger.info("	URL = " + URL);
 
 		HttpUriRequest request = new HttpGet(URL);
 		HttpResponse response = HttpClientBuilder.create().build().execute(request);
@@ -58,9 +67,9 @@ public class BatchTests {
 	}
 
 	@Test(enabled = true)
-	public void testGetAllBatches1() throws ClientProtocolException, IOException {
-		logger.info("Testing getAllBatches1()...");
-		String URL = "http://localhost:8085/TrackForce/api/batches";
+	public void testGetAllBatches() throws ClientProtocolException, IOException {
+		logger.info("Testing getAllBatches()...");
+		String URL = domain + "TrackForce/api/batches";
 		HttpUriRequest request = new HttpGet(URL);
 		request.addHeader("Authorization", token);
 
@@ -71,10 +80,10 @@ public class BatchTests {
 	}
 
 	@Test(enabled = true)
-	public void testGetAllBatches2() {
-		logger.info("Testing getAllBatches2()...");
-		String URL = "http://localhost:8085/TrackForce/api/associates/1";
-		logger.info("	url = " + URL);
+	public void testGetAssociate() {
+		logger.info("Testing getAssociate()...");
+		String URL = domain + "TrackForce/api/associates/1";
+		logger.info("	URL = " + URL);
 		HttpUriRequest request = new HttpGet(URL);
 		request.addHeader("Authorization", token);
 		try {
@@ -82,16 +91,16 @@ public class BatchTests {
 			int status = response.getStatusLine().getStatusCode();
 
 			Assert.assertEquals(status, HttpStatus.SC_OK);
-			
+
 			String jsonMimeType = "application/json";
 			String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
 			Assert.assertEquals(jsonMimeType, mimeType);
-			
+
 			// body
-//			AssociateInfo associate = new ObjectMapper().readValue(response.getEntity().getContent(), 
-//					AssociateInfo.class);
-//			logger.info("	body: " + associate);
-			} catch (IOException e) {
+			AssociateInfo associate = new ObjectMapper().readValue(response.getEntity().getContent(),
+					AssociateInfo.class);
+			logger.info(" body: " + associate);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}

02f00588bb68f00da5ba77bace24bcdf2e0fbbcd
unknown <Michael.wharty@yahoo.com>
2018-06-04 20:56:36 -0400

    updating tests

--- NGTrackForce/e2e/test-associate/homepage-spec.js
+++ NGTrackForce/e2e/test-associate/homepage-spec.js
@@ -16,40 +16,29 @@ describe('Log into the website for associate list', function() {
 
 });
 
-describe('Head over to the My interviews page and do something', function() {
-    it('should click the Myvinterviews button', function() {
-        wd.findElement(by.xpath('/html/body/app/app-associate-view/app-navbar/nav/div/ul[1]/li[2]/a')).click();
-        expect(wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/h3')).getText()).toEqual('My Interviews');
+describe('Should test out the navbar', function() {
+    it('Should go to client tab', function() {
+        wd.findElement(by.css('[routerlink="/client-listing"]')).click();
     });
 
-    it('should toggle the create new form button', function() {
-        wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/button')).click();
+    it('Should go to batch tab', function() {
+        wd.findElement(by.css('[routerlink="/batch-listing"]')).click();
     });
 
-    it('should select first client in list and verify', function() {
-        client = wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[1]/div/select/option')).getText();
-        wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[1]/div/select/option')).click();
-        expect(wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[1]/div/select')).getAttribute('ng-reflect-model')).toEqual(client);
+    it('Should go to associates tab', function() {
+        wd.findElement(by.css('[routerlink="/associate-listing"]')).click();
     });
 
-    it('should select interview type, phone,  and verify', function() {
-        wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[3]/div/select/option[1]')).click();
-        expect(wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[3]/div/select')).getAttribute('ng-reflect-model')).toEqual("1");
-    });
-
-    it('should enter something in feedback', function() {
-        testText = "Something written by Test";
-        wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[4]/div/textarea')).sendKeys(testText);
-        expect(wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/div/div/form/div[4]/div/textarea')).getAttribute('ng-reflect-model')).toEqual(testText);
+    it('Should go to predictions tab', function() {
+        wd.findElement(by.css('[routerlink="/predictions"]')).click();
     });
     
-    it('should toggle the form button again to close it', function() {
-        wd.findElement(by.xpath('/html/body/app/app-myinterview-view/div/div/div/button')).click();
+    it('should go to interviews tab', function(){
+        wd.findElement(by.css('[routerlink="/interviews"]')).click();
     });
+});
 
-    it('should log out', function() {
-        wd.findElement(by.css('a[href*="/login"]')).click();  // xpath does not work on this logout button
-    });
-
+it('should log out' ,function() {
+    wd.findElement(by.css('[routerlink="/login"]')).click();
 });
 

09a798c744876cadfe38a6f16ffc374ae4f2a8bf
hpark12 <hyunjae.v4@gmail.com>
2018-06-05 11:00:36 -0400

    Changes to caching in UserDaoImpl.java so cache updates upon create user

--- TrackForce/src/main/java/com/revature/dao/UserDaoImpl.java
+++ TrackForce/src/main/java/com/revature/dao/UserDaoImpl.java
@@ -22,6 +22,15 @@ import static com.revature.utils.LogUtil.logger;
 
 public class UserDaoImpl implements UserDAO {
 
+    private AssociateDao associateDao;
+
+    public UserDaoImpl() {
+        associateDao = AssociateDaoHibernate.getInstance();
+    }
+
+    public UserDaoImpl(AssociateDao dao) {		// testing purposes
+        associateDao = dao;
+    }
 
     public TfUser getUser(String username) {
         TfUser user = null;
@@ -95,7 +104,7 @@ public class UserDaoImpl implements UserDAO {
             t1.commit();
 
             logger.info("Associate successfully created");
-
+            associateDao.cacheAllAssociates();
             return true;
 
         } catch (HibernateException e) {

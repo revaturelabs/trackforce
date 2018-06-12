0c9627ee0006c5fe9807c8a53f2582a0c5a55b3e
cameron <skaggs1995@gmail.com>
2018-06-12 13:38:16 -0400

    added some javadocs

--- TrackForce/src/main/java/com/revature/model/MarketingStatusInfo.java
+++ TrackForce/src/main/java/com/revature/model/MarketingStatusInfo.java
@@ -2,6 +2,26 @@ package com.revature.model;
 
 import java.io.Serializable;
 
+/*
+ * This class maps to the selected-status const in constants folder
+ *  id  marketing status
+	1	MAPPED: TRAINING
+	2	MAPPED: RESERVED
+	3	MAPPED: SELECTED
+	4	MAPPED: CONFIRMED
+	5	MAPPED: DEPLOYED
+	6	UNMAPPED: TRAINING
+	7	UNMAPPED: OPEN
+	8	UNMAPPED: SELECTED
+	9	UNMAPPED: CONFIRMED
+	10	UNMAPPED: DEPLOYED
+	11	DIRECTLY PLACED
+	12	TERMINATED
+ * 
+ * Reviewed by Cameron
+ * @since 6.18.06.12
+ * 
+ * */
 public class MarketingStatusInfo  implements Serializable, Comparable<MarketingStatusInfo> {
 	
 	private static final long serialVersionUID = 52811943506766514L;

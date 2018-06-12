bee3ec91a005b4a7803d05e510c4571ae6758a8e
sonam <sgmyns2013@gmail.com>
2018-06-12 13:07:10 -0400

    added compareTo tests

--- TrackForce/src/test/java/com/revature/test/junit/ClinetInfoTest.java
+++ TrackForce/src/test/java/com/revature/test/junit/ClinetInfoTest.java
@@ -2,21 +2,20 @@ package com.revature.test.junit;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import java.util.HashSet;
-
+import org.junit.Before;
 import org.junit.Test;
-
 import com.revature.model.AssociateInfo;
 import com.revature.model.ClientInfo;
 import com.revature.model.StatusInfo;
 
 public class ClinetInfoTest {
-
-	@Test
-	public void testClientInfoGetters() {
-		//Creates a new Object 
-		ClientInfo ci = new ClientInfo();
+	
+	ClientInfo ci = new ClientInfo();
+	
+	@Before
+	public void setUp() throws Exception {
+		
 		ci.setId(1);
 		ci.setStats(new StatusInfo());
 		ci.setTfAssociates(new HashSet<AssociateInfo>());
@@ -24,12 +23,17 @@ public class ClinetInfoTest {
 		ci.setTfClientName("John Doe");
 		ci.setTfInterviews(new HashSet<>());
 		ci.setTfPlacements(new HashSet<>());
-		
-	
+	}
+
+	@Test
+	public void testClientInfoGetters() {
+		//Creates a new Object 
 		assertTrue(ci.getStats() instanceof  StatusInfo);
 		assertTrue(ci.getTfAssociates() instanceof HashSet);
+		
 		assertTrue(ci.getTfClientName().equals("John Doe"));
 		assertFalse(ci.getTfClientName().equals("John"));
+		
 		assertTrue(ci.getTfInterviews() instanceof HashSet);
 		assertTrue(ci.getTfPlacements() instanceof HashSet);
 			
@@ -38,34 +42,47 @@ public class ClinetInfoTest {
 	@Test
 	public void testClientInfoSetters() {
 		
-		//Creates a new Object 
-		ClientInfo ci = new ClientInfo();
-		ci.setId(1);
-		ci.setStats(new StatusInfo());
-		ci.setTfAssociates(new HashSet<AssociateInfo>());
-		ci.setTfClientId(5);
-		ci.setTfClientName("John Doe");
-		ci.setTfInterviews(new HashSet<>());
-		ci.setTfPlacements(new HashSet<>());
-		
+		ClientInfo ci2 = new ClientInfo();
 		//sets new Value to check if the values were set properly
-		ci.setId(2);
-		ci.setStats(new StatusInfo("John"));
-		ci.setTfAssociates(new HashSet<AssociateInfo>());
-		ci.setTfClientId(6);
-		ci.setTfClientName("Jane Roe");
-		ci.setTfInterviews(new HashSet<>());
-		ci.setTfPlacements(new HashSet<>());
+		ci2.setId(2);
+		ci2.setStats(new StatusInfo("John"));
+		ci2.setTfAssociates(new HashSet<AssociateInfo>());
+		ci2.setTfClientId(6);
+		ci2.setTfClientName("Jane Roe");
+		ci2.setTfInterviews(new HashSet<>());
+		ci2.setTfPlacements(new HashSet<>());
 		
 		
-		assertTrue(ci.getStats() instanceof  StatusInfo);
-		assertTrue(ci.getTfAssociates() instanceof HashSet);
-		assertTrue(ci.getTfClientName().equals("Jane Roe"));
-		assertFalse(ci.getTfClientName().equals("Jane"));
-		assertTrue(ci.getTfInterviews() instanceof HashSet);
-		assertTrue(ci.getTfPlacements() instanceof HashSet);
+		assertTrue(ci2.getStats() instanceof  StatusInfo);
+		assertTrue(ci2.getTfAssociates() instanceof HashSet);
 		
+		assertTrue(ci2.getTfClientName().equals("Jane Roe"));
+		assertFalse(ci2.getTfClientName().equals("Jane"));
 		
+		assertTrue(ci2.getTfInterviews() instanceof HashSet);
+		assertTrue(ci2.getTfPlacements() instanceof HashSet);
+		
+	}
+	
+	@Test
+	public void testCompareTo() {
+		
+		//create an obj to compareTo
+		ClientInfo ci3 = new ClientInfo();
+		ci3.setTfClientId(5);
+		
+		//create an object to compareTo
+		ClientInfo ci4 = new ClientInfo();
+		ci4.setTfClientId(10);
+		
+		assertTrue(ci.compareTo(ci3) == 0);		//test for equal
+		assertFalse(ci.compareTo(ci3) == 5);
+		
+		assertTrue(ci.compareTo(ci4) < 0);		//test for negative
+		assertFalse(ci.compareTo(ci4) == 5);
+		
+		assertTrue(ci4.compareTo(ci) > 0);
+		assertFalse(ci4.compareTo(ci) == 6);	//test for positive
 	}
 
 
--- TrackForce/src/test/java/com/revature/test/junit/EndClientInfoTest.java
+++ TrackForce/src/test/java/com/revature/test/junit/EndClientInfoTest.java
@@ -23,19 +23,10 @@ public class EndClientInfoTest {
 		eci.setTfPlacements(new HashSet<PlacementInfo> ());
 	}
 	
-	@After 
-	public void setObjectToDefault() throws Exception {
-		
-		eci.setId(0);
-		eci.setTfEndClientName(null);
-		eci.setTfAssociates(null);
-		eci.setTfPlacements(null);
-		
-	}
 	
 	
 	@Test
-	public void TestEndClientInfoGetters(){
+	public void testEndClientInfoGetters(){
 		
 		assertTrue(eci.getId() == 1);
 		assertFalse(eci.getId() == 2);
@@ -46,21 +37,44 @@ public class EndClientInfoTest {
 	}
 	
 	@Test 
-	public void TestEndClientInfoSetters() {
+	public void testEndClientInfoSetters() {
+		
+		EndClientInfo eci2 = new EndClientInfo();
 		
 		// Sets new Value
-		eci.setId(2);
-		eci.setTfEndClientName("John Doe");
-		eci.setTfAssociates(new HashSet<AssociateInfo> ());		
-		eci.setTfPlacements(new HashSet<PlacementInfo> ());
+		eci2.setId(2);
+		eci2.setTfEndClientName("John Doe");
+		eci2.setTfAssociates(new HashSet<AssociateInfo> ());		
+		eci2.setTfPlacements(new HashSet<PlacementInfo> ());
 	
 		//checks if the setters were set properly
-		assertTrue(eci.getId() == 2);
-		assertFalse(eci.getId() == 3);
-		assertTrue(eci.getTfEndClientName().equals("John Doe"));
-		assertFalse(eci.getTfEndClientName().equals("John"));
-		assertTrue(eci.getTfAssociates() instanceof HashSet);		
-		assertTrue(eci.getTfPlacements() instanceof HashSet);	
+		assertTrue(eci2.getId() == 2);
+		assertFalse(eci2.getId() == 3);
+		assertTrue(eci2.getTfEndClientName().equals("John Doe"));
+		assertFalse(eci2.getTfEndClientName().equals("John"));
+		assertTrue(eci2.getTfAssociates() instanceof HashSet);		
+		assertTrue(eci2.getTfPlacements() instanceof HashSet);	
+	}
+	
+	@Test
+	public void testCompareTo() {
+		
+		EndClientInfo eci3 = new EndClientInfo();
+		eci3.setId(1);
+		
+		EndClientInfo eci4 = new EndClientInfo();
+		eci4.setId(5);
+		
+		assertTrue(eci.compareTo(eci3) == 0);			//test for equality
+		assertFalse(eci.compareTo(eci3) == 1);
+		
+		assertTrue(eci.compareTo(eci4) < 0 );			//test for negative
+		assertFalse(eci.compareTo(eci4) == 1);
+		
+		assertTrue(eci4.compareTo(eci) > 0);			//test for positive
+		assertFalse(eci4.compareTo(eci) == 10);
+				
+		
 	}
 	
 }

b0b262be9fdf937f744ad9f2b67ba898a48cad89
Kirk Legarda <kirk.legarda@gmail.com>
2018-06-12 13:45:22 -0400

    Update models to match back-end models.

---
+++ NGTrackForce/src/app/models/trainer.model.ts
@@ -0,0 +1,15 @@
+/**
+ * Model information for trainer user.
+ * @author Kirk
+ * @since 6.18.6.12
+ */
+
+export class Trainer{
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  isApproved: 0;
+
+}
+

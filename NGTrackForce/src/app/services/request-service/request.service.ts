9f781a412ec55125ceb4bb81fced37d035601039
amelia <a.max.dunn@gmail.com>
2018-06-16 19:51:08 -0400

    deleted prediction service, added type boolean to all methods in request service

--- NGTrackForce/src/app/services/request-service/request.service.ts
+++ NGTrackForce/src/app/services/request-service/request.service.ts
@@ -21,20 +21,20 @@ export class RequestService {
     */
   constructor(private http: HttpClient) { }
 
-  public populateDB(): Observable<any> {
-    return this.http.get(this.trackPath + 'database/populateDB');
+  public populateDB(): Observable<boolean> {
+    return this.http.get<boolean>(this.trackPath + 'database/populateDB');
   }
 
-  public populateDBSF(): Observable<any> {
-    return this.http.get(this.trackPath + 'database/populateDBSF');
+  public populateDBSF(): Observable<boolean> {
+    return this.http.get<boolean>(this.trackPath + 'database/populateDBSF');
   }
 
-  public deleteDB(): Observable<any> {
-    return this.http.delete(this.trackPath + 'database/deleteFromDB');
+  public deleteDB(): Observable<boolean> {
+    return this.http.delete<boolean>(this.trackPath + 'database/deleteFromDB');
   }
 
-  public getStatuses(): Observable<any> {
-    return this.http.get(this.trackPath + 'marketing');
+  public getStatuses(): Observable<boolean> {
+    return this.http.get<boolean>(this.trackPath + 'marketing');
   }
 
 }

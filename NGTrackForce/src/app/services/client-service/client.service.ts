c9a5ccc966806c75290842f149b54cbe693fbe17
amelia <a.max.dunn@gmail.com>
2018-06-16 18:45:08 -0400

    the endpoints in the client service match the endpoints in the client resource, none were added or removed

--- NGTrackForce/src/app/services/client-service/client.service.ts
+++ NGTrackForce/src/app/services/client-service/client.service.ts
@@ -2,28 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment'
+import { Client } from '../../models/client.model';
 
 /**
  * @author Han Jung
- * @description methods for grabbing data from api for client list
+ * @description methods for grabbing client data
  */
 
 @Injectable()
 export class ClientService {
-  private url: string = environment.url + 'TrackForce/';
+  private baseURL: string = environment.url + 'TrackForce/clients';
 
   constructor(private http: HttpClient) { }
 
-  // get all Client objects
-  getAllClients(): Observable<any> {
-    return this.http.get(this.url + 'clients'); // previously /info
+  /**
+   * 
+   * Get a list of all of the clients
+   */
+  getAllClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.baseURL); 
   }
 
-  /** Client Object by id
-    *@param {number} clientId
-    */
-  getOneClient(clientId: number): Observable<any> {
-    return this.http.get(this.url + 'clients/' + clientId);
+  /** 
+   * 
+   * Client Object by id
+   * @param {number} clientId
+   */
+  getOneClient(clientId: number): Observable<Client> {
+    return this.http.get<Client>(this.baseURL + "/" + clientId);
   }
 
 }

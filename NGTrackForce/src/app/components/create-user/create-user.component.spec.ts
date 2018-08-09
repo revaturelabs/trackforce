3194ed354c743ebbd75c77b2995911265a6c7c96
Andrew Duffey <andrewduffey@outlook.com>
2018-08-08 19:02:28 -0500

    fixes create user component jasmine test failure to create
    Added correct User initialization in beforeAll()

--- NGTrackForce/src/app/components/create-user/create-user.component.spec.ts
+++ NGTrackForce/src/app/components/create-user/create-user.component.spec.ts
@@ -20,10 +20,7 @@ describe('CreateUserComponent', () => {
 
   // setup service mocks
   beforeAll(() => {
-    let user: User;
-    user.token = 'mockToken';
-    user.username = 'mockUser';
-    user.role = 1;
+    let user = new User("mockUser", "mockPassword", 1, 0, 0, "mockToken");
     spyOn(testAuthService, 'getUser').and.returnValue(user);  // needed by navbar
   });
 

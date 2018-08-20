7234a64180da078ccdc857d46a4c8ed4a992634f
Eric <opaqueradiance@gmail.com>
2018-08-20 16:29:30 -0500

    changed karma.js back to deployment settings

--- NGTrackForce/karma.conf.js
+++ NGTrackForce/karma.conf.js
@@ -39,9 +39,9 @@ module.exports = function (config) {
        ]
      }
    },
-    // browsers: ['ChromeHeadless'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    // browsers: ['Chrome'],
     // singleRun is true so it'll work on the pipeline, please don't change it <3
-    singleRun: false    //TODO: change this back to true before deploying.
+    singleRun: true    //TODO: change this back to true before deploying.
   });
 };

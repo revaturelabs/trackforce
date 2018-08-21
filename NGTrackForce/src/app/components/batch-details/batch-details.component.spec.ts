a540325e1359dc2bbb2dbf2468f8a996e29b2b68
nisfornature <etaoinshrdluwho@gmail.com>
2018-08-21 10:29:33 -0500

    fixed merge issues in batch-details

--- NGTrackForce/src/app/components/batch-details/batch-details.component.spec.ts
+++ NGTrackForce/src/app/components/batch-details/batch-details.component.spec.ts
@@ -6,23 +6,29 @@ import { ChartsModule } from 'ng2-charts';
 import { User } from '../../models/user.model';
 import { AuthenticationService } from '../../services/authentication-service/authentication.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormComponent } from '../form-component/form.component';
 import { SkillsetComponent } from '../skillset/skillset.component';
 import { Batch } from '../../models/batch.model';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import { RequestService } from '../../services/request-service/request.service';
-import { User } from '../../models/user.model';
-
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockActivatedRoute } from '../associate-view/associate-view.component.spec';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+
+import { BatchService } from '../../services/batch-service/batch.service';
+import { Ng2OrderPipe } from 'ng2-order-pipe';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BatchDetailsComponent', () => {
   let component: BatchDetailsComponent;
   let fixture: ComponentFixture<BatchDetailsComponent>;
   const testAuthService: AuthenticationService = new AuthenticationService(null, null, null);
+  const testBatchService: BatchService = new BatchService(null);
+  let user = new User('mockUser', 'mockPassword', 1, 0, 0, 'mockTokent');
 
   let router = {
     navigate: jasmine.createSpy('navigate')
@@ -98,20 +104,19 @@ describe('BatchDetailsComponent', () => {
     }
   });
 
-    it('should contain associates if loaded', () => {
-      if (component.isDataReady && !component.isDataEmpty) {
-        expect(component.associates).toBeTruthy();
-      }
-    });
+  it('should contain associates if loaded', () => {
+    if (component.isDataReady && !component.isDataEmpty) {
+      expect(component.associates).toBeTruthy();
+    }
+  });
 
-    it('goToFormComponent() should navigate to the formcomponent', () => {
-      expect(component.goToFormComponent(1)).toBeTruthy();
-    });
+  it('goToFormComponent() should navigate to the formcomponent', () => {
+    expect(component.goToFormComponent(1)).toBeTruthy();
+  });
 
-    it('getMapStatusBatch() should fetch data and data should be ready.', () => {
-      component.getMapStatusBatch()
-      expect(component.isDataEmpty).toBeFalsy;
-      expect(component.isDataReady).toBeTruthy;
-    });
+  it('getMapStatusBatch() should fetch data and data should be ready.', () => {
+    component.getMapStatusBatch()
+    expect(component.isDataEmpty).toBeFalsy;
+    expect(component.isDataReady).toBeTruthy;
   });
 });

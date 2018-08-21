564f30de1b4a8e9c99a4cbd636e34fa1f2ba344f
Logan Brewer <logan211@live.com>
2018-08-21 11:47:22 -0500

    added login component tests

--- NGTrackForce/src/app/components/login/login.component.spec.ts
+++ NGTrackForce/src/app/components/login/login.component.spec.ts
@@ -23,6 +23,9 @@ import { InterviewService } from '../../services/interview-service/interview.ser
 import { ClientService } from '../../services/client-service/client.service';
 import { BatchService } from '../../services/batch-service/batch.service';
 import { TrainerService } from '../../services/trainer-service/trainer.service';
+import { User } from '../../models/user.model';
+import { Trainer } from '../../models/trainer.model';
+import { Associate } from '../../models/associate.model';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -116,6 +119,7 @@ describe('LoginComponent', () => {
   it('if restrictions for username AND password are invalid, special error message', () => {
     component.username = '!@#invalidASDDAS*****()()~`notsurewhatsinvalid    ...';
     component.password = '!@#invalidASDDAS*****()()~`notsurewhatsinvalid    ...';
+    component.cpassword = '!@#invalidASDDAS*****()()~`notsurewhatsinvalid    ...';
     component.createUser();
     expect(component.errMsg).toBeTruthy();
   });
@@ -123,6 +127,7 @@ describe('LoginComponent', () => {
   it('if restrictions for username are invalid, special error message', () => {
     component.username = '!@#invalidASDDAS*****()()~`notsurewhatsinvalid    ...';
     component.password = 'good';
+    component.cpassword = 'good';
     component.createUser();
     expect(component.errMsg).toBeTruthy();
   });
@@ -130,19 +135,46 @@ describe('LoginComponent', () => {
   it('if restrictions for password are invalid, special error message', () => {
     component.username = 'good';
     component.password = '!@#invalidASDDAS*****()()~`notsurewhatsinvalid    ...';
+    component.cpassword = '!@#invalidASDDAS*****()()~`notsurewhatsinvalid    ...';
     component.createUser();
     expect(component.errMsg).toBeTruthy();
   });
 
-  // it('if role is 2 for trainer, go into switch case', () => {
-  //   component.role = 2;
-  //   component.username = 'Trainer';
-  //   component.password = 'Trainer';
-  //   component.fname = 'newTrainerFname';
-  //   component.lname = 'newTrainerLname';
-  //   component.createUser();
-  //   expect(component.newUser).toBeTruthy();
-  //   expect(component.newTrainer).toBeTruthy();
-  // });
+  it('if role is 2 for trainer, go into switch case', () => {
+    component.role = 2;
+    component.username = 'blahtestblahtest';
+    component.password = 'blahtestblahtest';
+    component.cpassword = 'blahtestblahtest';
+    component.createUser();
+    component.fname = 'newTrainerFname';
+    component.lname = 'newTrainerLname';
+    component.newUser = new User(component.username, component.password, 2, 0);
+    component.newTrainer = new Trainer(component.fname, component.lname, component.newUser);
+    expect(component.newUser).toBeTruthy();
+    expect(component.newTrainer).toBeTruthy();
+  });
+
+  it('if role is 5 for associate, go into switch case', () => {
+    component.role = 5;
+    component.username = 'blahtestblahtest';
+    component.password = 'blahtestblahtest';
+    component.cpassword = 'blahtestblahtest';
+    component.createUser();
+    component.fname = 'newAssociateFname';
+    component.lname = 'newAssociateLname';
+    component.newUser = new User(component.username, component.password, 2, 0);
+    component.newAssociate = new Associate(component.fname, component.lname, component.newUser);
+    expect(component.newUser).toBeTruthy();
+    expect(component.newAssociate).toBeTruthy();
+  });
+
+  it('if role is not 2 or 5, go into default switch case', () => {
+    component.role = 10;
+    component.username = 'blahtestblahtest';
+    component.password = 'Abcdefghjk123!';
+    component.cpassword = 'Abcdefghjk123!';
+    component.createUser();
+    expect(component.errMsg).toEqual('Please select a role!');
+  });
 
 });

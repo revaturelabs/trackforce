31f1b3f7ef776dbc97ff7f6540a0f481bd5e9037
Jacob <jacobgolding@Jacobs-MacBook-Pro.local>
2018-08-29 14:39:22 -0400

    Added Test to see if an associate can log in

--- TrackForce/src/test/java/com/revature/test/TestNG/AssociateTests.java
+++ TrackForce/src/test/java/com/revature/test/TestNG/AssociateTests.java
@@ -1,9 +1,16 @@
 package com.revature.test.TestNG;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+import com.revature.pom.LoginPage;
+
+import static org.testng.Assert.expectThrows;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,27 +19,42 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 
+
 public class AssociateTests {
   static WebDriver wd;
-  String username = "AssociateTest";
-  String password = "AssociateTest";
+  String username = "cyril";
+  String password = "cyril";
+  static WebDriverWait wait;
   
   @BeforeSuite
   void startup() {
 	  File chrome = new File("src/main/resources/chromeDriveer4Mac/chromedriver");
 	  System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
 	  wd = new ChromeDriver();
+	  wait = new WebDriverWait(wd,15);
   }
 	
-  @Test()
+  @Test(priority = 0)
   public void LoadWebpage() {
 	  wd.get("http://localhost:4200/#/login");
 	  new WebDriverWait(wd, 15).until(ExpectedConditions.urlContains("http://localhost:4200/#/login"));
   }
   
+  @Test (priority = 1)
+  public void LoginAssociate() {
+	 wait.until(ExpectedConditions.elementToBeClickable(LoginPage.username(wd)));
+	 LoginPage.username(wd).sendKeys(username);
+	 wait.until(ExpectedConditions.elementToBeClickable(LoginPage.password(wd)));
+	 LoginPage.password(wd).sendKeys(password);
+	 wait.until(ExpectedConditions.elementToBeClickable(LoginPage.signIn(wd)));
+	 LoginPage.signIn(wd).click();
+	 wait.until(ExpectedConditions.urlContains("http://localhost:4200/#/associate-view"));
+	 
+  }
+  
   @AfterSuite
 	void quit() {
-		wd.quit();
+	  wd.quit();
   }
 
 }

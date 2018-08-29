0a4b79336844a38de64ab73f8895bc2715812556
Henry <h.simon93@gmail.com>
2018-08-29 13:04:55 -0400

    Cucumber fixed, refactored, and simplified.
    Cucumber now runs via TestNG. The Cucumber Test Suite now simply
    includes 11 working test scenarios for happy/sad permutations of Login/Logout
    functionality, via the Login.feature and LoginCuke.java files.
    ServiceHooks.java, several .feature files, and several cuke files have
    been removed. The removed tests are currently being refactored for
    vanilla TestNG. Excessive sleeps are still an issue. Fixes #695

--- TrackForce/src/test/java/com/revature/test/admin/cuke/LoginCuke.java
+++ TrackForce/src/test/java/com/revature/test/admin/cuke/LoginCuke.java
@@ -1,4 +1,4 @@
-package com.revature.test.admin.cukes;
+package com.revature.test.admin.cuke;
 
 import static org.testng.Assert.assertEquals;
 
@@ -7,127 +7,134 @@ import org.openqa.selenium.WebDriver;
 
 import com.revature.test.admin.pom.HomeTab;
 import com.revature.test.admin.pom.Login;
-import com.revature.test.utils.ServiceHooks;
+import com.revature.test.utils.LoginUtil;
 import com.revature.test.utils.TestConfig;
 import com.revature.test.utils.WebDriverUtil;
 
 import cucumber.api.java.After;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class LoginCukes {
+
+
+public class LoginCuke {
+	
+	public static WebDriver driver;
+	
 	@Given("^I connect to caliber$")
 	public void i_connect_to_caliber(){
-		WebDriver driver = WebDriverUtil.getChromeDriver();
+		driver = WebDriverUtil.getChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(TestConfig.getBaseURL());
-		ServiceHooks.driver = driver;
 	}
 	
-	@Given("^the login page loads$")
+	@And("^the login page loads$")
 	public void the_login_page_loads() throws Throwable {
-		assertEquals(ServiceHooks.driver.getCurrentUrl(),"http://34.227.178.103:8090/NGTrackForce/login");
-	}
-	
-	@When("^I enter the correct \"([^\"]*)\" login information$")
-	public void i_enter_the_correct_login_information(String role) throws Throwable {
-	    String info = "";
-		switch(role) {
-	    case"admin":info = "TestAdmin"; break;
-	    case"delivery":info = "TestSales";break;
-	    case"associate":info = "TestAssociate";break;
-	    case"trainer":info = "TestTrainer";break;
-	    case"manager":info = "TestManager";break;
-	    }
-	    Login.login(info,info,ServiceHooks.driver);
+		assertEquals(driver.getCurrentUrl(),"http://34.227.178.103:8090/NGTrackForce/#/login");
 	}
 
 	@When("^I enter the correct admin login information$")
 	public void i_enter_the_correct_admin_login_information() throws Throwable {
-		Login.login("TestAdmin","TestAdmin",ServiceHooks.driver);
+		System.out.println("Entering correct admin login info");
+		Login.login(LoginUtil.getPropertyValue("adminUN"),LoginUtil.getPropertyValue("adminPW"),driver);
 	}
 	@When("^I enter the correct associate login information$")
 	public void i_enter_the_correct_associate_login_information() throws Throwable {
-	   Login.login("TestAssociate","TestAssociate",ServiceHooks.driver);
+		System.out.println("Entering correct associate login info");
+	   Login.login(LoginUtil.getPropertyValue("associateUN"),LoginUtil.getPropertyValue("associatePW"),driver);
 	}
 	@When("^I enter the correct manager login information$")
 	public void i_enter_the_correct_manager_login_information() throws Throwable {
-	   Login.login("TestManager","TestManager",ServiceHooks.driver);
+		System.out.println("Entering correct testmanager login info");
+	   Login.login(LoginUtil.getPropertyValue("stagingManagerUN"),LoginUtil.getPropertyValue("stagingManagerPW"),driver);
 	}
 	@When("^I enter the correct trainer login information$")
 	public void i_enter_the_correct_trainer_login_information() throws Throwable {
-	   Login.login("TestTrainer","TestTrainer",ServiceHooks.driver);
+		System.out.println("Entering correct trainer login info");
+	   Login.login(LoginUtil.getPropertyValue("trainerUN"),LoginUtil.getPropertyValue("trainerPW"),driver);
 	}
 	@When("^I enter the correct delivery login information$")
 	public void i_enter_the_correct_sales_login_information() throws Throwable {
-	   Login.login("TestSales","TestSales",ServiceHooks.driver);
+		System.out.println("Entering correct sales login info");
+	   Login.login(LoginUtil.getPropertyValue("salesDeliveryUN"),LoginUtil.getPropertyValue("salesDeliveryPW"),driver);
 	}
 
 	@When("^I click Submit$")
 	public void i_click_Submit() throws Throwable {
-		Login.getSignin(ServiceHooks.driver).click();
-		Thread.sleep(5000);
+		System.out.println("Clicking submit (login)");
+		Login.getSignin(driver).click();
+		Thread.sleep(3000);
 	}
 	
 	@When("^I enter a correct username without a password$")
 	public void i_enter_a_correct_username_without_a_password() throws Throwable {
-		Login.login("TestAdmin", "", ServiceHooks.driver);
+		System.out.println("Entering correct username with no password");
+		Login.login("TestAdmin", "", driver);
 	}
 	
 	@When("^I enter a correct password with an incorrect username$")
 	public void i_enter_a_correct_password_with_an_incorrect_username() throws Throwable {
-		Login.login("NotAUsername", "TestAdmin", ServiceHooks.driver);
+		System.out.println("Entering incorrect username with correct password");
+		Login.login("NotAUsername", "TestAdmin", driver);
 	}
 
 	@When("^I enter an incorrect password with an incorrect username$")
 	public void i_enter_an_incorrect_password_with_an_incorrect_username() throws Throwable {
-		Login.login("NotAUsername", "NotAPassword", ServiceHooks.driver);
+		Login.login("NotAUsername", "NotAPassword", driver);
 	}
 	
 	@When("^I enter a correct username with an incorrect password$")
 	public void i_enter_a_correct_username_with_an_incorrect_password() throws Throwable {
-		Login.login("TestAdmin","NotAPassword", ServiceHooks.driver);
+		Login.login("TestAdmin","NotAPassword", driver);
 	}
 
 	@When("^I enter a correct password without a username$")
 	public void i_enter_a_correct_password_without_a_username() throws Throwable {
-	    Login.login("", "TestAdmin", ServiceHooks.driver);
+	    Login.login("", "TestAdmin", driver);
 	}
 	
 	@When("^if I click Log out$")
 	public void if_I_click_Log_out() throws Throwable {
-		ServiceHooks.driver.findElement(By.xpath("/html/body/app-component/app-navbar/nav/div/ul[2]/li[1]/a")).click();
-		//Logout.logout(ServiceHooks.driver);
+		System.out.println("clicking logout");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("/html/body/app-component/app-navbar/nav/div/ul[2]/li[1]/a")).click();
 	}
 
 	@Then("^I should remain on the login page$")
 	public void i_should_remain_on_the_login_page() throws Throwable {
 		Thread.sleep(1500);
-		assertEquals(HomeTab.getCurrentURL(ServiceHooks.driver),"http://34.227.178.103:8090/NGTrackForce/login");
+		assertEquals(HomeTab.getCurrentURL(driver),"http://34.227.178.103:8090/NGTrackForce/#/login");
 	}
 
 	@Then("^I should be taken to the home page$")
 	public void i_should_be_taken_to_the_home_page() throws Throwable {
-		Thread.sleep(1000);
-		assertEquals(HomeTab.getCurrentURL(ServiceHooks.driver),"http://34.227.178.103:8090/NGTrackForce/app-home");
+		Thread.sleep(2000);
+		assertEquals(HomeTab.getCurrentURL(driver),"http://34.227.178.103:8090/NGTrackForce/#/app-home");
+	}
+	
+	@Then("^I should be taken to the trainer home page$")
+	public void i_should_be_taken_to_the_trainer_home_page() throws Throwable {
+		Thread.sleep(2000);
+		assertEquals(HomeTab.getCurrentURL(driver),"http://34.227.178.103:8090/NGTrackForce/#/trainer-view");
 	}
 	
 	@Then("^I should be taken to the associate home page$")
 	public void i_should_be_taken_to_the_associate_home_page() throws Throwable {
-		Thread.sleep(5000);
-		assertEquals(ServiceHooks.driver.getCurrentUrl(),"http://34.227.178.103:8090/NGTrackForce/associate-view/56052");
+		Thread.sleep(2000);
+		assertEquals(driver.getCurrentUrl(),"http://34.227.178.103:8090/NGTrackForce/#/associate-view");
 	}
 
 	@Then("^I should be on the login page$")
 	public void i_should_be_on_the_login_page() throws Throwable {
-		Thread.sleep(7000);
-		assertEquals(ServiceHooks.driver.getCurrentUrl(),"http://34.227.178.103:8090/NGTrackForce/login");
+		Thread.sleep(2000);
+		assertEquals(driver.getCurrentUrl(),"http://34.227.178.103:8090/NGTrackForce/#/login");
 	}
 	
 	@After
 	public void close() {
-		ServiceHooks.driver.close();
+		driver.quit();
 	}
 	
 	

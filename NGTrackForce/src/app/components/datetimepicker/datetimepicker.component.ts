7b6e3824599ddb8964bc651e547a52b4eaadc1a3
Kevin Wu <kwu.uic@gmail.com>
2018-08-29 15:24:12 -0400

    TestAdmin "Batch" tab date limits
    Logging in as TestAdmin, under the "Batch" tab, there is now a limit to
    the start and end dates as well as an error if the submitted from date
    exceeds the to date and vice versa.

--- NGTrackForce/src/app/components/datetimepicker/datetimepicker.component.ts
+++ NGTrackForce/src/app/components/datetimepicker/datetimepicker.component.ts
@@ -1,52 +1,63 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { DateService } from '../../services/date-service/date.service';
-
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { DateService } from "../../services/date-service/date.service";
 
 @Component({
-  selector: 'app-datetimepicker',
-  templateUrl: './datetimepicker.component.html',
-  styleUrls: ['./datetimepicker.component.css']
+  selector: "app-datetimepicker",
+  templateUrl: "./datetimepicker.component.html",
+  styleUrls: ["./datetimepicker.component.css"]
 })
-
 export class DateTimePickerComponent implements OnInit {
-    @Input() width = '250px';   //default value
-    @Input() format = 'date';   //default value
-    @Input() originalDate: number;    //no default
-    @Input() dateType: string;  // start or end date
-    @Output() datePicked = new EventEmitter();
-    @Output() error = new EventEmitter();
-    calendarView = false;
-    displayErrorInvalidDate = false;
+  @Input()
+  width = "250px"; //default value
+  @Input()
+  format = "date"; //default value
+  @Input()
+  originalDate: number; //no default
+  @Input()
+  dateType: string; // start or end date
+  @Output()
+  datePicked = new EventEmitter();
+  @Output()
+  error = new EventEmitter();
+  calendarView = false;
+  displayErrorInvalidDate = false;
 
-    options_date = {month:'long', day: 'numeric', year: 'numeric'};
-    options_datetime = {month:'short', day: 'numeric', year: 'numeric', minute:'numeric', hour: 'numeric'};
-    date = new Date();   //initialized to today's date
-    stringDate: string;
+  options_date = { month: "long", day: "numeric", year: "numeric" };
+  options_datetime = {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    minute: "numeric",
+    hour: "numeric"
+  };
+  date = new Date(); //initialized to today's date
+  stringDate: string;
+  minDate = new Date();
+  maxDate = new Date();
 
-    oldDate: Date;
+  oldDate: Date;
 
-    constructor(private dateService: DateService) {
+  constructor(private dateService: DateService) {}
 
-    }
+  ngOnInit() {
+    this.minDate.setFullYear(2010);
+    this.maxDate.setFullYear(new Date().getFullYear() + 1);
 
-    ngOnInit() {
-        this.dateReset();
-    }
+    this.dateReset();
+  }
 
-    dateReset(){
-        setTimeout(()=>{
-            if (this.originalDate){ //because its an optional parameter
-                this.date = new Date(this.originalDate);
-                this.toggleCalendarView();
-                this.dateClicked(); //this is to validate it and update other internal variables.
-            }
-        },0);
-    }
+  dateReset() {
+    setTimeout(() => {
+      if (this.originalDate) {
+        //because its an optional parameter
+        this.date = new Date(this.originalDate);
+        this.toggleCalendarView();
+        this.dateClicked(); //this is to validate it and update other internal variables.
+      }
+    }, 0);
+  }
 
-    public toggleCalendarView(){
-        this.calendarView = !this.calendarView;
-    }
-
+<<<<<<< HEAD
     public dateClicked(){
         let localOptions = null;
         switch(this.format){
@@ -60,17 +71,40 @@ export class DateTimePickerComponent implements OnInit {
               this.calendarView = !this.calendarView;
             }
             this.oldDate = this.date;
+=======
+  public toggleCalendarView() {
+    this.calendarView = !this.calendarView;
+  }
+>>>>>>> TestAdmin "Batch" tab date limits
 
-            this.validateDate();
-        }
+  public dateClicked() {
+    let localOptions = null;
+    switch (this.format) {
+      case "date":
+        localOptions = this.options_date;
+        break;
+      case "datetime":
+        localOptions = this.options_datetime;
+        break;
     }
+    if (this.date != null) {
+      this.stringDate = this.date.toLocaleDateString("en-US", localOptions);
+      this.datePicked.emit(this.stringDate);
+      if (this.oldDate != this.date) {
+        this.calendarView = !this.calendarView;
+      }
+      this.oldDate = this.date;
 
-    public manualEntry(){
-        this.date = new Date(this.stringDate);
+      //if the date chosen is invalid (before 2010, after +1 of current year), revert to previous date
+      if (this.validateDate() !== 0) {
+        this.date = new Date(this.originalDate);
+        this.stringDate = this.date.toLocaleDateString("en-US", localOptions);
         this.datePicked.emit(this.stringDate);
-        this.validateDate();
+      }
     }
+  }
 
+<<<<<<< HEAD
     public validateDate(){
         if (this.date.toString() === "Invalid Date"){
             this.displayErrorInvalidDate = true;
@@ -80,6 +114,28 @@ export class DateTimePickerComponent implements OnInit {
           this.displayErrorInvalidDate = false;
         }
         this.error.emit(this.displayErrorInvalidDate);
+=======
+  public manualEntry() {
+    this.date = new Date(this.stringDate);
+    this.datePicked.emit(this.stringDate);
+    this.validateDate();
+  }
+
+  public validateDate() {
+    if (this.date < this.minDate) {
+      this.displayErrorInvalidDate = true;
+      this.date = this.oldDate;
+    }
+    else if (this.date > this.maxDate) {
+      this.displayErrorInvalidDate = true;
+      this.date = this.oldDate;
+    } else {
+      this.displayErrorInvalidDate = false;
+      return 0;
+>>>>>>> TestAdmin "Batch" tab date limits
     }
 
+    this.error.emit(this.displayErrorInvalidDate);
+
+  }
 }

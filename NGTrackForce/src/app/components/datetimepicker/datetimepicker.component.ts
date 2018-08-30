0a51b7d57c590c6879365f6d77e1791003d18b37
Kevin Wu <kwu.uic@gmail.com>
2018-08-30 10:04:49 -0400

    fixed merging conflicts in batch-list and datetimepicker components

--- NGTrackForce/src/app/components/datetimepicker/datetimepicker.component.ts
+++ NGTrackForce/src/app/components/datetimepicker/datetimepicker.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { DateService } from '../../services/date-service/date.service';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { DateService } from "../../services/date-service/date.service";
 
 
 @Component({
-  selector: 'app-datetimepicker',
-  templateUrl: './datetimepicker.component.html',
-  styleUrls: ['./datetimepicker.component.css']
+  selector: "app-datetimepicker",
+  templateUrl: "./datetimepicker.component.html",
+  styleUrls: ["./datetimepicker.component.css"]
 })
 
 export class DateTimePickerComponent implements OnInit {
-    @Input() width = '250px';   //default value
-    @Input() format = 'date';   //default value
-    @Input() originalDate: number;    //no default
-    @Input() dateType: string;  // start or end date
-    @Output() datePicked = new EventEmitter();
-    @Output() error = new EventEmitter();
-    calendarView = false;
-    displayErrorInvalidDate = false;
+  @Input()
+  width = "250px"; //default value
+  @Input()
+  format = "date"; //default value
+  @Input()
+  originalDate: number; //no default
+  @Input()
+  dateType: string; // start or end date
+  @Output()
+  datePicked = new EventEmitter();
+  @Output()
+  error = new EventEmitter();
+  calendarView = false;
+  displayErrorInvalidDate = false;
 
-    options_date = {month:'long', day: 'numeric', year: 'numeric'};
-    options_datetime = {month:'short', day: 'numeric', year: 'numeric', minute:'numeric', hour: 'numeric'};
-    date = new Date();   //initialized to today's date
-    stringDate: string;
+  options_date = { month: "long", day: "numeric", year: "numeric" };
+  options_datetime = {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    minute: "numeric",
+    hour: "numeric"
+  };
+  date = new Date(); //initialized to today's date
+  stringDate: string;
+  minDate = new Date();
+  maxDate = new Date();
 
     oldDate: Date;
 
@@ -64,9 +78,24 @@ export class DateTimePickerComponent implements OnInit {
             this.validateDate();
         }
     }
+<<<<<<< HEAD
 
     public manualEntry(){
         this.date = new Date(this.stringDate);
+=======
+    if (this.date != null) {
+      this.stringDate = this.date.toLocaleDateString("en-US", localOptions);
+      this.datePicked.emit(this.stringDate);
+      if (this.oldDate != this.date) {
+        this.calendarView = !this.calendarView;
+      }
+      this.oldDate = this.date;
+
+      //if the date chosen is invalid (before 2010, after +1 of current year), revert to previous date
+      if (this.validateDate() !== 0) {
+        this.date = new Date(this.originalDate);
+        this.stringDate = this.date.toLocaleDateString("en-US", localOptions);
+>>>>>>> fixed merging conflicts in batch-list and datetimepicker components
         this.datePicked.emit(this.stringDate);
         this.validateDate();
     }

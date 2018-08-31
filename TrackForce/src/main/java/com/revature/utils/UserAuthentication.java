2f09a874d6ecd1ee4aff036ddca6cd17b51d4c6d
TaylorBrodowsky <taylor.brodowsky@gmail.com>
2018-08-31 11:14:32 -0400

    updated authorization method
    fixes#734

--- TrackForce/src/main/java/com/revature/utils/UserAuthentication.java
+++ TrackForce/src/main/java/com/revature/utils/UserAuthentication.java
@@ -16,24 +16,30 @@ public class UserAuthentication {
  * @param levelNeeded
  * level needed is the role from the database of your user, ex. an Admin has 1 and an Associate is 5
  * @return
- * returns true or false so that the page can continue
- * 
- * effect: reduced the lines and complexity of the check for each individual authentication.
+ * returns true or false so that the page can continue.
+ * true means that the page was authorized.
  */
 	public static boolean Authorized (String token, int [] levelNeeded) {
+		//creates payload object that we can get the role of the user from
 		Claims payload = JWTService.processToken(token);
-		// this checks to see if Id is in the array
-		boolean result = IntStream.of(levelNeeded).anyMatch(x -> x == Integer.parseInt(payload.getId()));
-
-		if (payload != null && result) {
+		//makes sure payload is not null
+		if (payload == null) {
 			return false;
 		}else {
-
-			return true;
+			//checks if the users role# is within the parameter for the request
+			boolean result = IntStream.of(levelNeeded).anyMatch(
+					x -> x == Integer.parseInt(payload.getId())
+			);
+			
+			if ( result) {
+				//if it is then they are authorized for the page
+				return true;
+			}else {
+				//if not then they are denied 
+				return false;
+			}
 		}
 	}
-
-
 }
 
 

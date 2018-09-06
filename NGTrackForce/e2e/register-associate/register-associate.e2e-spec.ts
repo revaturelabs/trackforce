1eb1f1e1ee9b3b06499715fa700f9229d3d08883
Jacob <jacobgolding@Jacobs-MacBook-Pro.local>
2018-09-06 11:57:25 -0400

    almost finished registar user

--- NGTrackForce/e2e/register-associate/register-associate.e2e-spec.ts
+++ NGTrackForce/e2e/register-associate/register-associate.e2e-spec.ts
@@ -19,28 +19,53 @@ describe('Registering as a new associate', () => {
 
     it('should navigate to the login page', () => {
         loginPage.navigateTo();
+        expect(registerAssociatePage.RegisterBtn()).toBeTruthy();
     });
 
     it('allow user to click the register button', () => {
-        registerAssociatePage.clickRegisterBtn();
+        registerAssociatePage.RegisterBtn().click();
+        expect(registerAssociatePage.RegisterUserBtn()).toBeTruthy();
     });
 
-    it('allow associate to enter info into input fields', () => {
-        registerAssociatePage.enterAssociateInfo('john', 'john');
+    it('allow associate to enter a username', () => {
+        registerAssociatePage.AssociateUsername().sendKeys('john');
+        expect(registerAssociatePage.AssociateUsername().getAttribute("value")).toEqual('john');
+    });
+
+    it('allow associate to enter a password', () => {
+        registerAssociatePage.AssociatePassword().sendKeys('password');
+        expect(registerAssociatePage.AssociatePassword().getAttribute("value")).toEqual('password');
+    });
+
+    it('allow associate to enter a password', () => {
+        registerAssociatePage.AssociateCPassword().sendKeys('password');
+        expect(registerAssociatePage.AssociateCPassword().getAttribute("value")).toEqual('password');
     });
 
     xit('should allow associate to click the next page button', () => {
         registerAssociatePage.clickNextPageBtn();
     });
 
-    it('should allow associate to enter first and last name', () => {
-        registerAssociatePage.enterFirstName('John');
-        registerAssociatePage.enterLastName('Doe');
+    it('should allow associate to enter first name', () => {
+        registerAssociatePage.FirstName().sendKeys('John');
+        expect(registerAssociatePage.FirstName().getAttribute("value")).toEqual('John')
     });
 
-    it('should allow associate to register as an associate by clicking register button', () => {
-        registerAssociatePage.clickRegisterBtn();
+    it('should allow associate to enter last name', () => {
+        registerAssociatePage.LastName().sendKeys('Doe');
+        expect(registerAssociatePage.LastName().getAttribute("value")).toEqual('Doe')
     });
+
+    it('show allow associate to pick a role', () => {
+      registerAssociatePage.RolePick().click();
+      registerAssociatePage.RolePick().getAttribute("value")  .then(text => {
+            console.log(text);
+        });
+    })
+
+    // it('should allow associate to register as an associate by clicking register button', () => {
+    //     registerAssociatePage.RegisterBtn();
+    // });
 
     it('exit browser', () => {
         loginPage.navigateTo();

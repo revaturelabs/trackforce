28d427b2ee0ec6c49afd0c7c517e860fb2e72d75
Christopher <chrisissiu@gmail.com>
2018-09-07 13:37:18 -0400

    adding more associateDaoTests

--- TrackForce/src/test/java/com/revature/test/dao/AssociateDAOTest.java
+++ TrackForce/src/test/java/com/revature/test/dao/AssociateDAOTest.java
@@ -6,6 +6,7 @@ import static org.testng.Assert.assertEquals;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
@@ -13,18 +14,23 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.revature.dao.AssociateDao;
+import com.revature.dao.UserDao;
 import com.revature.daoimpl.AssociateDaoImpl;
+import com.revature.daoimpl.UserDaoImpl;
 import com.revature.entity.TfAssociate;
+import com.revature.entity.TfUser;
 import com.revature.test.utils.Log;
 
 public class AssociateDAOTest {
 	
 	private AssociateDao dao;
+	private UserDao userDao;
 	private Properties props;
 	
 	@BeforeClass
 	public void initialize() {
 		dao = new AssociateDaoImpl();
+		userDao = new UserDaoImpl();
 		props = new Properties();
 		try {
 			FileInputStream propFile = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\database_entries.properties");
@@ -109,15 +115,42 @@ public class AssociateDAOTest {
 
 	@Test
 	public void testAssociateDAOApproveAssociate() {
+		dao.approveAssociate(2);
+		TfUser user = userDao.getUser("Duncan");
+		assertEquals(user.getIsApproved(), 1);
+		
+		//Undo change to database
+		user.setIsApproved(0);
+		userDao.updateUser(user);
 		
 	}
 	
 	@Test
 	public void testAssociateDAOApproveAssociates() {
+		List<Integer> toBeApproved = new LinkedList<Integer>();
+		toBeApproved.add(2);
+		toBeApproved.add(3);
+		toBeApproved.add(4);
+		toBeApproved.add(5);
+		toBeApproved.add(6);
+		toBeApproved.add(7);
+		toBeApproved.add(8);
+		toBeApproved.add(9);
+		toBeApproved.add(10);
+		
+		dao.approveAssociates(toBeApproved);
+		String[] userlist = ((String)props.get("usernames")).split(",");
+		for(String username : userlist) {
+			TfUser user = userDao.getUser(username);
+			assertEquals(user.getIsApproved(), 1);
+			
+			user.setIsApproved(0);
+			userDao.updateUser(user);
+		}
 		
 	}
 	
-	@Test
+	@Test(dependsOnMethods= {"testAssociateDAOGetAssociate"})
 	public void testAssociateDAOCreateAssociate() {
 		
 	}

93a1568d399c511c4251f997e4330964ae277df6
perryja <perryja@umail.iu.edu>
2018-09-07 13:51:24 -0700

    service tests and updated property file

--- TrackForce/src/test/java/com/revature/test/services/JWTServicesTest.java
+++ TrackForce/src/test/java/com/revature/test/services/JWTServicesTest.java
@@ -1,12 +1,41 @@
 package com.revature.test.services;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.revature.daoimpl.AssociateDaoImpl;
+import com.revature.services.AssociateService;
+import com.revature.services.JWTService;
+import com.revature.test.utils.Log;
 
 public class JWTServicesTest {
 
+  private JWTService service;
+  Properties prop;
+  
+  @BeforeClass
+  public void init() {
+	service = new JWTService();
+	prop = new Properties();
+	try {
+		FileInputStream propFile = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\database_entries.properties");
+		prop.load(propFile);
+		propFile.close();
+	} catch(FileNotFoundException e) {
+		Log.Log.error(e.getMessage());
+	} catch (IOException e) {
+		Log.Log.error(e.getMessage());
+	}
+  }
+	
   @Test
   public void testJWTValidateToken() {
-
+	  
   }
 
   @Test
@@ -16,7 +45,7 @@ public class JWTServicesTest {
 
   @Test
   public void testJWTProcessToken() {
-
+	  
   }
 
   @Test

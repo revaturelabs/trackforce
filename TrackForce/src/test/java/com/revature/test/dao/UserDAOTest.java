6e8eadb97b90e7d8f4370821ed8bb4436ce6a7a7
Mohamed Yousef <mfy5013@gmail.com>
2018-09-10 10:33:19 -0400

    added dao tests

--- TrackForce/src/test/java/com/revature/test/dao/UserDAOTest.java
+++ TrackForce/src/test/java/com/revature/test/dao/UserDAOTest.java
@@ -1,32 +1,48 @@
 package com.revature.test.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class UserDAOTest {
-	
+import com.revature.daoimpl.UserDaoImpl;
+import com.revature.entity.TfUser;
+
+
+
+public class UserDaoTest {
   @Test
-  public void testUserDAOGetUser() {
-	  
+  public void canGetUserByUsername() {
+	  Assert.assertEquals(1, new UserDaoImpl().getUser("TestAdmin").getId());
   }
   
   @Test
-  public void testUserDAOGetUsers() {
-	  
+  public void getAllUsersIsNotEmpty() {
+	  Assert.assertNotEquals(0, new UserDaoImpl().getAllUsers().size());
   }
   
   @Test
-  public void testUserDAOInsertUser() {
-	  
+  public void getAllUsersWorks() {
+	  List<TfUser> list = new ArrayList<TfUser>();
+	  list = new UserDaoImpl().getAllUsers();
+	  Assert.assertEquals(list.get(10).getId() , 10 );
+  }
+  
+  @Test(expectedExceptions = NullPointerException.class)
+  public void insertingDuplicateUsersThrowsException() {
+	  TfUser user = new UserDaoImpl().getUser("TestAdmin");
+	  new UserDaoImpl().insertUser(user);
   }
   
   @Test
-  public void testUserDAOGetRole() {
-	  
+  public void getRoleUserDaoWorks() {
+	  Assert.assertEquals(new UserDaoImpl().getRole(1).getTfRoleName(), "Admin");
   }
   
   @Test
-  public void testUserDAOUpdateUser() {
-	  
+  public void updateUserDaoWorks() {
+	  TfUser user = new UserDaoImpl().getUser("Associate2");
+	  Assert.assertEquals(new UserDaoImpl().updateUser(user), true);
   }
-  
 }

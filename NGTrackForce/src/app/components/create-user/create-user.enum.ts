0758503bb666ac7a730b7b837b8f15f4a68d9d3a
Steve Loayza <stvloayza2@gmail.com>
2018-09-09 21:45:50 -0400

    changed batch list ui, client list ui, and create user ui

--- NGTrackForce/src/app/components/create-user/create-user.enum.ts
+++ NGTrackForce/src/app/components/create-user/create-user.enum.ts
@@ -1,9 +1,17 @@
 /**
  * Enumeration containing all expected display messages for the create user page
+ *
+          <h3>Password must contain the following:</h3>
+          <p id="length" class="invalid">Minimum <b>8 characters</b></p>
+          <p id="letter" class="invalid">At least one <b>lowercase</b> letter</p>
+          <p id="capital" class="invalid">At least one <b>capital (uppercase)</b> letter</p>
+          <p id="number" class="invalid">At least one <b>number</b></p>
+          <p id="char" class="invalid">At least one special character <b>(.!@#$%^&*)</b></p>
+          Password must have a number, a capital letter and a special character
  */
 export enum StatusMessage {
     INVALID_PASS = 'Password must have a number, a capital letter and a special character',
-    INVALID_USER = 'Invalid username, please do not use spaces or special characters',
+    INVALID_USER = 'Alphabetical and numerical characters only, no spaces',
     MISMATCH = 'Passwords do not match!',
     NONUNIQUE = 'Username is not unique!',
     SUCCESS = 'User created successfully',

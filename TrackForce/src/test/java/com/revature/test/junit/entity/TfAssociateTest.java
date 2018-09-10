a7dfe312936a65acf8decc04a90904313cb1d4ca
csiu13 <chrisissiu@gmail.com>
2018-09-10 08:33:22 -0400

    Add daoimpl tests (#807)
    * added more unit tests for daoimplementations

--- TrackForce/src/test/java/com/revature/test/junit/entity/TfAssociateTest.java
+++ TrackForce/src/test/java/com/revature/test/junit/entity/TfAssociateTest.java
@@ -7,7 +7,7 @@ import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import com.revature.entity.TfAssociate;
 import com.revature.entity.TfBatch;

7290a2cb7351b2a055dfa8a315a3f4ba543ed587
Unknown <jdpressley5@catamount.wcu.edu>
2018-09-11 14:49:51 -0400

    Left note for finalizing implementation.

--- TrackForce/src/main/java/com/revature/utils/DbResetUtil.java
+++ TrackForce/src/main/java/com/revature/utils/DbResetUtil.java
@@ -1,19 +1,30 @@
 package com.revature.utils;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import org.flywaydb.core.Flyway;
-import org.hibernate.Session;
 
 /** This implementation is used to reset the database with 
  * proper variables so testing and modifications can be made.
  * @author Josh Pressley ,Chris Siu - 1807 */
 public class DbResetUtil {
 	
-//	/** Calls the sql script that resets the DB*/
+	/* The method implementations below both have issues with the script file.
+	 * Method 1:
+	 * 		Hibernate has to read and execute every line individually. Latest error hints that the script needs to use
+	 * 		entity names instead of table names.
+	 * Method 2:
+	 * 		Flyway cannot read the script file even though it is finding a file in the proper directory. 
+	 * 		Assumptions on this error are that the file needs some type of prefix on the file name or a special formatting to be read.
+	 * 
+	 * Time ran out for us to finish the implementation of this feature but you should only need to get the script to read. The 
+	 * rest of the process should be in place. 
+	 * 
+	 * Best of luck -1807 */
+	
+	//Dummy method for functionalities sake.
+	public static boolean resetDatabase() { 
+		return true;
+	}
+	
+	/** Calls the sql script that resets the DB
+	* Hibernate reformatting version */
 //	public static boolean resetDatabase() throws IOException {
 //		BufferedReader reader = null;
 //		String localCopy;		
@@ -21,7 +32,6 @@ public class DbResetUtil {
 //		try {
 //			File f = new File("/Users/Josh/Documents/GitHub/trackforce/TrackForce/src/main/resources/reInitDB.sql");
 //			reader = new BufferedReader(new FileReader(f));
-//			
 //			session = HibernateUtil.getSessionFactory().openSession();
 //			session.beginTransaction();
 //			
@@ -30,7 +40,6 @@ public class DbResetUtil {
 //				localCopy = localCopy.substring(0, localCopy.length()-1); //removes the semicolon
 //				session.createQuery(localCopy).executeUpdate();
 //			}
-//			
 //			session.getTransaction().commit();
 //			return true;
 //		} catch (FileNotFoundException e) {
@@ -44,12 +53,13 @@ public class DbResetUtil {
 //		return false;
 //	}
 	
-	/** Calls the sql script that resets the DB*/
-	public static boolean resetDatabase() {
-		Flyway flyway = new Flyway();
-		flyway.setDataSource(System.getenv("TRACKFORCE_DB_URL"),System.getenv("TRACKFORCE_DB_USERNAME"),System.getenv("HBM_PW_ENV"));
-		flyway.setLocations("filesystem:src/main/resources/");
-		flyway.migrate();
-		return true;
-	}
+	/** Calls the sql script that resets the DB
+	 * flyway version */
+//	public static boolean resetDatabase() {
+//		Flyway flyway = new Flyway();
+//		flyway.setDataSource(System.getenv("TRACKFORCE_DB_URL"),System.getenv("TRACKFORCE_DB_USERNAME"),System.getenv("HBM_PW_ENV"));
+//		flyway.setLocations("filesystem:src/main/resources/");
+//		flyway.migrate();
+//		return true;
+//	}
 }

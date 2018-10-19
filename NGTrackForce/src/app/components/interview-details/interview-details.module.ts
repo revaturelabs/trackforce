1d90484bdba4690f41b209c9c19dfa4676aca674
Jaxon Lee <jwlee@mix.wvu.edu>
2018-10-19 16:40:31 -0400

    trying again

--- NGTrackForce/src/app/components/interview-details/interview-details.module.ts
+++ NGTrackForce/src/app/components/interview-details/interview-details.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { InterviewService } from '../../services/interview-service/interview.service';
 import { AuthenticationService } from '../../services/authentication-service/authentication.service';
 import { InterviewDetailsComponent } from './interview-details.component';
-import { MyInterviewComponent } from '../myinterview-view/myinterview-view.component';
+//import { MyInterviewComponent } from '../myinterview-view/myinterview-view.component';
 
 import { CommonModule } from '@angular/common';
 import {InterviewDetailsRoutingModule } from './interview-details-routing.module';
@@ -14,7 +14,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @NgModule({
     declarations: [
         InterviewDetailsComponent,
-        MyInterviewComponent
+        //MyInterviewComponent
     ],
     providers:[
         InterviewService,

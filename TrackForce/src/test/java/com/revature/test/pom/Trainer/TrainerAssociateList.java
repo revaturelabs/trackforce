3c2ea1200c2d86eef537156534e6ad2b9ed50f60
Art Burtch <artburtch@huskers.unl.edu>
2018-10-23 15:06:43 -0400

    updated assoc list and details across the board -ab

--- TrackForce/src/test/java/com/revature/test/pom/Trainer/TrainerAssociateList.java
+++ TrackForce/src/test/java/com/revature/test/pom/Trainer/TrainerAssociateList.java
@@ -1,36 +1,92 @@
 package com.revature.test.pom.Trainer;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class TrainerAssociateList {
-	public WebElement getTextFilterBox(WebDriver driver) {
-		return driver.findElement(By.name("filterByText"));
+	public static WebElement getFilterByText(WebDriver driver) {
+		return driver.findElement(By.cssSelector("input[type='text'][name='filterByText']"));
 	}
-	public WebElement getStatusDropdown(WebDriver driver) {
-		return driver.findElement(By.name("filterByStatus"));
+	
+	public static WebElement getStatusSelect(WebDriver driver) {
+		return driver.findElement(By.cssSelector("select[name='filterByStatus']"));
 	}
-	public WebElement getStatusByName(WebDriver driver, String name) {
-		return getStatusDropdown(driver).findElement(By.linkText(name));
+	
+	public static WebElement getStatusOption(WebDriver driver, String value) {
+		return getStatusSelect(driver).findElement(By.cssSelector("option[value='" + value + "']"));
 	}
-	public WebElement getStatusById(WebDriver driver, int id) {
-		return getStatusDropdown(driver).findElement(By.cssSelector("option:nth-child(" + id + ")"));
+	
+	public static WebElement getClientSelect(WebDriver driver) {
+		return driver.findElement(By.cssSelector("select[name='filterByClient']"));
 	}
-	public WebElement getClientDropdown(WebDriver driver) {
-		return driver.findElement(By.name("filterByClient"));
+	
+	public static WebElement getClientOptionByName(WebDriver driver, String clientName) {
+		return getClientSelect(driver).findElement(By.cssSelector("option[innerText='" + clientName + "']"));
 	}
-	public WebElement getClientByName(WebDriver driver, String name) {
-		return getClientDropdown(driver).findElement(By.linkText(name));
+	
+	public static WebElement getClientOptionByValue(WebDriver driver, int clientValue) {
+		return getClientSelect(driver).findElement(By.cssSelector("option[value='" + clientValue + "']"));
 	}
-	public WebElement getClientById(WebDriver driver, int id) {
-		return getClientDropdown(driver).findElement(By.cssSelector("option:nth-child(" + id + ")"));
+	
+	public static WebElement getClearFiltersButton(WebDriver driver) {
+		return driver.findElement(By.cssSelector("button[type='button'][innerText='Clear Filters']"));
 	}
-	public WebElement getClearFiltersButton(WebDriver driver) {
-		return driver.findElement(By.linkText("Clear Filters"));
+	
+	public static WebElement getFilterButton(WebDriver driver) {
+		return driver.findElement(By.cssSelector("button[type='button'][innerText='Filter']"));
 	}
-	public WebElement getFilterButton(WebDriver driver) {
-		return driver.findElement(By.linkText("Filter"));
+	
+	public static List<WebElement> getAssociateRows(WebDriver driver) {
+		return driver.findElements(By.cssSelector("tr[class='ng-star-inserted']"));
+	}
+	
+	public static WebElement getAssociateRow(WebDriver driver, int id) {
+		String detailsHref = "/NGTrackForce/form-comp/"+id;
+		for (WebElement row: getAssociateRows(driver)) {
+			if (getAssociateDetails(driver, row).getAttribute("href").equals(detailsHref)) {
+				return row;
+			}
+		}
+		return null;
+	}
+	
+	public static List<WebElement> getRowColumns(WebDriver driver, WebElement row) {
+		return row.findElements(By.tagName("td"));
+	}
+	
+	public static WebElement getFirstName(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(0);
+	}
+	
+	public static WebElement getLastName(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(1);
+	}
+	
+	public static WebElement getMarketingStatus(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(2);
+	}
+	
+	public static WebElement getClientName(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(3);
+	}
+	
+	public static WebElement getBatchName(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(4);
+	}
+	
+	public static WebElement getVerificationStatus(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(5);
+	}
+	
+	public static WebElement getAssociateDetails(WebDriver driver, WebElement row) {
+		return getRowColumns(driver, row).get(6).findElement(By.tagName("a"));
+	}
+	
+	public static WebElement getUpdateAssociatesButton(WebDriver driver) {
+		return driver.findElement(By.cssSelector("button[type='button'][innerText='Update']"));
 	}
 }
 

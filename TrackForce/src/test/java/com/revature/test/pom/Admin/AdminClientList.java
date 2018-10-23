a31177ceda4ecc662ebf2b8ea3ce3202226d5d00
Dylan Smith <mrdcs92@gmail.com>
2018-10-23 13:09:49 -0400

    completed admin clientlist and home poms

--- TrackForce/src/test/java/com/revature/test/pom/Admin/AdminClientList.java
+++ TrackForce/src/test/java/com/revature/test/pom/Admin/AdminClientList.java
@@ -1,6 +1,25 @@
 package com.revature.test.pom.Admin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 //IMPORTANT: A lot of code for this POM already exists in the general POM Package.
 public class AdminClientList {
 
+	public static List<WebElement> elementList = new ArrayList<WebElement>();
+	public static WebElement element;
+
+	public static List<WebElement> getClientList(WebDriver driver) {
+		elementList = driver.findElements(By.id("clients-list"));
+		return elementList;
+	}
+
+	public static WebElement filter(WebDriver driver) {
+		element = driver.findElement(By.id("clientSearch"));
+		return element;
+	}
 }
--- TrackForce/src/test/java/com/revature/test/pom/Admin/AdminHome.java
+++ TrackForce/src/test/java/com/revature/test/pom/Admin/AdminHome.java
@@ -1,6 +1,18 @@
 package com.revature.test.pom.Admin;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 //IMPORTANT: A lot of code for this POM already exists in the general POM Package.
 public class AdminHome {
 
+	private static List<WebElement> element;
+	
+	public static List<WebElement> getCharts(WebDriver driver) {	
+		element = driver.findElements(By.id("pie"));
+		return element;		
+	}
 }

5bb333b749767dace538fb02025d34b9bfed745f
werlej <werlej@hotmail.com>
2018-10-23 14:22:55 -0400

    my initial rewrite of the Trainer associate and batch list views -jw

--- TrackForce/src/test/java/com/revature/test/pom/Trainer/TrainerBatchList.java
+++ TrackForce/src/test/java/com/revature/test/pom/Trainer/TrainerBatchList.java
@@ -1,5 +1,14 @@
 package com.revature.test.pom.Trainer;
 
-public class TrainerBatchList {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class TrainerBatchList {
+	public WebElement getStartDateInput(WebDriver driver) {
+		return driver.findElement(By.id("startDate"));
+	}
+	public WebElement getEndDateInput(WebDriver driver) {
+		return driver.findElement(By.id("endDate"));
+	}
 }

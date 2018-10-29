1949f86c7481d7de3e9dfc9f6ff86d0a0c612ccb
Cory <cory.davisalan@yahoo.com>
2018-10-29 11:58:22 -0400

    Changed outdated methods in RegisterUserCuke.java to the new versions, also added small amount of documentation about unimplemented methods.

--- TrackForce/src/test/java/com/revature/test/cuke/RegisterUserCuke.java
+++ TrackForce/src/test/java/com/revature/test/cuke/RegisterUserCuke.java
@@ -1,5 +1,7 @@
 package com.revature.test.cuke;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import com.revature.test.pom.Login;
 import com.revature.test.utils.ServiceHooks;
 
@@ -12,82 +14,83 @@ public class RegisterUserCuke {
 	
 	@Given("^I click on the register button$")
 	public void i_click_on_the_register_button() throws Throwable {
-	    Login.clickRegisterButton(ServiceHooks.driver);
+	    Login.getRegisterButton(ServiceHooks.driver).click();
 	}
 
 	@When("^I enter a valid username$")
 	public void i_enter_a_valid_username() throws Throwable {
-	    Login.inputRegisterUsername(ServiceHooks.driver, "NewUserTom");
+	    Login.getUsername(ServiceHooks.driver).sendKeys("TestUsername");
 	}
 
 	@When("^I enter a valid password$")
 	public void i_enter_a_vaild_password() throws Throwable {
-		Login.inputRegisterPassword(ServiceHooks.driver, "Tom'spass!");
+		Login.getPassword(ServiceHooks.driver).sendKeys("Password");
 	}
 
 	@When("^I confirm my password$")
 	public void i_confirm_my_password() throws Throwable {
-		Login.confirmPassword(ServiceHooks.driver, "Tom'spass!");
+		Login.getConfirmPassword(ServiceHooks.driver).sendKeys("Password");
 	}
 
 	@When("^I enter a invalid firstname$")
 	public void i_enter_a_vaild_firstname() throws Throwable {
-
-	   Login.inputRegisterFirstName(ServiceHooks.driver, "$$$$");
-
+	   Login.getFirstName(ServiceHooks.driver).sendKeys("Bob");
 	}
 
 	@When("^I enter a invalid lastname$")
 	public void i_enter_a_vaild_lastname() throws Throwable {
-
-	    Login.inputRegisteLastName(ServiceHooks.driver, "@@@@34");
-
+	    Login.getLastName(ServiceHooks.driver).sendKeys("Builder");
 	}
 
 	@When("^I click register user$")
 	public void i_click_register_user() throws Throwable {
-	   Login.clickRegisterNewButton(ServiceHooks.driver);
+	   Login.getRegisterButton(ServiceHooks.driver).click();
 	}
 
 	@Then("^a new user should be registered$")
 	public void a_new_user_should_be_registered() throws Throwable {
-
 		//check that user is registered
-    //NEED TO IMPLEMENT
+		//Needs to use a verify method to check if the user has been successfully created.
 
 
 	}
 
 	@When("^I enter a invalid username \"([^\"]*)\"$")
 	public void i_enter_a_invalid_username(String username) throws Throwable {
-		Login.inputRegisterUsername(ServiceHooks.driver, username);
+		Login.getUsername(ServiceHooks.driver).sendKeys(username);
 	}
 
 	@Then("^an error message should appear$")
 	public void an_error_message_should_appear() throws Throwable {
-    
-		Login.getPopup(ServiceHooks.driver);
-
+		//Login.getPopup(ServiceHooks.driver);
+		//TODO: Error popup should appear if username/password are invalid
+		//Error popup should appear if password/confirmPassword do not match
+		//No methods in Page Object Model that grab the error popup
+		
 	}
 
 	@When("^I enter a invalid password\"([^\"]*)\"$")
 	public void i_enter_a_invalid_password(String password) throws Throwable {
-		Login.inputRegisterPassword(ServiceHooks.driver, password);
+		Login.getPassword(ServiceHooks.driver).sendKeys("password");
 	}
 
 	@When("^I confirm my invalid password\"([^\"]*)\"$")
 	public void i_confirm_my_invalid_password(String password) throws Throwable {
-		Login.confirmPassword(ServiceHooks.driver, password);
+		Login.getConfirmPassword(ServiceHooks.driver).sendKeys("password");
 	}
 	
 	@And("^I select an associate role$")
 	public void i_select_an_associate_role() throws Throwable {
-		Login.selectAssociateRole(ServiceHooks.driver);
+		Login.getSelectRole(ServiceHooks.driver).click();
+		ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(Login.getAssociateRole(ServiceHooks.driver)));
+		Login.getAssociateRole(ServiceHooks.driver).click();
 	}
 	
 	@And("^I select a trainer role$")
 	public void i_select_a_trainer_role() throws Throwable {
-		Login.selectTrainerRole(ServiceHooks.driver);
+		Login.getSelectRole(ServiceHooks.driver).click();
+		ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(Login.getTrainerRole(ServiceHooks.driver)));
+		Login.getTrainerRole(ServiceHooks.driver).click();
 	}
 	
 

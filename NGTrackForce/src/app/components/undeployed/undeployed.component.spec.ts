702ffc8d82ac1875dd8f928eda9ff5a23e9fecaa
bss1251 <43614982+bss1251@users.noreply.github.com>
2018-10-30 11:07:07 -0400

    Update undeployed.component.spec.ts

--- NGTrackForce/src/app/components/undeployed/undeployed.component.spec.ts
+++ NGTrackForce/src/app/components/undeployed/undeployed.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import {ChartsModule} from 'ng2-charts';
-import { appRoutes } from '../../routing/app-routing.module'
+//import { appRoutes } from '../../routing/app-routing.module'
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 

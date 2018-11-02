496fc8efd07a948f5cc554823bb71513208c6d86
Katelyn Barnes <kbarnes@hmc.edu>
2018-11-02 11:40:17 -0500

    Cleaned up AssociateResourceTest.java, testUpdateAssociateHappyPath currently fails with a 500 status code

--- TrackForce/src/main/java/com/revature/utils/HibernateUtil.java
+++ TrackForce/src/main/java/com/revature/utils/HibernateUtil.java
@@ -36,7 +36,6 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		try {
 			Configuration cfg = new Configuration();			
-
 			cfg.setProperty("hibernate.connection.url", System.getenv("TRACKFORCE_DB_URL"));
 			cfg.setProperty("hibernate.connection.username", System.getenv("TRACKFORCE_DB_USERNAME"));
 			cfg.setProperty("hibernate.connection.password", System.getenv("HBM_PW_ENV"));

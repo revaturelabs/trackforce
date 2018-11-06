726c24eb2dc208255316b3392334e8108bd5687b
Shane Russell <sr464@nau.edu>
2018-11-06 13:57:09 -0600

    Fixed Associate-List-Page Jasmine test. Creates component successfully, now.

--- NGTrackForce/src/app/components/associate-list-page/associate-list-page.component.spec.ts
+++ NGTrackForce/src/app/components/associate-list-page/associate-list-page.component.spec.ts
@@ -1,6 +1,26 @@
+import { SearchFilterPipeModule } from './../../pipes/search-filter/search-filter.module';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { AssociateService } from './../../services/associate-service/associate.service';
+import { ClientService } from './../../services/client-service/client.service';
+import { RequestService } from './../../services/request-service/request.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NavbarComponent } from './../navbar/navbar.component';
+import { NotFoundComponent } from './../not-found/not-found.component';
+import { AssociateViewComponent } from './../associate-view/associate-view.component';
+import { InvalidSessionComponent } from './../invalid-session/invalid-session.component';
+import { LoginComponent } from './../login/login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from './../../routing/app-routing.module';
+import { FormComponent } from './../form-component/form.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AssociateListPageComponent } from './associate-list-page.component';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { AssociateListRoutingModule } from './associate-list-page-routing.module';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 describe('AssociateListPageComponent', () => {
   let component: AssociateListPageComponent;
@@ -8,7 +28,29 @@ describe('AssociateListPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AssociateListPageComponent ]
+     declarations: [
+        AssociateListPageComponent,
+        LoginComponent,
+        InvalidSessionComponent,
+        FormComponent,
+        AssociateViewComponent,
+        NotFoundComponent,
+        NavbarComponent
+      ],
+      providers: [
+        RequestService,
+        AssociateService,
+        ClientService
+      ],
+      imports:[
+        FormsModule,
+        AppRoutingModule,
+        RouterTestingModule,
+        MatProgressSpinnerModule,
+        HttpClientModule,
+        OverlayModule,
+        MatDialogModule
+      ]
     })
     .compileComponents();
   }));

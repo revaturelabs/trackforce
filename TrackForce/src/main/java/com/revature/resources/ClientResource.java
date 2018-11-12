dffba61be94ce7c02e6641b86ef128455b4d28c1
Ben Slappey <slappeyb@gmail.com>
2018-11-11 22:11:31 -0600

    message

--- TrackForce/src/main/java/com/revature/resources/ClientResource.java
+++ TrackForce/src/main/java/com/revature/resources/ClientResource.java
@@ -16,8 +16,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import com.revature.entity.TfClient;
-import com.revature.entity.TfRole;
-import com.revature.entity.TfUser;
 import com.revature.services.AssociateService;
 import com.revature.services.BatchService;
 import com.revature.services.ClientService;
@@ -56,7 +54,10 @@ public class ClientResource {
 	InterviewService interviewService = new InterviewService();
 	TrainerService trainerService = new TrainerService();
 	UserService userService = new UserService();
-
+	Response badToken = null;
+	Response forbidden = null;
+	Response authorized = null;
+	
 	/**
 	 * 
 	 * @author Adam L.
@@ -73,76 +74,70 @@ public class ClientResource {
 	@Path("/getAll/")
 	@ApiOperation(value = "Returns all clients", notes = "Returns a map of all clients.")
 	public Response getAllClients(@HeaderParam("Authorization") String token) {
-		logger.info("getAllClients()...");
-		int[] level = { 1 };
-		if (JWTService.validateToken(token) == true) {
-			if (UserAuthentication.Authorized(token, level)) {
-				System.out.println("AUTHENTICATION PASSED");
-				Status status = null;
-				List<TfClient> clients = clientService.getAllTfClients();
-				Claims payload = JWTService.processToken(token);
-				System.out.println("TOKEN PROCESSED");
-				if (payload == null) {
-					return Response.status(Status.UNAUTHORIZED).entity(JWTService.invalidTokenBody(token)).build();
-				} else {
-					status = clients == null || clients.isEmpty() ? Status.NO_CONTENT : Status.OK;
-				}
-				return Response.status(status).entity(clients).build();
-			} else {
-				System.out.println("AUTHENTICATION FAILED");
-				return Response.status(403).entity(JWTService.forbiddenToken(token)).build();
-			}
-		} else {
-			System.out.println("TOKEN INVALID");
-			return Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
-		}
+		logger.info("Method call to getAllClients()...");
+		List<TfClient> clients = clientService.getAllTfClients();
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(clients).build();
+		Response authorized = Response.status(clients == null || clients.isEmpty() ? Status.NO_CONTENT : Status.OK).entity(clients).build();
+
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
+		
 	}
+
 
 	@GET
 	@Path("/associates/get/{client_id}")
-	public Response getMappedAssociatesByClientId(@PathParam("client_id") Long client_id,
-			@HeaderParam("Authorization") String token) {
-		int[] level = { 1 };
-		if (JWTService.validateToken(token)) {
-			if (UserAuthentication.Authorized(token, level)) {
-				Long[] response = new Long[4];
-				for (Integer i = 0; i < response.length; i++) {
-					response[i] = associateService.getMappedAssociateCountByClientId(client_id, i + 1);
-				}
-				return Response.status(200).entity(response).build();
-			} else {
-				return Response.status(403).entity(JWTService.forbiddenToken(token)).build();
-			}
-		} else {
-			return Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+	public Response getMappedAssociatesByClientId(@PathParam("client_id") Long client_id, @HeaderParam("Authorization") String token) {
+		Long[] response = new Long[4];
+		for (Integer i = 0; i < response.length; i++) {
+			response[i] = associateService.getMappedAssociateCountByClientId(client_id, i + 1);
 		}
+		
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(response).build();
+		Response authorized = Response.status(200).entity(response).build();
+		
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
 	}
 
 	@GET
 	@Path("/mapped/get/")
 	public Response getMappedClients(@HeaderParam("Authorization") String token) {
-		Claims payload = JWTService.processToken(token);
-		Status status = null;
-		int[] level = { 1 };
-		if (JWTService.validateToken(token)) {
-			if (UserAuthentication.Authorized(token, level)) {
-				if (payload == null) {
-					return Response.status(403).entity(clientService.getMappedClients()).build();
-				} else {
-					return Response.status(200).entity(clientService.getMappedClients()).build();
-				}
-			} else {
-				return Response.status(403).entity(JWTService.forbiddenToken(token)).build();
-			}
-		} else {
-			return Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
-		}
-	}	
+		List<TfClient> clients = clientService.getMappedClients();
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(clients).build();
+		Response authorized = Response.status(200).entity(clients).build();
+
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
+	}
 
 	@GET
 	@Path("/50/")
-	public Response getFirstFiftyClients() {
-		System.out.println("GET FIRST FIFTY CLIENTS CALLED");
-		return Response.status(200).entity(clientService.getFirstFiftyClients()).build();
+	public Response getFirstFiftyClients(@HeaderParam("Authorization") String token) {
+		List<TfClient> clients = clientService.getFirstFiftyClients();
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(clients).build();
+		Response authorized = Response.status(200).entity(clients).build();
+
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
 	}
-}
+	
+	public Response authorizeAdminUser(Response badToken, Response forbidden, Response authorized, String token) {
+		Claims payload = JWTService.processToken(token);
+		if (payload == null) {
+			return badToken;
+		} else {
+
+			if (JWTService.validateToken(token) == false) {
+				return forbidden;
+			} else {
+				int role = 0;
+				role = Integer.parseInt((String) payload.get("roleID"));
+				if (role == 1) {
+					return authorized;
+				} else {
+					return forbidden;
+				}
+			}
+		}
+}}
--- TrackForce/src/test/java/com/revature/test/restAssured/ClientResourceTest.java
+++ TrackForce/src/test/java/com/revature/test/restAssured/ClientResourceTest.java
@@ -15,7 +15,6 @@ import org.testng.annotations.Test;
 import com.revature.entity.TfClient;
 import com.revature.services.ClientService;
 import com.revature.services.JWTService;
-import com.revature.utils.EnvManager;
 
 import io.restassured.http.Header;
 import io.restassured.response.Response;
@@ -30,9 +29,9 @@ import io.restassured.specification.RequestSpecification;
  */
 public class ClientResourceTest {
 
-	//static final String URL = "http://52.87.205.55:8086/TrackForce/clients";
+	// static final String URL = "http://52.87.205.55:8086/TrackForce/clients";
 	static final String URL = "http://localhost:8085/TrackForce/clients";
-	//static final String URL = EnvManager.TomTrackForce_URL + "clients/";
+	// static final String URL = EnvManager.TomTrackForce_URL + "clients/";
 
 	ClientService cs = new ClientService();
 	List<TfClient> clients;
@@ -56,10 +55,8 @@ public class ClientResourceTest {
 
 	/**
 	 * Test that the resource can be accessed properly. Check that the content type
-	 * is what is expected. 
-	 * Test that a bad token gives a 401.
-	 * Test that a bad url gives a 404.
-	 * Test that a bad method gives a 405.
+	 * is what is expected. Test that a bad token gives a 401. Test that a bad url
+	 * gives a 404. Test that a bad method gives a 405.
 	 * 
 	 * @author Jesse
 	 * @since 6.18.06.13
@@ -78,113 +75,111 @@ public class ClientResourceTest {
 	}
 
 	/**
-	 * Unhappy path testing for testGetAllClients, tests that a 401 status code
-	 * is given if a request is made with a bad token
-	 * @author Katelyn B 
-	 * Written Nov. 4, 2018, Batch 1809
+	 * Unhappy path testing for testGetAllClients, tests thatn a 401 status code is
+	 * given if a request is made with a bad token
+	 * 
+	 * @author Katelyn B Written Nov. 4, 2018, Batch 1809
 	 */
-	@Test(priority = 10)
-	public void testGetAllClientsBadToken() {
-		given().header("Authorization", "Bad Token").when().get(URL + "/getAll/").then().assertThat().statusCode(401);
-		Response response = given().header("Authorization", "Bad Token").when().get(URL + "/getAll/").then().extract().response();
+	@Test(priority = 10, dataProvider = "urls")
+	public void testGetAllClientsBadToken(String url) {
+		String newURL = URL + url;
+		given().header("Authorization", "Bad Token").when().get(newURL).then().assertThat().statusCode(401);
+		Response response = given().header("Authorization", "Bad Token").when().get(newURL).then().extract().response();
 
 		assertTrue(response.statusCode() == 401);
 		assertTrue(response.asString().contains("Unauthorized"));
 	}
+
+	@Test(priority = 10, dataProvider = "urls")
+	public void testNoToken(String url) {
+		String newURL = URL + url;
+		String emptyToken = null;
+		given().header("Authorization", "").when().get(newURL).then().assertThat().statusCode(401);
+		Response response = given().header("Authorization", "").when().get(newURL).then().extract().response();
+
+		assertTrue(response.statusCode() == 401);
+		assertTrue(response.asString().contains("Unauthorized"));
+	}
+
 	/**
-	 * Unhappy path testing for testGetAllClient, tests that a 404 status code is generated for a 
-	 * bad URL
-	 * @author Katelyn B 
-	 * Written Nov. 4, 2018, Batch 1809
+	 * Unhappy path testing for testGetAllClient, tests that a 404 status code is
+	 * generated for a bad URL
+	 * 
+	 * @author Katelyn B Written Nov. 4, 2018, Batch 1809
 	 */
 	@Test(priority = 10)
 	public void testGetAllClientsBadUrl() {
-		Response response = given().header("Authorization", token).when().get(URL + "notAURL/").then().extract().response();
+		Response response = given().header("Authorization", token).when().get(URL + "notAURL/").then().extract()
+				.response();
 
 		assertTrue(response.statusCode() == 404);
 	}
-	
+
 	/**
-	 * Unhappy path testing for testGetAllClient, tests that a 405 status code is given for a
-	 * bad html verb
-	 * @author Katelyn B 
-	 * Written Nov. 4, 2018, Batch 1809
+	 * Unhappy path testing for testGetAllClient, tests that a 405 status code is
+	 * given for a bad html verb
+	 * 
+	 * @author Katelyn B Written Nov. 4, 2018, Batch 1809
 	 */
 	@Test(priority = 10)
 	public void testGetAllClientsBadVerb() {
-		Response response = given().header("Authorization", token).when().post(URL + "/getAll/").then().extract().response();
+		Response response = given().header("Authorization", token).when().post(URL + "/getAll/").then().extract()
+				.response();
 
 		assertTrue(response.statusCode() == 405);
 	}
-	
+
 	@Test(priority = 10)
 	public void testUserRole() {
 		String url = URL + "/getAll/";
 		given().header("Authorization", assocToken).when().get(url).then().assertThat().statusCode(403);
-		//Response response = given().header("Authorization", assocToken).when().post(URL + "getAll").then().extract().response();
-		//assertTrue(response.statusCode() == 403);
+		// Response response = given().header("Authorization",
+		// assocToken).when().post(URL + "getAll").then().extract().response();
+		// assertTrue(response.statusCode() == 403);
 	}
-	
+
 	@DataProvider(name = "urls")
 	public String[] getURLs() {
-		return new String[] { 
-			 "/getAll/", 
-			 "/associates/get/0",
-			 "/mapped/get/" 
-			// "/50/"  
-			};
+		return new String[] { "/getAll/", "/associates/get/0", "/mapped/get/", "/50/" };
 	}
-	
+
 	/**
 	 * An unhappy test method that checks every method but login to assure that
-	 * those that need security must block the user if he: 
-	 * 1. uses an unsupported verb.
-	 * 2. uses a service without a token
-	 * 3. uses a bad token. 
-	 * 4. the role in the
-	 * token deemed the user unauthorized to use the service As this method of
+	 * those that need security must block the user if he: 1. uses an unsupported
+	 * verb. 2. uses a service without a token 3. uses a bad token. 4. the role in
+	 * the token deemed the user unauthorized to use the service As this method of
 	 * unhappy testing is consistent, I (Seth L.) suggest using this and its
 	 * dependent methods to perform unhappy tests for all Jersey resources. Note:
 	 * All unhappy tests failed. The ClientResource needs improved security. Same
 	 * goes for /batch/{id}
 	 * 
 	 * @author Seth L.
-	 * @param method   - comma-separated list of HTTP Verbs that the service uses
-	 * @param url      - the uri for the service
-	 * @param needAuth - if the user has to be a higher level of authorization than
-	 *                 Associate like Admin to use the service, this value is true;
+	 * @param method
+	 *            - comma-separated list of HTTP Verbs that the service uses
+	 * @param url
+	 *            - the uri for the service
+	 * @param needAuth
+	 *            - if the user has to be a higher level of authorization than
+	 *            Associate like Admin to use the service, this value is true;
 	 */
-	@Test(enabled = true, priority = 11, dataProvider = "urls")
-	public void unhappyPathTest(String method, String url, Boolean needAuth) {
-		String adminToken = token;
-		String assocToken = JWTService.createToken("AssociateTest", 5);
-		String[] verbs = method.split(", ");
-		url = URL + url;
+	@Test(enabled = true, priority = 15, dataProvider = "urls")
+	public void unhappyPathTest(String url) {
+		String newURL = URL + url;
 		// test no token
-		for (String verb : verbs) {
-			sendRequest(verb, url, null).then().assertThat().statusCode(401);
-			// test invalid token
-			sendRequest(verb, url, new Header("Authorization", "badtoken")).then().assertThat().statusCode(401);
-			// test with associate token
-			if (needAuth)
-				sendRequest(verb, url, new Header("Authorization", assocToken)).then().assertThat().statusCode(403);
-		}
+		given().header("Authorization", "").when().get(newURL).then().assertThat().statusCode(401);
+		// test invalid token
+		given().header("Authorization", "Bad Token").when().get(newURL).then().assertThat().statusCode(401);
+		// test with associate token
+		given().header("Authorization", assocToken).when().get(newURL).then().assertThat().statusCode(403);
 		// look for an HTTP verb not used
-		String knownVerbs[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+		String knownVerbs[] = new String[] { "POST", "PUT", "DELETE" };
 		for (String verb : knownVerbs) {
-			if (!method.contains(verb)) {
-				// test unsupported verb
-				sendRequest(verb, url, new Header("Authorization", adminToken)).then().assertThat().statusCode(405);
-				break;
-			}
+			// test unsupported verb
+			sendRequest(verb, newURL, new Header("Authorization", token)).then().assertThat().statusCode(405);
 		}
 
 	}
 
-	/*
-	 * This method performs the appropriate request based on the HTTP verb and
-	 * returns the response
-	 */
 	private Response sendRequest(String method, String url, Header h) {
 		RequestSpecification given = given().contentType("application/json");
 		if (h != null)
@@ -202,16 +197,4 @@ public class ClientResourceTest {
 			return given().when().get();
 		}
 	}
-/*
-	@DataProvider(name = "urls")
-	public Object[][] getURLs() {
-		return new Object[][] { 
-			{ "GET", "", new Boolean(true) }, 
-			{ "GET", "associates/get/0/", new Boolean(true) },
-			{ "GET", "mapped/get/", new Boolean(true) }, 
-			{ "GET", "50/", new Boolean(true) } };
-	}*/
-	
-	
 }
-

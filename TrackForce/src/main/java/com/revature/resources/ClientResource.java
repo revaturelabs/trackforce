488dc3fb668bd98474fcee82530271cf98d19e4e
Michael Tinning <mwtinning@gmail.com>
2019-01-24 13:31:00 -0600

    Integrated all restAssured tests left unimplemented by previous batch. Some still break, but less and with more coverage.

--- TrackForce/src/main/java/com/revature/resources/ClientResource.java
+++ TrackForce/src/main/java/com/revature/resources/ClientResource.java
@@ -1,7 +1,5 @@
 package com.revature.resources;
 
-import static com.revature.utils.LogUtil.logger;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -14,6 +12,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.apache.log4j.Logger;
 
 import com.revature.entity.TfClient;
 import com.revature.services.AssociateService;
@@ -40,6 +40,7 @@ import io.swagger.annotations.ApiOperation;
 @Consumes({ MediaType.APPLICATION_JSON })
 @Produces({ MediaType.APPLICATION_JSON })
 public class ClientResource {
+	private final static Logger logger = Logger.getLogger(ClientResource.class);
 
 	// You're probably thinking, why would you ever do this? Why not just just make
 	// the methods all static in the service class?
@@ -53,7 +54,18 @@ public class ClientResource {
 	InterviewService interviewService = new InterviewService();
 	TrainerService trainerService = new TrainerService();
 	UserService userService = new UserService();
-
+	Response badToken = null;
+	Response forbidden = null;
+	Response authorized = null;
+	
+	/*
+	 * Each of the methods below builds three different responses determined by the method
+	 * authorizeAdminUser whether or not the 1) token is bad, 2) if the token is either expired 
+	 * or the username does not match the token username or 3) if the token is good, is valid, 
+	 * and the user role is equal to 1, designating an administrator which is the only role 
+	 * which should be permitted to view the client list. 
+	 *
+	 */
 	/**
 	 * 
 	 * @author Adam L.
@@ -67,45 +79,87 @@ public class ClientResource {
 	 * @throws IOException
 	 */
 	@GET
+	@Path("/getAll/")
 	@ApiOperation(value = "Returns all clients", notes = "Returns a map of all clients.")
 	public Response getAllClients(@HeaderParam("Authorization") String token) {
-		logger.info("getAllClients()...");
-		Status status = null;
+		logger.info("Method call to getAllClients()...");
 		List<TfClient> clients = clientService.getAllTfClients();
-		Claims payload = JWTService.processToken(token);
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(clients).build();
+		Response authorized = Response.status(clients == null || clients.isEmpty() ? Status.NO_CONTENT : Status.OK).entity(clients).build();
 
-		if (payload == null) {
-			return Response.status(Status.UNAUTHORIZED).entity(JWTService.invalidTokenBody(token)).build();
-		}
-		// invalid token
-		else {
-			status = clients == null || clients.isEmpty() ? Status.NO_CONTENT : Status.OK;
-		}
-		
-		return Response.status(status).entity(clients).build();
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
 	}
 
 	@GET
 	@Path("/associates/get/{client_id}")
-	public Response getMappedAssociatesByClientId(@PathParam("client_id") Long client_id) {
+	public Response getMappedAssociatesByClientId(@PathParam("client_id") Long client_id, @HeaderParam("Authorization") String token) {
 		Long[] response = new Long[4];
 		for (Integer i = 0; i < response.length; i++) {
 			response[i] = associateService.getMappedAssociateCountByClientId(client_id, i + 1);
 		}
-		return Response.status(200).entity(response).build();
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(response).build();
+		Response authorized = Response.status(200).entity(response).build();
+
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
 	}
-	
+
 	@GET
 	@Path("/mapped/get/")
-	public Response getMappedClients() {
-		return Response.status(200).entity(clientService.getMappedClients()).build();
+	public Response getMappedClients(@HeaderParam("Authorization") String token) {
+		List<TfClient> clients = clientService.getMappedClients();
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(clients).build();
+		Response authorized = Response.status(200).entity(clients).build();
+
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
 	}
-	
+
 	@GET
 	@Path("/50/")
-	public Response getFirstFiftyClients() {
-		return Response.status(200).entity(clientService.getFirstFiftyClients()).build();
+	public Response getFirstFiftyClients(@HeaderParam("Authorization") String token) {
+		List<TfClient> clients = clientService.getFirstFiftyClients();
+		Response badToken = Response.status(401).entity(JWTService.invalidTokenBody(token)).build();
+		Response forbidden = Response.status(403).entity(clients).build();
+		Response authorized = Response.status(200).entity(clients).build();
+
+		return authorizeAdminUser(badToken, forbidden, authorized, token);
 	}
 	
-	
-}
+	/**
+	 * 
+	 *Created to abstract out the authorization portion for each of the above methods in the 
+	 *ClientResource class. Allows each to return a response unique to its purpose. 
+	 *@author Ashley R
+	 *Written 11 Nov 2018
+	 */
+	public Response authorizeAdminUser(Response badToken, Response forbidden, Response authorized, String token) {
+		//Processes the token and returns the payload
+		Claims payload = JWTService.processToken(token);
+		logger.info("Processing the user's JWT.");
+		//Checks to see if payload is null, and if so assigns it as being an invalid token body 
+		if (payload == null) {
+			return badToken;
+		} else {
+			//Ensures the token is unexpired and the username on the token matches that
+			//of the logged in user. If it is not valid, assigns it a status of forbidden
+			if (new JWTService().validateToken(token) == false) {
+				logger.info("Checking to see if the JWT is valid.");
+				return forbidden;
+			} else {
+				//The roleID is checked from the decrypted token. Since only administrators
+				//are allowed to view the client lists, only a value of 1 is permitted 
+				//to return with a status of 200. 
+				int role = 0;
+				role = Integer.parseInt((String) payload.get("roleID"));
+				logger.info("Returning user roleID.");
+				if (role == 1) {
+					logger.info("User authorized to view client list.");
+					return authorized;
+				} else {
+					return forbidden;
+				}
+			}
+		}
+}}
--- TrackForce/src/test/java/com/revature/test/restAssured/UserResourceTest.java
+++ TrackForce/src/test/java/com/revature/test/restAssured/UserResourceTest.java
@@ -2,20 +2,24 @@ package com.revature.test.restAssured;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.revature.entity.TfAssociate;
+import com.revature.entity.TfMarketingStatus;
 import com.revature.entity.TfRole;
+import com.revature.entity.TfTrainer;
 import com.revature.entity.TfUser;
 import com.revature.entity.TfUserAndCreatorRoleContainer;
 import com.revature.resources.UserResource;
 import com.revature.services.JWTService;
 import com.revature.services.MarketingStatusService;
 import com.revature.services.UserService;
-import com.revature.utils.EnvManager;
-
+import com.revature.utils.HibernateUtil;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -29,17 +33,17 @@ import io.restassured.specification.RequestSpecification;
  * @author Jesse
  * @since 06.18.06.16
  */
-/*
- * Note by Seth L.: Every unhappy path test should have these test scenarios: 
- * 1. Using an unsupported method. 
- * 2. Not using a token. 
- * 3. Using a bad token.
- * 4. Using an unauthorized token (i.e. associate token).
- * 5. Bad data.
+/* Note by Seth L.: Every unhappy path test should have these test scenarios:
+ *   1. Using an unsupported method
+ *   2. Not using a token
+ *   3. Using a bad token
+ *   4. Using an unauthorized token (i.e. associate token)
+ *   5. Bad data
  */
 public class UserResourceTest {
 
-	static final String URL = EnvManager.TomTrackForce_URL + "users/";
+	static final String URL = "http://52.87.205.55:8086/TrackForce/users";
+	//static final String URL = "http://localhost:8085/TrackForce/users";
 
 	TfUser user;
 	TfUserAndCreatorRoleContainer container;
@@ -50,7 +54,6 @@ public class UserResourceTest {
 	int knownTrainerId;
 	Header adminTokenHeader, assocTokenHeader, badTokenHeader;
 	String username, password;
-
 	@BeforeClass
 	public void beforeClass() {
 		username = "TestUsernameChris";
@@ -59,71 +62,65 @@ public class UserResourceTest {
 		badTokenHeader = new Header("Authorization", "badtoken");
 		assocTokenHeader = new Header("Authorization", JWTService.createToken("cyril", 5));
 		knownTrainerId = 64832;
-
+		
 		role = new TfRole();
 		role = userService.getRole(1);
-
+		
 		user = new TfUser();
 		user.setIsApproved(1);
 		user.setPassword(password);
 		user.setUsername(username);
 		user.setTfRole(role);
 		user.setRole(1);
-
+		
 		container = new TfUserAndCreatorRoleContainer(user, 1);
 	}
 
 	/**
-	 * 1806_Chris_P: The way that the following methods are currently setup, you can
-	 * NOT use a REST call. If you do, the password will become null due to
-	 * the @JSONIGNORE annotation on the password in the TfUser class. The rest call
-	 * works with the Angular since the user passed in that way is not technically a
-	 * TfUser Java class and therefore does not have its password nulled.
+	 * 1806_Chris_P: The way that the following methods are currently setup, you can NOT use a REST call. 
+	 * If you do, the password will become null due to the @JSONIGNORE annotation on the password in the TfUser class.
+	 * The rest call works with the Angular since the user passed in that way is not technically a TfUser Java class
+	 * and therefore does not have its password nulled. 
 	 * 
-	 * 1808_Seth_L: Following the previous comment because of the @JSONIGNORE
-	 * annotation, I have to manually create the JSON for the TFUser so I could pass
-	 * the password directly. Unfortunately there is no way to delete the user once
-	 * its created and there are some ways around this: 
-	 * 1. randomly generate a username string everytime. Pros: No time wasted reloading the database
-	 * everytime a test needs to be made. Cons: Loads the database with dummy
-	 * entries. Not a problem with a test database but would be for a production
-	 * one.
-	 * 2. Reload the database before every test Pros: Fresh and "untouched"
-	 * data reduces complications of running tests. Cons: time-consuming.
-	 * 3. Manually deleting the entry from the database. Happy path testing for create user.
-	 * This should create an admin, staging manager or sales user when each of those
-	 * specified roles is used.
+	 * 1808_Seth_L: Following the previous comment because of the @JSONIGNORE annotation, I have to manually create the JSON
+	 * for the TFUser so I could pass the password directly.
+	 * Unfortunately there is no way to delete the user once its created and there are some ways around this:
+	 * 1. randomly generate a username string everytime.
+	 * 		Pros: No time wasted reloading the database everytime a test needs to be made.
+	 * 		Cons: Loads the database with dummy entries.  Not a problem with a test database but would be for a production one.
+	 * 2. Reload the database before every test
+	 * 		Pros: Fresh and "untouched" data reduces complications of running tests.
+	 * 		Cons: time-consuming
+	 * 3. Manually deleting the entry from the database.
+	 * Happy path testing for create user. This should create an admin, staging
+	 * manager or sales user when each of those specified roles is used.
 	 * 
-	 * Issue: This test would work but the backend would have to restart everytime
-	 * because once a user is created it is saved to the database and the backend's
-	 * Hibernate cache. This test deletes the user from the database, but the user
-	 * object still persists in the cache resulting in a 417 message. This test can
-	 * run multiple times if there is a way to delete that detached object too.
-	 * Issue 2: As of 10/30/18 I have discovered that this test fails because of a
-	 * server-side error that needs looking into. - Seth L.
-	 * 
+	 * Issue: This test would work but the backend would have to restart everytime because once a user is created it is saved to the database
+	 * and the backend's Hibernate cache.  This test deletes the user from the database, but the user object still persists in the cache
+	 * resulting in a 417 message.  This test can run multiple times if there is a way to delete that detached object too.
+	 * Issue 2: As of 10/30/18 I have discovered that this test fails because of a server-side error that needs looking into. - Seth L.
 	 * @author Jesse and Seth L.
 	 * @since 06.18.06.16
 	 */
 	@Test(enabled = true, priority = 10)
 	public void testCreateUser1() {
-		// manually constructed
-		String jsonContainer = "{\"user\": { \"username\":\"" + container.getUser().getUsername()
-				+ "\", \"password\":\"" + container.getUser().getPassword() + "\", \"role\":"
-				+ container.getUser().getRole() + "}, \"creatorRole\":" + container.getCreatorRole() + "}";
-		given().header(adminTokenHeader).contentType("application/json").body(jsonContainer).when()
-				.post(URL + "newUser/").then().assertThat().statusCode(201);
+		//manually constructed
+		String jsonContainer = "{\"user\": { \"username\":\"" + container.getUser().getUsername() + "\", \"password\":\"" +
+				container.getUser().getPassword() + "\", \"role\":"+ container.getUser().getRole() + "}, \"creatorRole\":" +
+				container.getCreatorRole() + "}";
+		given().header(adminTokenHeader).contentType("application/json").body(jsonContainer)
+			.when().post(URL + "/newUser").then().assertThat().statusCode(201);
 //		Response r = given().header(adminTokenHeader).contentType("application/json").body(jsonContainer).when().post(URL+"/newUser");
 		TfUser newUser = userService.getUser(username);
 
 		assertEquals(user.getUsername(), newUser.getUsername());
-		// assertEquals(2, 1); //results in expected [1] but found [2]
+		//assertEquals(2, 1); //results in expected [1] but found [2]
 		assertEquals(user.getTfRole().getTfRoleId(), newUser.getTfRole().getTfRoleId());
 		assertEquals(user.getIsApproved(), newUser.getIsApproved());
-		// should expect error if reentering the same data
-		given().header(adminTokenHeader).contentType("application/json").body(container).when().post(URL + "newUser/")
-				.then().assertThat().statusCode(417);
-		// an unused method to delete a user
+		//should expect error if reentering the same data
+		given().header(adminTokenHeader).contentType("application/json").body(container)
+			.when().post(URL + "/newUser").then().assertThat().statusCode(417);
+		//an unused method to delete a user
 		userService.deleteUser(newUser);
 	}
 
@@ -143,81 +140,74 @@ public class UserResourceTest {
 
 	/**
 	 * More unhappy path testing to ensure that verbs and URLs return the
-	 * appropriate status code disabled because the unhappy Path Test tests this
-	 * endpoint. - Seth L.
-	 * 
+	 * appropriate status code
+	 * disabled because the unhappy Path Test tests this endpoint. - Seth L.
 	 * @author Jesse
 	 * @since 06.18.06.16
 	 */
 	@Test(enabled = false, priority = 15)
 	public void testCreateUserUnhappyPath() {
-
-		// not using a token
-		given().contentType("application/json").body(container).when().get(URL + "newUser/").then().assertThat()
+		
+		//not using a token
+		given().contentType("application/json").body(container).when().get(URL + "/newUser").then().assertThat()
 				.statusCode(405);
-		// using the wrong http method
-		given().contentType("application/json").header(adminTokenHeader).body(container).when()
-				.post(URL + "newUserBADURL/").then().assertThat().statusCode(404);
-		// using an invalid token
-		given().contentType("application/json").header(badTokenHeader).body(container).when().post(URL + "newUser/")
-				.then().assertThat().statusCode(401);
-		// using an unauthorized token
-		given().contentType("application/json").header(assocTokenHeader).body(container).when().post(URL + "newUser/")
-				.then().assertThat().statusCode(403);
+		//using the wrong http method
+		given().contentType("application/json").header(adminTokenHeader).body(container).when().post(URL + "/newUserBADURL").then().assertThat()
+				.statusCode(404);
+		//using an invalid token
+		given().contentType("application/json").header(badTokenHeader).body(container).when().post(URL + "/newUser").then().assertThat()
+				.statusCode(401);
+		//using an unauthorized token
+		given().contentType("application/json").header(assocTokenHeader).body(container).when().post(URL + "/newUser").then().assertThat()
+				.statusCode(403);
 	}
-
 	/**
-	 * An unhappy test method that checks every method but login to assure that
-	 * those that need security must block the user if he:
-	 * 1. uses an unsupported verb. 
-	 * 2. uses a service without a token.
-	 * 3. uses a bad token.
-	 * 4. the role in the token deemed the user unauthorized to use the service As this method of
-	 * unhappy testing is consistent, I (Seth L.) suggest using this and its
-	 * dependent methods to perform unhappy tests for all Jersey resources. Only
-	 * submitCredentials is not used here because it does not require any tokens to
-	 * use.
-	 * 
+	 * An unhappy test method that checks every method but login to assure that those that need security must
+	 * block the user if he:
+	 * 1. uses an unsupported verb
+	 * 2. uses a service without a token
+	 * 3. uses a bad token
+	 * 4. the role in the token deemed the user unauthorized to use the service
+	 * As this method of unhappy testing is consistent, I (Seth L.) suggest using this and its dependent methods to perform unhappy tests for all
+	 * Jersey resources.
+	 * Only submitCredentials is not used here because it does not require any tokens to use.
 	 * @author Seth L.
-	 * @param method   - comma-separated list of HTTP Verbs that the service uses
-	 * @param url      - the uri for the service
-	 * @param needAuth - if the user has to be a higher level of authorization than
-	 *                 Associate like Admin to use the service, this value is true;
+	 * @param method - comma-separated list of HTTP Verbs that the service uses
+	 * @param url - the uri for the service
+	 * @param needAuth - if the user has to be a higher level of authorization than Associate like Admin to use the service, this value is true;
 	 */
 	@Test(enabled = true, priority = 20, dataProvider = "urls")
 	public void unhappyPathTest(String method, String url, Boolean needAuth) {
 		String[] verbs = method.split(", ");
 		url = URL + url;
-		// test no token
-		for (String verb : verbs) {
-			sendRequest(verb, url, null).then().assertThat().statusCode(401);
-			// test invalid token
+		//test no token
+		for(String verb : verbs) {
+			sendRequest(verb,url, null).then().assertThat().statusCode(401);
+			//test invalid token
 			sendRequest(verb, url, badTokenHeader).then().assertThat().statusCode(401);
-			// test with associate token
-			if (needAuth)
+			//test with associate token
+			if(needAuth)
 				sendRequest(verb, url, assocTokenHeader).then().assertThat().statusCode(403);
 		}
-		// look for an HTTP verb not used
-		String knownVerbs[] = new String[] { "GET", "POST", "PUT", "DELETE" };
-		for (String verb : knownVerbs) {
-			if (!method.contains(verb)) {
-				// test unsupported verb
+		//look for an HTTP verb not used
+		String knownVerbs[] = new String[] {"GET", "POST", "PUT", "DELETE"};
+		for(String verb : knownVerbs) {
+			if(!method.contains(verb)) {
+				//test unsupported verb
 				sendRequest(verb, url, adminTokenHeader).then().assertThat().statusCode(405);
 				break;
 			}
 		}
-
+		
 	}
-
 	/*
-	 * This method performs the appropriate request based on the HTTP verb and
-	 * returns the response
+	 * This method performs the appropriate request based on the HTTP verb and returns the response
 	 */
 	private Response sendRequest(String method, String url, Header h) {
 		RequestSpecification given = given().contentType("application/json").body(container);
-		if (h != null)
+		if(h != null)
 			given.header(h);
-		switch (method) {
+		switch(method) {
 		case "GET":
 			return given.when().get(url);
 		case "POST":
@@ -230,34 +220,32 @@ public class UserResourceTest {
 			return given().when().get();
 		}
 	}
-
-	@DataProvider(name = "urls")
+	@DataProvider(name = "urls") 
 	public Object[][] getURLs() {
-		return new Object[][] { { "POST", "newUser/", new Boolean(true) }, { "GET", "check/", new Boolean(false) },
-				{ "GET", "getUserRole/", new Boolean(false) } };
+		return new Object[][]  {{"POST", "/newUser", new Boolean(true)},
+								{"GET", "/check", new Boolean(false)},
+								{"GET", "/getUserRole", new Boolean(false)}};
 	}
-
 	/**
-	 * 1806_Chris_P: This used to test invalid credentials as well, but this
-	 * conflicted with the way the front end was handling responses, so that section
-	 * got pulled out.
+	 * 1806_Chris_P: This used to test invalid credentials as well, but this conflicted 
+	 * with the way the front end was handling responses, so that section got pulled out.
 	 * 
 	 * 
-	 * Test to ensure that a 200 is returned with valid credentials and a 401
-	 * UNAUTHORIZED is returned with invalid credentials. Also ensure that a valid
-	 * username with an invalid password cannot log in.
+	 * Test to ensure that a 200 is returned with valid credentials and a 401 UNAUTHORIZED is
+	 * returned with invalid credentials. Also ensure that a valid username with an
+	 * invalid password cannot log in.
 	 * 
 	 * @author Jesse and Seth L.
 	 * @since 06.18.06.16
 	 */
 	@Test(enabled = true, priority = 40)
 	public void testSubmitCredentials() {
-		// positive test
+		//positive test
 		given().contentType("application/json").body("{ \"username\": \"TestAdmin\", \"password\": \"TestAdmin\"}")
-				.post(URL + "login/").then().assertThat().statusCode(200);
-		// wrong password should result in an unauthorized response
+				.post(URL + "/login").then().assertThat().statusCode(200);
+		//wrong password should result in an unauthorized response
 		given().contentType("application/json").body("{ \"username\": \"TestAdmin\", \"password\": \"badpassword\"}")
-				.post(URL + "login/").then().assertThat().statusCode(401);
+				.post(URL + "/login").then().assertThat().statusCode(401);
 	}
 
 	/**
@@ -269,45 +257,40 @@ public class UserResourceTest {
 	 */
 	@Test(enabled = true, priority = 45)
 	public void testSubmitCredentialsUnhappyPath() {
-		// wrong http method
+		//wrong http method
 		given().contentType("application/json").body("{ \"username\": \"TestAdmin\", \"password\": \"TestAdmin\"}")
-				.when().get(URL + "login/").then().assertThat().statusCode(405);
-		// wrong password
+				.when().get(URL + "/login").then().assertThat().statusCode(405);
+		//wrong password
 		given().contentType("application/json").body("{ \"username\": \"TestAdmin\", \"password\": \"TestAdmin\"}")
-				.when().post(URL + "loginBad/").then().assertThat().statusCode(404);
+				.when().post(URL + "/loginBad").then().assertThat().statusCode(404);
 	}
-
 	/**
-	 * Simple test for the token validation method. Assert that valid token results
-	 * in an OK response and an invalid token results in an UNAUTHORIZED response.
-	 * 
+	 * Simple test for the token validation method.  Assert that valid token results in an OK response and an invalid
+	 * token results in an UNAUTHORIZED response.
 	 * @author Seth L.
 	 */
 	@Test(enabled = true, priority = 50)
 	public void testTokenCheck() {
-		// good token should pass
-		given().header(assocTokenHeader).when().get(URL + "check/").then().assertThat().statusCode(200);
-		// bad token should fail
-		given().header(badTokenHeader).when().get(URL + "check/").then().assertThat().statusCode(401);
+		//good token should pass
+		given().header(assocTokenHeader).when().get(URL + "/check").then().assertThat().statusCode(200);
+		//bad token should fail
+		given().header(badTokenHeader).when().get(URL + "/check").then().assertThat().statusCode(401);
 	}
-
 	/**
-	 * Verify that the getUserRole function returns the right role id and return 401
-	 * if the token is invalid
-	 * 
+	 * Verify that the getUserRole function returns the right role id and return 401 if the token is invalid
 	 * @author Seth L.
 	 */
 	@Test(enabled = true, priority = 55)
 	public void testRoleChecker() {
-		Response adminTokenResponse = given().header(adminTokenHeader).when().get(URL + "getUserRole/").andReturn();
+		Response adminTokenResponse = given().header(adminTokenHeader).when().get(URL + "/getUserRole").andReturn();
 		assertEquals(adminTokenResponse.getStatusCode(), 200);
 		assertEquals(adminTokenResponse.getBody().asString(), "1");
 		System.out.println(adminTokenResponse.getHeader("Allow"));
-		Response assocTokenResponse = given().header(assocTokenHeader).when().get(URL + "getUserRole/").andReturn();
+		Response assocTokenResponse = given().header(assocTokenHeader).when().get(URL + "/getUserRole").andReturn();
 		assertEquals(assocTokenResponse.getStatusCode(), 200);
 		assertEquals(assocTokenResponse.getBody().asString(), "5");
-
-		Response badTokenResponse = given().header(badTokenHeader).when().get(URL + "getUserRole/").andReturn();
+		
+		Response badTokenResponse = given().header(badTokenHeader).when().get(URL + "/getUserRole").andReturn();
 		assertEquals(badTokenResponse.getStatusCode(), 401);
 	}
 }

34ee074a5a67db7250dd0ab9f2bb9d2813699e5c
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2019-01-25 16:35:24 -0600

    fixed the associates login test. plan to add more test

--- TrackForce/src/test/java/com/revature/test/TestNG/AssociateTests.java
+++ TrackForce/src/test/java/com/revature/test/TestNG/AssociateTests.java
@@ -17,64 +17,60 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
-
-
 public class AssociateTests {
-  
-  @BeforeSuite
-  void startup() {
-	  /*
-	   * @ Jacob Golidng
-	   * Uncomment this line if you are working on a Mac OS to run this with test ng
-	   */
-	  //File chrome = new File("src/main/resources/chromeDriveer4Mac/chromedriver");
-	  // Uncomment this line you are working with Windows to run testng
-	 
-	  
-	  ServiceHooks.driver = WebDriverUtil.getChromeDriver();
-	  ServiceHooks.driver.get(EnvManager.NGTrackForce_URL);
-	  ServiceHooks.wait = new WebDriverWait(ServiceHooks.driver,5);
-  }
+	static WebDriver wd;
+	static WebDriverWait wait;
+	String username = "cyril";
+	String password = "cyril";
+	public final String url = "http://34.227.178.103:8090/NGTrackForce/";
+
+
+	@BeforeClass
+	public void LoadWebpage() {
+		File chrome = new File("src/main/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
+		wd = new ChromeDriver();
+		wd.get(url);// EnvManager.NGTrackForce_URL+"login/");
+		wait = new WebDriverWait(wd, 10);
+		wait.until(ExpectedConditions.urlContains(url));// EnvManager.NGTrackForce_URL+"login/"));
+		
+	}
+
+	@Test(priority = 0)
+	public void LoginAssociate() {
+		wait.until(ExpectedConditions.elementToBeClickable(Login.getUsername(wd)));
+		Login.getUsername(wd).sendKeys(username);
+		Login.getPassword(wd).sendKeys(password);
+		wait.until(ExpectedConditions.elementToBeClickable(Login.getSignInButton(wd)));
+		Login.getSignInButton(wd).click();
+		wait.until(ExpectedConditions.urlContains("/associate-view"));
+		
+	}
+
+	/*
+	 * making sure that the appropriate tabs are visible on the associates 
+	 * homepage
+	 */
+	@Test(dependsOnMethods= {"LoginAssociate"})
+	public void elementsOnPage() {
+		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.linkText("My Interviews")));
+	}
 	
-  @Test(priority = 0)
-  public void LoadWebpage() {
-	 ServiceHooks.driver.get(EnvManager.NGTrackForce_URL + "#/login/");
-	 ServiceHooks.wait.until(ExpectedConditions.urlContains("/login"));
-  }
-  
-  @Test (priority = 1)
-  public void LoginAssociate() {
-	 LoginUtil.loginAsAssociate(ServiceHooks.driver);
-	 Login.getSignInButton(ServiceHooks.driver).click();
-	 ServiceHooks.wait.until(ExpectedConditions.urlContains(EnvManager.NGTrackForce_URL+"associate-view/"));
-  }
-  
-  @Test (priority = 2)
-  public void LogOut() {
-	  
-	  ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(NavBar.getWelcomeDropdown(ServiceHooks.driver)));
-	  NavBar.getWelcomeDropdown(ServiceHooks.driver).click();
-	  ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(NavBar.getLogout(ServiceHooks.driver)));
-	  NavBar.getLogout(ServiceHooks.driver).click();
-	  ServiceHooks.wait.until(ExpectedConditions.urlContains(EnvManager.NGTrackForce_URL+"login/"));
-	  
-  }
-  @Test (priority = 3)
-  public void invalidLogin() {
-	  ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(Login.getUsername(ServiceHooks.driver)));
-		 Login.getUsername(ServiceHooks.driver).sendKeys("hat");
-		 ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(Login.getPassword(ServiceHooks.driver)));
-		 Login.getPassword(ServiceHooks.driver).sendKeys("bat");
-		 ServiceHooks.wait.until(ExpectedConditions.elementToBeClickable(Login.getSignInButton(ServiceHooks.driver)));
-		 Login.getSignInButton(ServiceHooks.driver).click();
-		 ServiceHooks.wait.until(ExpectedConditions.visibilityOf(Login.getErrorPopup(ServiceHooks.driver)));
-  }
-  
-  @AfterSuite
+	@Test(priority = 1)
+	public void LogOut() {
+		wait.until(ExpectedConditions.elementToBeClickable(NavBar.getWelcomeDropdown(wd)));
+		NavBar.getWelcomeDropdown(wd).click();
+		wait.until(ExpectedConditions.elementToBeClickable(NavBar.getLogout(wd)));
+		NavBar.getLogout(wd).click();
+		wait.until(ExpectedConditions.urlContains(url));//EnvManager.NGTrackForce_URL + "login/"));
+	}
+
+	@AfterSuite
 	void quit() {
-	  ServiceHooks.driver.quit();
-  }
+		wd.quit();
+	}
 
 }

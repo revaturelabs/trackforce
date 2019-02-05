87c8066f5531e905a062cfd9cefb19cf0a80dca6
Michael Tinning <mwtinning@gmail.com>
2019-02-05 09:38:26 -0600

    Created Resource.Test package for unit tests of resources

---
+++ TrackForce/src/test/java/com/revature/test/resources/ClientResource.java
@@ -0,0 +1,19 @@
+package com.revature.test.resources;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class ClientResource {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}

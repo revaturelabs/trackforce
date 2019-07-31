4fa5234cc84e0778519bfa63af20ffe158f5d945
cadamos <31325509+cadamos@users.noreply.github.com>
2019-07-31 12:16:46 -0400

    Sprint metrics

--- S3Bucket/s3-bucket-app/src/app/components/sprint-reports/sprint-reports.component.ts
+++ S3Bucket/s3-bucket-app/src/app/components/sprint-reports/sprint-reports.component.ts
@@ -20,17 +20,17 @@ export class SprintReportsComponent implements OnInit {
   project : string;
   link : string;
   submitted : boolean;
+  bucketName : string;
   
 
   constructor(private uploadService:UploadService) { }
 
   ngOnInit() {
-
     this.projectList = this.uploadService.getProjectList();
     this.fileList = [];
     this.project= "";
-    this.link = "#"; // find out link later
     this.submitted = false;
+    this.bucketName = "ccoverage"; // temp
   }
 
   showIterations(value : string) {
@@ -41,18 +41,38 @@ export class SprintReportsComponent implements OnInit {
 
   selectFiles(event) {
     this.fileList.push(event.target.files.item(0));
-    console.log(this.fileList);
   }
 
-  setProject(value: string) {
-    this.project = value;
+  setProject(project: string) {
+    this.project = project;
+  }
+
+  createLink(iter: string) {
+    this.link = "https://" + this.bucketName + ".s3.amazonaws.com/" + this.projectChoice + "/" + iter + "/index.html";
   }
 
 
   submit() {
     // send this.fileList, this.iteration, this.project, and this.index to S3 bucket
     this.submitted = true;
-    this.index = new File(['<head><title>HI</title></head><body>Hello World.... MicDrop</body>'], "index.html", {type: "text/html"});
+    this.index = new File(
+      [`
+      <html>
+        <head>
+          <title>${this.iteration}</title>
+        </head>
+        <body>
+          <h1>Sprint Metrics:</h1>
+          <b>Project:</b> ${this.project} <br>
+          <b>Iteration:</b> ${this.iteration} <br>
+          <b>Velocity:</b> PUT THINGS HERE <br>
+          <b>Start Date:</b> PUT THINGS HERE <br>
+          <b>End Date:</b> PUT THINGS HERE <br>
+          <b>Files:</b>${this.fileList.map(file => `<a href="report/${file.name}" target="_blank">${file.name}</a>`)}
+        </body>
+      </html>
+      `]
+      , "index.html", {type: "text/html"});
     console.log("Project: " + this.project);
     console.log("Iteration: " + this.iteration);
     console.log("Files: " + this.fileList);
@@ -64,6 +84,16 @@ export class SprintReportsComponent implements OnInit {
     });
       this.uploadService.uploadReport(this.index, this.project, this.iteration+"/index.html")
     console.log(this.index);
+    setTimeout( () => {
+      this.resetValues();
+    }, 2000);
+  }
+
+  resetValues() {
+    this.submitted = false;
+    this.fileList = [];
+    this.project = "";
+    this.iteration = "";
   }
 
 }

550bd807596a16fd58bf07b34d08482525bc481a
cadamos <31325509+cadamos@users.noreply.github.com>
2019-07-31 13:57:43 -0400

    formatting

--- S3Bucket/s3-bucket-app/src/app/components/sprint-reports/sprint-reports.component.ts
+++ S3Bucket/s3-bucket-app/src/app/components/sprint-reports/sprint-reports.component.ts
@@ -18,29 +18,27 @@ export class SprintReportsComponent implements OnInit {
   iteration : string;
   iterationList: Observable<Array<string>>;
   project : string;
-  link : string;
+  iterationLink : string;
   submitted : boolean;
   bucketName : string;
-  startDate: Date;
-  endDate: Date;
+  inputStartDate: string;
+  inputEndDate: string;
   completedStoryPoints: number;
   assignedStoryPoints: number;
   
-
   constructor(private uploadService:UploadService) { }
 
   ngOnInit() {
     this.projectList = this.uploadService.getProjectList();
     this.fileList = [];
-    this.project= "";
+    this.project = "";
     this.submitted = false;
     this.bucketName = "ccoverage"; // temp
   }
 
   showIterations(value : string) {
     this.projectChoice = value;
-    console.log(value)
-    this.iterationList=this.uploadService.getProjectSprints(value);
+    this.iterationList = this.uploadService.getProjectSprints(value);
   }
 
   selectFiles(event) {
@@ -52,15 +50,15 @@ export class SprintReportsComponent implements OnInit {
   }
 
   createLink(iter: string) {
-    this.link = "https://" + this.bucketName + ".s3.amazonaws.com/" + this.projectChoice + "/" + iter + "/index.html";
+    this.iterationLink = "https://" + this.bucketName + ".s3.amazonaws.com/" + this.projectChoice + "/" + iter + "/index.html";
   }
-
 
   submit() {
     // send this.fileList, this.iteration, this.project, and this.index to S3 bucket
     this.submitted = true;
-    const days = (this.endDate.getTime()-this.startDate.getTime())*1000*60*60*24
-    const velocity = (this.completedStoryPoints-this.assignedStoryPoints)/days;
+    let startDate = new Date(this.inputStartDate);
+    let endDate = new Date(this.inputEndDate);
+    const days = (endDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24;
     this.index = new File(
       [`
       <html>
@@ -71,18 +69,15 @@ export class SprintReportsComponent implements OnInit {
           <h1>Sprint Metrics:</h1>
           <b>Project:</b> ${this.project} <br>
           <b>Iteration:</b> ${this.iteration} <br>
-          <b>Velocity:</b> ${velocity} <br>
-          <b>Start Date:</b> ${this.startDate.toUTCString} <br>
-          <b>End Date:</b> ${this.endDate.toUTCString} <br>
-          <b>Story Points:</b> ${this.completedStoryPoints}/${this.assignedStoryPoints}<br>
-          <b>Files:</b>${this.fileList.map(file => `<a href="report/${file.name}" target="_blank">${file.name}</a>`)}
+          <b>Start Date:</b> ${this.inputStartDate}<br>
+          <b>End Date:</b> ${this.inputEndDate} <br>
+          <b>Duration:</b> ${days} days <br>
+          <b>Velocity:</b> ${this.completedStoryPoints}/${this.assignedStoryPoints}<br>
+          <b>Files:</b> ${this.fileList.map(file => `<br><a href="report/${file.name}" target="_blank">${file.name}</a>`)}
         </body>
       </html>
       `]
       , "index.html", {type: "text/html"});
-    console.log("Project: " + this.project);
-    console.log("Iteration: " + this.iteration);
-    console.log("Files: " + this.fileList);
     const proj = this.project;
     const iter = this.iteration;
     const uservice= this.uploadService;
@@ -90,7 +85,6 @@ export class SprintReportsComponent implements OnInit {
       uservice.uploadReport(file, proj, iter+"/report/"+ file.name)
     });
       this.uploadService.uploadReport(this.index, this.project, this.iteration+"/index.html")
-    console.log(this.index);
     setTimeout( () => {
       this.resetValues();
     }, 2000);
@@ -101,6 +95,10 @@ export class SprintReportsComponent implements OnInit {
     this.fileList = [];
     this.project = "";
     this.iteration = "";
+    this.inputStartDate = "";
+    this.inputEndDate = "";
+    this.assignedStoryPoints = undefined;
+    this.completedStoryPoints = undefined;
   }
 
 }

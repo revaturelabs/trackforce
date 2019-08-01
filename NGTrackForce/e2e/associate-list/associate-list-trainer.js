25abefff92eccdb172d82caec544643c3200d281
Wesley Jones <monsterzero7@gmail.com>
2019-08-01 11:23:00 -0400

    new trainer feature test

---
+++ NGTrackForce/e2e/associate-list/associate-list-trainer.js
@@ -0,0 +1,20 @@
+
+describe('Trainer only sees associates in their batches', function(){
+    browser.get('http://localhost:4200/#/login');
+    browser.driver.sleep(10000);
+    element(by.id('username')).sendKeys('Trainer0');
+    element(by.id('password')).sendKeys('Trainer');
+    browser.driver.sleep(5000);
+    element(by.id('associates')).click();
+    browser.driver.sleep(10000);
+    let associates = JSON.parse(localStorage.getItem('associatePage|/page?startIndex=0&numResults=60'));
+    let x = 0;
+    let validation = 0;
+    for (x = 0; x < associates.length; x++){
+        if (associate[x].firstName === 'Samuel'){
+            validation = 1;
+            break;
+        }
+    }
+    expect(validation).toBe(1);
+})

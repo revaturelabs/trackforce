9c0fb2abe183d2d0d4e6115fc64d51632c885a4f
mjrerle <mjrerle@gmail.com>
2019-08-03 16:28:05 -0400

    gitignore fix + some tests

--- S3Bucket/s3-bucket-app/e2e/src/view-report/viewReport.po.ts
+++ S3Bucket/s3-bucket-app/e2e/src/view-report/viewReport.po.ts
@@ -8,20 +8,37 @@ const Locators = {
   projectListSelector: {
     type: IdentificationType[IdentificationType.Id],
     value: 'projectChoice'
-  }
+  },
+  cmsOption: {
+    type: IdentificationType[IdentificationType.Xpath],
+    value: '//*[@id="projectChoice"]/option[1]'
+  },
+  selectedIteration: {
+    type: IdentificationType[IdentificationType.Id],
+    value: 'selectedIteration'
+  },
+  youAreWrongOption: {
+    type: IdentificationType[IdentificationType.Xpath],
+    value: '//*[@id="selectedIteration"]/option[1]'
+  },
 };
 export class ViewReport extends BasePage {
 
   viewReportTab = this.ElementLocator(Locators.viewReportTab);
+  projectListSelector = this.ElementLocator(Locators.projectListSelector);
+  cmsOption = this.ElementLocator(Locators.cmsOption);
+  selectedIteration = this.ElementLocator(Locators.selectedIteration);
 
   ViewReportTab() {
     return this.viewReportTab;
   }
-
-  ProjectListSelector(){
-    return this.ProjectListSelector;
+  ProjectListSelector() {
+    return this.projectListSelector;
   }
-
-
-
+  CmsOption() {
+    return this.cmsOption;
+  }
+  SelectedIteration() {
+    return this.selectedIteration;
+  }
 }
